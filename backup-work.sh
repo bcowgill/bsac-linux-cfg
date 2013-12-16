b1b6c0b4fb07d6389c2224bc53cf751a78bd0b5e
Brent Cowgill <brent.cowgill@ontology.com>
2013-12-16 09:56:59 +0000

    additions to files to backup to dropbox for WFH possibilities

--- backup-work.sh
+++ backup-work.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 # save some stuff to dropbox for work at home
+DROP=~/Dropbox/WorkSafe/_tx/ontology
+echo ======================================================================
+date
+mkdir -p $DROP
 pushd ~/workspace
-tar cvzf work-visualise.tgz play/d3 charles-config/
-cp work-visualise.tgz ~/Dropbox/WorkSafe/work-visualise.tgz
+mkdir backup
+tar cvzf backup/ontology-notes.tgz *.txt
+tar cvzf backup/ontology-bin.tgz bin/
+tar cvzf backup/work-visualise.tgz play/ charles-config/
+
+cp ~/Documents/PhoenixYard*.pdf $DROP
+cp backup/ontology-notes.tgz $DROP
+cp backup/ontology-bin.tgz $DROP
+cp backup/work-visualise.tgz $DROP
+
+date
+echo Backup complete: $DROP
+ls -al $DROP
 popd
 
