cc34b7e3c2663222a0eeb4a62832b71a6ed9419c
Brent Cowgill <brent.cowgill@ontology.com>
2013-12-16 10:20:43 +0000

    paste-list.pl script to take a pasted list of items and spit them out as a CSV on a single line

---
+++ paste-list.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl
+# take a pasted list of items one per line and spit it out as comma separated on a single line.
+
+use strict;
+use warnings;
+use English qw(-no_match_vars); 
+
+my $input;
+local $INPUT_RECORD_SEPARATOR = undef;
+$input = <STDIN>;
+$input =~ s{\s*,?\s*\n}{,}xmsg;
+$input =~ s{,\z}{}xmsg;
+$input =~ s{\A,}{}xmsg;
+print "$input\n";
+
+__END__
+Example pasted data:
+2G_Diepsloot_Mall_SGC,
+
+2G_Olivenhoutbosch_SGC,
+
+3G_Blue_Hills_Mobile_SGC,
+
+3G_Saddlebrook_MN1_SGC,
+
+
