9cfdd2f348017d530f6fa60b7658ca3d9afe56c4
Brent S.A. Cowgill <brent.cowgill@ontology.command>
2014-01-14 12:43:27 +0000

    what-new-cfg.sh puts message about hidden files

--- what-new-cfg.sh
+++ what-new-cfg.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 # show what files are newer than specified file.
 # assumes newest reconfigure.timestamp* if none given
+REGEX='/(workspace|.fontconfig|.Skype|.mozilla|(.config|.cache)/chromium)/'
+
 if [ -z $1 ]; then
    newer=`ls -1 ~/reconfigure.timestamp* -t | head -1`
 else
    newer="$1"
 fi
-pushd ~
-find . -newer $newer | egrep -v "/(workspace|.fontconfig|.Skype|.mozilla|(.config|.cache)/chromium)/" 
-popd
+pushd ~ > /dev/null
+find . -newer $newer | egrep -v "$REGEX" 
+echo NOTE: some files hidden by regex $REGEX
+popd > /dev/null
 
