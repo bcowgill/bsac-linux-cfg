e810186da7352316a27b64604f26e5c2dc540c43
Brent S.A. Cowgill <brent.cowgill@ontology.command>
2014-01-14 18:26:35 +0000

    Added a TODO for charles export

--- backup-work-manual.sh
+++ backup-work-manual.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Manual backup instructions
 echo "Eclipse: File > Export > General > Preferences - save to workspace/backup/brent-eclipse-preferences.epf"
+echo "TODO Charles settings"
