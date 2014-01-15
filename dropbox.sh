4507b020cee8df90d8358c7e3731360aecee1b99
Brent S.A. Cowgill <brent.cowgill@ontology.command>
2014-01-15 15:01:42 +0000

    Updated dropbox.sh starter to match where dropbox is now installed so that it won't be removed is a disk wipe happens again

--- dropbox.sh
+++ dropbox.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# start up dropbox daemon
-~/.dropbox-dist/dropboxd > /tmp/dropbox.log 2>&1 &
+# start up dropbox daemon - shouldn't need to as kde autostart is configured to do it.
+~/workspace/dropbox-dist/.dropbox-dist/dropboxd > /tmp/dropbox.log 2>&1 &
