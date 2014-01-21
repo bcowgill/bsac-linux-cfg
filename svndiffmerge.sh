2da1cf1abf1aaa645dae06e0836f8fb17fc5fced
Brent S.A. Cowgill <brent.cowgill@ontology.command>
2014-01-21 12:45:01 +0000

    Added launcher scripts for using SourceGear DiffMerge with svn diff and merge note: merge is untested but diff works

---
+++ svndiffmerge.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# svn diff script for diffmerge
+# svn diff --diff-cmd=svndiffmerge.sh filename
+# http://support.sourcegear.com/viewtopic.php?f=33&t=11661
+# 1:-u 2:-L 3:Godel clean.launch (revision 37148) 4:-L 5:Godel clean.launch (working copy) 6:/home/brent.cowgill/workspace/trunk/.svn/pristine/63/636deeb529b7ed4699f9a20a53fec14908a24c72.svn-base 7:/home/brent.cowgill/workspace/trunk/features/com.ontologypartners.godel/Godel clean.launch 8: 9:
+#echo 1:$1 2:$2 3:$3 4:$4 5:$5 6:$6 7:$7 8:$8 9:$9
+diffmerge -u -t1="$3" -t2="$5" "$6" "$7"
