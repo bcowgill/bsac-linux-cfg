35dbaaf6f4c6b724215f97149605a8b5f98020b5
Brent S.A. Cowgill <brent.cowgill@ontology.command>
2014-01-22 17:38:52 +0000

    godel-clean.sh script to walk through a clean runtime build and deploy of resources

---
+++ godel-clean.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# run godel really clean 
+echo Stop your runtime now!
+echo Going to delete your /tmp/ontology/system and output you will lose stuff you have not pulled from the runtime!
+read WAIT
+rm -rf /tmp/ontology/output/ /tmp/ontology/system/
+mkdir /tmp/ontology/output/ /tmp/ontology/system
+find /tmp/ontology/
+ps -ef | grep 8085
+echo Go ahead and build Godel clean now in Eclipse then come back here to apply config fragments
+read WAIT
+apply-config-localhost.sh
+echo Go ahead and deploy your resources to the runtime now.
+
