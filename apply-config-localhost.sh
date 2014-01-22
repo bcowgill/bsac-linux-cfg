5829f16829729ce8fc07f8c49aee8309456eae94
Brent S.A. Cowgill <brent.cowgill@ontology.command>
2014-01-22 17:37:45 +0000

    apply-config-localhost.sh adding a sleep to config fragments requests

--- apply-config-localhost.sh
+++ apply-config-localhost.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # Request localhost url's to set all configuration fragments after doing a godel clean run
 curl 'http://localhost:8081/configuration-fragments?action=apply&config-name=analytics-repository'
+sleep 1
 curl 'http://localhost:8081/configuration-fragments?action=apply&config-name=audit-repository'
+sleep 1
 curl 'http://localhost:8081/configuration-fragments?action=apply&config-name=search-annotations-repository'
+sleep 1
 curl 'http://localhost:8081/configuration-fragments?action=apply&config-name=security-repositories'
+sleep 1
 curl 'http://localhost:8081/configuration-fragments?action=apply&config-name=system-repository'
+sleep 1
 curl 'http://localhost:8081/configuration-fragments?action=apply&config-name=ui-metadata-repository'
