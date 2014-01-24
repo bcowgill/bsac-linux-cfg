cc8c644717a595f571904f10ad67bc1ba5975bbf
Brent S.A. Cowgill <brent.cowgill@ontology.command>
2014-01-24 14:47:28 +0000

    cfg-diff.sh touch the reconfigure.timestamp before continuing

--- cfg-diff.sh
+++ cfg-diff.sh
@@ -6,8 +6,9 @@ rm -rf $DIR
 mkdir -p $DIR
 cp -r .kde $DIR/kde
 cp -r .thunderbird $DIR/thunderbird
+touch reconfigure.timestamp
 echo Make some configuration changes in KDE/firefox now!
 read WAIT
-diffmerge .kde/ $DIR/kde &
-diffmerge .thunderbird/ $DIR/thunderbird &
+diffmerge --nosplash .thunderbird/ $DIR/thunderbird &
+diffmerge --nosplash .kde/ $DIR/kde &
 popd
