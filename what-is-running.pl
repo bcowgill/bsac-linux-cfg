c50f7afb160665e3e466cc0d14a45425300f66a5
Brent S.A. Cowgill <brent.cowgill@ontology.command>
2014-01-28 14:45:37 +0000

    What is running: shows rest of java command again

--- what-is-running.pl
+++ what-is-running.pl
@@ -7,8 +7,8 @@ use English;
 
 while (my $line = <>)
 {
-   $line = "MODELLER\n" if $line =~ m{java .+ -launcher \s+ /home/brent.cowgill/modeller/modeller}xms;
-   $line = "ECLIPSE\n"  if $line =~ m{java .+ -launcher \s+ /home/brent.cowgill/eclipse/eclipse}xms;
+   $line = "MODELLER $line\n" if $line =~ m{java .+ -launcher \s+ /home/brent.cowgill/modeller/modeller}xms;
+   $line = "ECLIPSE $line\n"  if $line =~ m{java .+ -launcher \s+ /home/brent.cowgill/eclipse/eclipse}xms;
    $line = "CHARLES PROXY $line"  if $line =~ m{java .+ -jar \s+ /usr/lib/charles-proxy/charles.jar}xms;
    if ($line =~ m{-dev \s+ file:/home/brent.cowgill/workspace/.metadata/.plugins/org.eclipse.pde.core/([^/]+)/dev.properties}xms)
    {
