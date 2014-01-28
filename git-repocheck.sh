10c67675e488bc694d812220fccf81fc9c5af03f
Brent S.A. Cowgill <brent.cowgill@ontology.command>
2014-01-28 09:27:44 +0000

    Adding a separation banner on the repocheck commands

--- git-repocheck.sh
+++ git-repocheck.sh
@@ -5,6 +5,7 @@ REPOS=`find . -type d -name .git | perl -pne 's{\.git \s* \z}{\n}xms'`
 for dir in $REPOS
 do
    pushd $dir > /dev/null
+   perl -e 'print(("=" x 78) . "\n")'
    pwd
    git status
    popd > /dev/null
--- svn-repocheck.sh
+++ svn-repocheck.sh
@@ -5,6 +5,7 @@ REPOS=`find . -type d -name .svn | perl -pne 's{\.svn \s* \z}{\n}xms'`
 for dir in $REPOS
 do
    pushd $dir > /dev/null
+   perl -e 'print(("=" x 78) . "\n")'
    pwd
    svn status
    popd > /dev/null
