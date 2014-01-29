8e08c0470903a7812b24c49d884f257c14da96de
Brent S.A. Cowgill <brent.cowgill@ontology.command>
2014-01-29 09:20:54 +0000

    paste-js-enquote-xml.pl quotes javascript for inclusion into XML configuration files

---
+++ paste-js-enquote-xml.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+# take a pasted bunch of javascript and enquote it as XML element text
+# used for storing code in XML configuration files
+
+use strict;
+use warnings;
+use English qw(-no_match_vars); 
+
+my $input;
+local $INPUT_RECORD_SEPARATOR = undef;
+$input = <STDIN>;
+$input =~ s{<}{&lt;}xmsg;
+$input =~ s{>}{&gt;}xmsg;
+print qq{$input};
+
+__END__
+(function () {
+  var el = document.getElementById('<?>');
+  el.style.height='1024px';
+  el.style.width='100%';
+
+  if (Ontology && Ontology.CCA && Ontology.CCA.loadAssets)
+  {
+    var obj = Ontology.CCA;
+    obj.loadAssets(function () {
+
+Becomes
+
+(function () {
+  var el = document.getElementById('&lt;?&gt;');
+  el.style.height='1024px';
+  el.style.width='100%';
+
+  if (Ontology && Ontology.CCA && Ontology.CCA.loadAssets)
+  {
+    var obj = Ontology.CCA;
+    obj.loadAssets(function () {
+
