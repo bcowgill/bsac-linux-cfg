84efff4c42e6eca43bc4cf24f8672e9a81f6f241
Brent S.A. Cowgill <brent.cowgill@ontology.command>
2014-02-05 12:39:14 +0000

    onboot script start another webserver for the ontoscope test bed in dataflow

--- onboot-bcowgill-dt.sh
+++ onboot-bcowgill-dt.sh
@@ -2,5 +2,8 @@
 
 # Start up a local webserver for the play/d3 area
 pushd ~/workspace/play/d3
-webserver.sh 9999 > /tmp/bcowgill-webserver.log 2>&1 &
+webserver.sh 9999 > /tmp/bcowgill-webserver-9999.log 2>&1 &
+popd
+pushd ~/workspace/play/d3/WebContent/dataflow
+webserver.sh 9898 > /tmp/bcowgill-webserver-9898.log 2>&1 &
 popd
