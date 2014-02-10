45652b5c448745fb1ebfed9427bc34e36403faec
Brent S.A. Cowgill <brent.cowgill@ontology.command>
2014-02-10 09:54:33 +0000

    Adding scripts to disable and enable dojo events in mirrored ontoscope files

--- disable-dojo-events.sh
+++ disable-dojo-events.sh
@@ -19,3 +19,12 @@ MATCH="var _541"
 egrep -rl "$MATCH" . 
 perl -i.bak -pne 's{ (var \s+ _541 \s* = \s* function \s* \( [^\)]* \) \s* \{) \s* }{$1 return; /* BSAC DISABLE DOJO MOUSE EVENT HANDLER */\n}xms' \
    `egrep -rl "$MATCH" .` 
+
+MATCH="scaleCellGraphUp : function"
+egrep -rl "$MATCH" . 
+perl -i.bak -pne 's{ (scaleCellGraph(Up|Down) \s* : \s* function \s* \( [^\)]* \) \s* \{ ) \s* \z }{$1 return; /* BSAC DISABLE DOJO MOUSE EVENT HANDLER */\n}xms' \
+   `egrep -rl "$MATCH" .` 
+
+MATCH="DojoGfxScalePanel.scaleCellGraphUp"
+perl -i.bak -pne 's{ \s (onclick \s* = \s* "DojoGfxScalePanel \. scaleCellGraph(Up|Down) [^"]+") }{ data-bsac-disabled-$1 */\n}xms' \
+   `egrep -rl "$MATCH" .` 
--- enable-dojo-events.sh
+++ enable-dojo-events.sh
@@ -4,3 +4,8 @@ MATCH="BSAC DISABLE DOJO MOUSE"
 egrep -rl "$MATCH" . 
 perl -i.bak -pne 's{ return; \s* /\* \s+ BSAC \s+ DISABLE \s+ DOJO \s+ MOUSE \s+ EVENT \s+ HANDLER \s+ \*/}{}xms' \
    `egrep -rl "$MATCH" .` 
+
+MATCH="data-bsac-disabled-"
+egrep -rl "$MATCH" . 
+perl -i.bak -pne 's{ data-bsac-disabled- }{}xmsg' \
+   `egrep -rl "$MATCH" .` 
