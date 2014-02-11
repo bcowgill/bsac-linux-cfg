64cfed41609a0311276b2606bfe9149dec544382
Brent S.A. Cowgill <brent.cowgill@ontology.command>
2014-02-11 09:51:07 +0000

    disable-ajax-timer.sh shows the files being changed

--- disable-ajax-timer.sh
+++ disable-ajax-timer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # turn off the ajax timer call in mirrored web pages
 
-perl -i.bak -pne 's{function \s+ doAjaxTimer \s* ( \( [^\)]* \) ) \s* \{ \s* \z}{function doAjaxTimer $1 \{ return; /* BSAC DISABLE AJAX TIMER REFRESH */\n}xms' `egrep -rl 'function doAjaxTimer' . | head -1`
+MATCH='function doAjaxTimer' 
+egrep -rl "$MATCH" .
+perl -i.bak -pne 's{function \s+ doAjaxTimer \s* ( \( [^\)]* \) ) \s* \{ \s* \z}{function doAjaxTimer $1 \{ return; /* BSAC DISABLE AJAX TIMER REFRESH */\n}xms' `egrep -rl "$MATCH" . | head -1`
 
