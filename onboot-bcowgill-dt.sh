3d6594e5a18eb87a5c3650266549f8b632bccbfc
Brent S.A. Cowgill <brent.cowgill@ontology.command>
2014-02-21 12:06:24 +0000

    watch-apps remove what-is-running from output. onboot start webserver in grunt test dir

--- onboot-bcowgill-dt.sh
+++ onboot-bcowgill-dt.sh
@@ -7,5 +7,8 @@ popd
 pushd ~/workspace/play/d3/WebContent/dataflow
 webserver.sh 9898 &
 popd
+pushd ~/workspace/play/d3/WebContent/grunt-test/grunt-test-jshint-uglify
+webserver.sh 8555 &
+popd
 echo You need to start Charles before the browsers!
 
