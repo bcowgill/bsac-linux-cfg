8979def0a07aa83c19a5631a6ba8e1fc097acb28
Brent S.A. Cowgill <brent.cowgill@ontology.command>
2014-02-26 09:31:15 +0000

    Added support for the node npm package.json/config.port setting so npm run-script devwebserver can be used

--- webserver.sh
+++ webserver.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Use python to fire up a simple web server to serve content
+# recognize the node npm config.port value if present and use that.
 
 PORT=$1
 DOCROOT=$2
@@ -7,8 +8,13 @@ HTTP_MOD=SimpleHTTPServer
 # for python 3.0+
 #HTTP_MOD=http.server
 
-if [ "x$PORT" == "x" ]; then
-   PORT=9999
+if [ -z "$PORT" ]; then
+   if [ -z "$npm_package_config_port" ]; then
+      PORT=9999
+   else
+      echo package.json config.port setting=$npm_package_config_port
+      PORT=$npm_package_config_port
+   fi
 fi
 
 if [ "x$DOCROOT" == "x" ]; then
