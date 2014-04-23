ec292f95fdc0beb8a7b0b1af9e4250c5d39fb348
Brent S.A. Cowgill <brent@blismedia.com>
2014-04-23 12:17:50 +0100

    ls-tt-tags.pl added to find Template::Toolkit processing markup in files. Aliases findtt and alltt added to help finding TT markup

---
+++ bin/ls-tt-tags.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+# ls-tt-tags.pl
+# list template toolkit tags present within a file
+
+# Usage:
+# find all unique template toolkit blocks
+# export LS_TT_TAGS_INLINE=1; find . -name '*.tt' -exec ls-tt-tags.pl {} \; | sort | uniq
+
+# find template files and show markup with filename.
+# export LS_TT_TAGS_ECHO=1; find . -name '*.tt' -exec ls-tt-tags.pl {} \;
+
+use strict;
+use warnings;
+use English -no_match_vars;
+local $INPUT_RECORD_SEPARATOR = undef; # slurp it all in
+
+my $INLINE = $ENV{LS_TT_TAGS_INLINE} || 0; # convert multiline blocks into a single line for display
+my $ECHO = $ENV{LS_TT_TAGS_ECHO} || 0; # echo the filename if only one on command line
+my $INDENT = "";
+
+if ($ECHO && scalar(@ARGV) == 1) {
+   print "@ARGV\n";
+   $INDENT = "   ";
+}
+
+my $content = <>;
+
+# Template Toolkit markers look like so:
+# [% DIRECTIVE .... %]
+# [%# COMMENTED ... %]
+# can also just be $obj.prop.subprop ...
+
+$content =~ s{( \[ \% .+? \% \] | \$(\w|\.)+ )}{
+   my $match = $1;
+   $match =~ s{\s+}{ }xmsg if $INLINE;
+   $match =~ s{\n}{\n$INDENT}xmsg;
+   print "$INDENT$match\n";
+   ""
+}xmsge;
