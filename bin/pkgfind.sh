f97082c70c2b3340f80746f6a0ca2bc735634a00
Brent S.A. Cowgill <brent@blismedia.com>
2014-04-25 15:01:21 +0100

    Added some usage info when pkgfiles.sh pkgfind.sh pkgless.sh used with no parameter

--- bin/pkgfind.sh
+++ bin/pkgfind.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-# find packages and files
 PKG=$1
-echo "### dpkg -l * -- list similar"
-which dpkg > /dev/null  && dpkg -l *$PKG*
-echo "### apt-cache search -- search similar"
-which apt-cache > /dev/null && apt-cache search $PKG
-
+if [ -z "$PKG" ]; then
+   echo find similarly named packages and files
+   exit 1
+else
+   echo "### dpkg -l * -- list similar"
+   which dpkg > /dev/null  && dpkg -l *$PKG*
+   echo "### apt-cache search -- search similar"
+   which apt-cache > /dev/null && apt-cache search $PKG
+fi
