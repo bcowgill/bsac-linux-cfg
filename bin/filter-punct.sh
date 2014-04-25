726b205071de3b2a4784692b779557dbdc6b5b9d
Brent S.A. Cowgill <brent@blismedia.com>
2014-04-25 14:59:43 +0100

    Added filter-punct.sh to strip away punctuation from config files (was going to use for libre office config checking but ran out of time)

---
+++ bin/filter-punct.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env perl
+# Filter out all punctuation and reduce spacing to single
+use strict;
+use warnings;
+use English -no_match_vars;
+
+while (my $line = <>) {
+   $line =~ s{[<>'"`]+}{ }xmsg;
+   $line =~ s{\s\s+}{ }xmsg;
+   print "$line\n";
+}
