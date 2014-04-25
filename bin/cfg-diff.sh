548fad2cc2301d8bba34d027af4f439e0ad07475
Brent S.A. Cowgill <brent@blismedia.com>
2014-04-25 15:03:01 +0100

    cfg-diff.sh make a copy of .config as well as .kde and diff the results after a config change

--- bin/cfg-diff.sh
+++ bin/cfg-diff.sh
@@ -5,10 +5,12 @@ pushd $HOME
 rm -rf $DIR
 mkdir -p $DIR
 cp -r .kde $DIR/kde
+cp -r .config $DIR/config
 #cp -r .thunderbird $DIR/thunderbird
 touch reconfigure.timestamp
 echo Make some configuration changes in KDE/firefox now!
 read WAIT
 #diffmerge --nosplash .thunderbird/ $DIR/thunderbird &
 diffmerge --nosplash .kde/ $DIR/kde &
+diffmerge --nosplash .config/ $DIR/config &
 popd
