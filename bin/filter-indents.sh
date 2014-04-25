752b9ef8d099ea3870eaf9ae5e59194a0770ebfa
Brent S.A. Cowgill <brent@blismedia.com>
2014-04-25 17:24:39 +0100

    Added filter-indents.sh to remove all leading whitespace to test some html alignment problem caused by indenting.

---
+++ bin/filter-indents.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env perl
+# filter out all indentation from a file
+
+use strict;
+use warnings;
+use English -mo_match_vars;
+
+while (my $line = <>) {
+   chomp($line);
+   $line =~ s{ \A \s+ }{}xmsg;
+   print "$line\n";
+}
+
