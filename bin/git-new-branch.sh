7262eee3940d4e0a58af6f125c389640c5a7fd85
Brent S.A. Cowgill <brent@blismedia.com>
2014-05-09 12:07:31 +0100

    git-new-branch.sh added to create new remote feature branches

---
+++ bin/git-new-branch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# create a new feature branch local and remote from current checked out branch
+NEW=$1
+
+git branch
+if [ -z "$NEW" ]; then
+   echo Please specify the name of a new branch.
+   echo use git branch --list --remote   to see all remote branches.
+   exit 1
+fi
+
+echo Will create a new branch from current one. Press Enter.
+read WAIT
+git checkout -b "$NEW" && \
+   git push --set-upstream origin "$NEW"
+
+echo Teammates can get your branch with:
+echo "git fetch && git checkout origin/$NEW"
