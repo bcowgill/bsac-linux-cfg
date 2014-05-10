640c1eed66345dc345ebf72ce34e5593b57d0390
Brent S.A. Cowgill <zardozcs@yahoo.com>
2014-05-10 19:06:06 +0100

    use-profont.sh added to replace CSS font-family with ProFontWindows

---
+++ bin/use-profont.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# use-profont.sh -i.bak file.css
+# use-profont.sh -i.bak `find . -name '*.css'`
+
+if [ -z "$1" ]; then
+   echo Usage: $0 [-i.bak] file ...
+   echo Change CSS font-family to use profont.
+   echo Example:
+   echo $0 -i.bak \`find . -name \'\*.css\'\`
+   exit 1
+fi
+perl -pne 'BEGIN { $/ = undef; } s{font-family \s* : [^;]+;}{font-family: ProFontWindows, monospace;}xmsg;' $*
+
