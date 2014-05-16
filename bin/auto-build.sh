dcbd7e4ab1acc6b95241607e12e7514e8b0596f2
Brent S.A. Cowgill <brent@blismedia.com>
2014-05-16 16:13:24 +0100

    auto-build.sh display vars and build command when building

--- bin/auto-build.sh
+++ bin/auto-build.sh
@@ -9,12 +9,15 @@ if [ -z "$1" ]; then
    echo You must supply a build command to run.
    exit 1
 fi
+BUILD="$1"
+echo BUILD=$BUILD
+echo TOUCH=$TOUCH
 
 while  [ /bin/true ]
 do
    BUILDIT=0
    if [ -f $TOUCH ]; then
-      if [ `find .. -newer last-build.timestamp | wc -l` == 0 ] ; then
+      if [ `find .. -newer $TOUCH | wc -l` == 0 ]; then
          if [ $LOOPS -gt $TIMES ]; then
             echo `date --rfc-3339=seconds` still nothing new...
             LOOPS=0
@@ -25,9 +28,8 @@ do
    else
       BUILDIT=1
    fi
-
    if [ $BUILDIT == 1 ]; then
-      echo `date --rfc-3339=seconds` building because of something new
+      echo `date --rfc-3339=seconds` "building ($BUILD) because of something new"
       $BUILD
       touch $TOUCH
    fi
