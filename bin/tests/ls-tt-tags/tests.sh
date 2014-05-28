a7c480907bff1e82a3f2f989534c78b61b7a9d49
Brent S.A. Cowgill <brent@blismedia.com>
2014-05-28 12:35:03 +0100

    ls-tt-tags.pl test cases for options echo and inline

--- bin/tests/ls-tt-tags/tests.sh
+++ bin/tests/ls-tt-tags/tests.sh
@@ -15,7 +15,36 @@ echo TEST basic operation
 TEST=basic-operation
 OUT=out/$TEST.out
 BASE=base/$TEST.base
+
+# MUSTDO command line options instead of ENV vars
+export LS_TT_TAGS_INLINE=0
+export LS_TT_TAGS_ECHO=0
+
 $PROGRAM $DEBUG < $SAMPLE > $OUT
+assertFilesEqual "$OUT" "$BASE" "$TEST"
+
+echo TEST echo filename
+TEST=echo-filename
+OUT=out/$TEST.out
+BASE=base/$TEST.base
+
+# MUSTDO command line options instead of ENV vars
+export LS_TT_TAGS_INLINE=0
+export LS_TT_TAGS_ECHO=1
+
+$PROGRAM $DEBUG $SAMPLE > $OUT
+assertFilesEqual "$OUT" "$BASE" "$TEST"
+
+echo TEST tags inline
+TEST=tags-inline
+OUT=out/$TEST.out
+BASE=base/$TEST.base
+
+# MUSTDO command line options instead of ENV vars
+export LS_TT_TAGS_INLINE=1
+export LS_TT_TAGS_ECHO=0
+
+$PROGRAM $DEBUG $SAMPLE > $OUT
 assertFilesEqual "$OUT" "$BASE" "$TEST"
 
 # clean up output directory if no failures
