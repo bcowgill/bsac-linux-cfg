c53c4fdec4049d5e62904c0e29f7b8ef4f16ebee
Brent S.A. Cowgill <brent@blismedia.com>
2014-05-29 09:42:13 +0100

    template/tests.shell.sh added a template for shell script based testing

---
+++ bin/template/tests.shell.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+# Shell script based test plan
+# set -e gotchas http://mywiki.wooledge.org/BashFAQ/105
+set -e
+
+# What we're testing and sample input data
+PROGRAM=../bin/program.pl
+SAMPLE=in/input.txt
+DEBUG=--debug
+DEBUG=
+SKIP=0
+
+# Include testing library and make output dir exist
+source shell-test.sh
+[ -d out ] || mkdir out
+rm out/* > /dev/null 2>&1 || echo OK output dir ready
+
+echo TEST --version option
+TEST=version-option
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG --version"
+	$PROGRAM $ARGS > $OUT || assertCommandSuccess $? "$PROGRAM $ARGS"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+   echo SKIP $TEST "$SKIP"
+fi
+
+echo TEST unknown option
+TEST=unknown-option
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	EXPECT=1
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG --unknown-option"
+	$PROGRAM $ARGS > $OUT 2>&1 || ERR=$?
+	assertCommandFails $ERR $EXPECT "$PROGRAM $ARGS"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+   echo SKIP $TEST "$SKIP"
+fi
+
+echo TEST --man option
+TEST=man-option
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG --man"
+	$PROGRAM $ARGS > $OUT || assertCommandSuccess $? "$PROGRAM $ARGS"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+   echo SKIP $TEST "$SKIP"
+fi
+
+echo TEST basic operation
+TEST=basic-operation
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG $SAMPLE"
+	$PROGRAM $ARGS > $OUT || assertCommandSuccess $? "$PROGRAM $ARGS"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+   echo SKIP $TEST "$SKIP"
+fi
+
+# clean up output directory if no failures
+rm out/* && rmdir out
+echo OK All tests complete `pwd`/out cleaned up
