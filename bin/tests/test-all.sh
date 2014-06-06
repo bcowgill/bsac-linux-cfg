b41147ab248b6556448082b6b12736ef74815f4d
Brent S.A. Cowgill <zardozcs@yahoo.com>
2014-06-06 01:00:26 +0100

    test-all.sh added perltidy-me test suite

--- bin/tests/test-all.sh
+++ bin/tests/test-all.sh
@@ -5,5 +5,6 @@ source ./shell-test.sh
 testSuite ls-tt-tags "list template toolkit tags in templates"
 testSuite render-tt "render a perl Template::Toolkit page with specific variables set"
 testSuite filter-css-colors "filter and replace CSS colors in files"
+testSuite perltidy-me "evaluate perltidy formatting options"
 
 echo OK all test suites completed
