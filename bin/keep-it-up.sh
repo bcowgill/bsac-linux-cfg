f4c38bbefc439b18c8c709f86e6950b64cb600c1
Brent S.A. Cowgill <brent@blismedia.com>
2014-06-12 16:33:57 +0100

    keep-it-up.sh created to continually restart an app (npm start) modified watch-app to show auto-build and keep-it-up scripts

---
+++ bin/keep-it-up.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# poor man's runit - keeps re-running a command when it dies.
+
+if [ -z "$1" ]; then
+   RUN='npm start'
+else
+   RUN=$1
+fi
+
+while  [ /bin/true ]
+do
+   echo Press ^C to stop keeping it up [ $RUN ]
+   sleep 2
+   $RUN
+done
--- bin/watch-apps.sh
+++ bin/watch-apps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # watch java processes continually
-watch 'ps -ef | egrep "node|perl|python|java" | grep -v grep | grep -v what-is-running | what-is-running.pl | sort | perl -ne "print unless m{\A \s* \z}xms;#hidmehideme" | grep -v hidmehideme'
+watch 'ps -ef | egrep "node|perl|python|java|keep-it-up|auto-build" | grep -v grep | grep -v what-is-running | what-is-running.pl | sort | perl -ne "print unless m{\A \s* \z}xms;#hidmehideme" | grep -v hidmehideme'
 
