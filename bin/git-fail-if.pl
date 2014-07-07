e542d089a9be6edd98fc1028a31f2a856add905d
Brent S.A. Cowgill <brent@blismedia.com>
2014-07-07 09:41:19 +0100

    Added git-fail-if.pl script to assist with git bisection finding of errors.

---
+++ bin/git-fail-if.pl
@@ -0,0 +1,82 @@
+#!/usr/bin/env perl
+
+=item OVERVIEW
+
+    Making git bisect more useful
+    By Ovid on July 4, 2014 9:27 AM
+
+    Start bisecting
+    Tell git this is a bad commit
+    Go far enough back in history to find a commit where the test passes
+    Rerun the test to make sure the test passes (very important)
+    Tell git that this is a good commit
+
+    At this point, git will automatically check out a commit halfway between the two commits. You rerun your test and if it passes, you type git bisect good. If it fails, you type git bisect bad. (There's a whole lot more that I'm not covering here). Even if you don't have a test to run, you can use git bisect and, for example, refresh your browser to look for that display bug you're trying to track down and then return to the command line and mark it good or bad as appropriate.
+
+    Repeat this process a few times and eventually git bisect will tell you which commit first introduced the failure. Then you type git bisect reset to stop bisecting.
+
+    And git bisect will happily run the test for you and mark commits good or bad as appropriate, using the exit code of the program. You just sit back and wait for it to finish. Much nicer.
+
+    There are a couple of cases where this doesn't work, though. One case, obviously, is where you must manually verify good/bad and can't write a program to do this. Another case is where sometimes the test fails in different ways. When you do this manually, you can use git bisect skip to skip a commit (or various commits). However, this means falling back to the tedious manual usage of git bisect. Thus, I hacked together the following proof of concept:
+
+    I dropped that into my path and named it fail_if. You pass it a string of text to look for (--contains for an exact match or --matches for a regex match). If that text is found in the programs STDERR, the fail_if exits with a non-zero exit status. Otherwise, it exits with 0 (success).
+
+=item SOURCE
+
+    http://blogs.perl.org/users/ovid/2014/07/making-git-bisect-more-useful.html
+
+=item EXAMPLE
+
+    git bisect start 
+    git bisect bad  
+    git checkout HEAD~20 
+    prove ...
+    git bisect good
+
+    git bisect run git-fail-if.pl --contains "Field 'user' doesn't have a default value" \
+        prove t/path/to/test/that/sometimes/fails/in/different/ways.t
+
+=cut
+
+
+use strict;
+use warnings;
+use Getopt::Long;
+use IPC::Open3;
+use Symbol qw(gensym);
+
+GetOptions(
+    'contains=s' => \my $contains,
+    'matches=s'  => \my $matches,
+) or usage();
+
+usage() unless $contains xor $matches;
+usage() unless @ARGV;
+
+my $command = join ' ' => @ARGV;
+
+my $stderr = gensym;
+my $pid = open3( gensym, ">&STDERR", $stderr, $command );
+
+my $output = '';
+while ( my $err = <$stderr> ) {
+    print STDERR $err;
+    $output .= $err;
+}
+
+my $failed = $matches
+  ? ( $output =~ qr/$matches/ )
+  : ( index( $output, $contains ) > -1 );
+
+waitpid( $pid, 0 );
+
+exit $failed;
+
+sub usage {
+    die <<"END";
+Usage:
+
+$0 --contains 'exact text to fail on' command to run
+$0 --matches  'pattern to fail on'    command to run
+END
+}
