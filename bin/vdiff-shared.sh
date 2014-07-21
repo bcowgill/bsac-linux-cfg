b6832f9752a4edd11213e9ce9a1df59c9891bc3b
Brent S.A. Cowgill <brent@blismedia.com>
2014-07-21 09:23:40 +0100

    vdiff-shared.sh file aded to diff my config files from those in projects

---
+++ bin/vdiff-shared.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Visual diff files from my linux config versus versions in projects
+
+REVERSE=0
+if [ "$1" != "" ]; then
+	REVERSE=1
+fi
+
+function diff_them {
+   local file from to reverse
+   file="$1"
+   from="$2"
+   to="$3"
+   reverse="$4"
+   if diff "$FROM/$FILE" "$TO/$FILE" > /dev/null; then
+      echo no differences for "$FILE" in "$FROM/" or "$TO/" 
+   else
+      if [ $reverse == 0 ]; then
+	      $DIFF "$FROM/$FILE" "$TO/$FILE";
+      else
+	      $DIFF "$TO/$FILE" "$FROM/$FILE";
+      fi
+   fi
+}
+
+DIFF=diffmerge
+FROM=$HOME/bin
+TO=$HOME/workspace/projects/infinity-plus-dashboard/setup
+
+FILE=lib-check-system.sh 
+diff_them $FILE $FROM $TO $REVERSE
+
+FROM=$HOME/bin/tests
+TO=$HOME/workspace/projects/infinity-plus-dashboard/test
+FILE=shell-test.sh
+diff_them $FILE $FROM $TO $REVERSE
+
