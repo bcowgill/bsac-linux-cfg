7f10b96b702de67ec008f8e47b658fd7fa42e223
Brent S.A. Cowgill <brent@blismedia.com>
2014-08-27 14:36:54 +0100

    git-rebase.sh added comments about proper way to rebase/push

--- bin/git-rebase.sh
+++ bin/git-rebase.sh
@@ -11,3 +11,14 @@ else
    git rebase -Xignore-all-space --interactive "$branch" || git mergetool
 fi
 
+# First on your local machine:
+# (on local machine) switch to develop branch
+# git pull (should update your develop with origin/develop - assuming that your develop is set to track origin)
+# switch to the branch you wish to merge
+# git rebase develop
+# Deal with any conflicts (or feel free to abandon the rebase it it's just too painful)
+# git push -f origin [branchname]
+# Then in bitbucket:
+# Reload the pull request and check it still looks OK.
+# Merge the pull request
+# Don't forget to pull in origin/develop before the next rebase.
