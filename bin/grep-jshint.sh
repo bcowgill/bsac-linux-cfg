0aa37dd649c6e058fa0d8a729d5ea80f381b02e5
Brent S.A. Cowgill <brent@blismedia.com>
2014-09-04 16:45:59 +0100

    grep-jshint.sh search the jshint.js file if messages.js has no match

--- bin/grep-jshint.sh
+++ bin/grep-jshint.sh
@@ -2,4 +2,7 @@
 # grep the jshint messages for some text
 
 MESSAGES=`locate --regex 'messages.js$' | grep /node_modules/grunt-contrib-jshint/ | perl -pne 's{\A(.*)\z}{length($1) . " $1" }xmse' | sort -n | perl -pne 's{\A \d+ \s}{}xms' | head -1`
-grep -i "$*" "$MESSAGES"
+JSHINT=`locate --regex 'jshint.js$' | grep /node_modules/grunt-contrib-jshint/node_modules/ | perl -pne 's{\A(.*)\z}{length($1) . " $1" }xmse' | sort -n | perl -pne 's{\A \d+ \s}{}xms' | head -1`
+#echo $MESSAGES
+#echo $JSHINT
+grep -i "$*" "$MESSAGES" || grep -i "$*" "$JSHINT"
