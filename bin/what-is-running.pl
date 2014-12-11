3d79d5de4cefa4c6f0cf8a9ddf57d1a831ab6021
Brent S.A. Cowgill <brent@blismedia.com>
2014-12-11 14:26:18 +0000

    what-is-running.pl - added a check for the location upload daemon

--- bin/what-is-running.pl
+++ bin/what-is-running.pl
@@ -10,10 +10,12 @@ my $prefix = "___";
 while (my $line = <>)
 {
    $line = "${prefix}WEBSERVER     $line\n" if $line =~ m{python .+ (SimpleHTTP|http\.server)}xms;
-   $line = "${prefix}CHARLES PROXY $line\n"   if $line =~ m{java .+ -jar \s+ /usr/lib/charles-proxy/charles.jar}xms;
-   $line = "${prefix}KARMA         $line\n"   if $line =~ m{node(js)? .+ karma \s* start}xms;
-   $line = "${prefix}EXPRESS       $line\n"   if $line =~ m{node(js)? .+ \./bin/www}xms;
-   $line = "${prefix}DASHBOARD     $line\n"   if $line =~ m{perl .+ infinity-plus-dashboard/bin/app\.pl}xms;
+   $line = "${prefix}CHARLES PROXY $line\n" if $line =~ m{java .+ -jar \s+ /usr/lib/charles-proxy/charles.jar}xms;
+   $line = "${prefix}KARMA         $line\n" if $line =~ m{node(js)? .+ karma \s* start}xms;
+   $line = "${prefix}EXPRESS       $line\n" if $line =~ m{node(js)? .+ \./bin/www}xms;
+   $line = "${prefix}DASHBOARD     $line\n" if $line =~ m{perl .+ infinity-plus-dashboard/bin/app\.pl}xms;
+   $line = "${prefix}UPDAEMON      $line\n" if $line =~ m{perl .+ blis-location-uploadd.pl}xms;
+
    $line = "${prefix}AUTOBUILD     $line\n" if $line =~ m{auto-build\.sh}xms;
    $line = "${prefix}KEEP IT UP    $line\n" if $line =~ m{keep-it-up\.sh}xms;
    print $line;
