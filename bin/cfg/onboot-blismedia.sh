5f56e5e2af2b8c97f9612d4c714909c3999e96d3
Brent S.A. Cowgill <brent@blismedia.com>
2015-01-26 16:13:08 +0000

    onboot added watch-apps.sh to final tab

--- bin/cfg/onboot-blismedia.sh
+++ bin/cfg/onboot-blismedia.sh
@@ -132,5 +132,6 @@ echo "3: pushd ~/workspace/projects/infinity-plus-dashboard/"
 echo "4: dbinf"
 echo "5: dbp42"
 echo "6: pushd ~/workspace/projects/review-infinity-plus-dashboard/"
+echo "7: watch-apps.sh"
 fi
 
