63a25b96e18ba27e2c86ce7f0e898f8fb4114449
Brent S.A. Cowgill <zardoz@infoserve.net>
2015-05-21 19:38:53 +0100

    diffetc.sh added cp/diff commands as needed

--- bin/raspberrypi/diffetc.sh
+++ bin/raspberrypi/diffetc.sh
@@ -1,9 +1,24 @@
+if [ -z $DIFFER ]; then
+	source `which differ.sh`
+fi
+
 for file in `find . -type f | grep -v diffetc`
 do
 	if diff $file /$file > /dev/null ; then
 		echo same $file
 	else
 		echo diff $file
+		# get /etc version to here
+		#cp /$file $file
+
+		# put version here to /etc
+		#cp $file /$file
+
+		# diff /etc to here
+		$DIFFER /$file $file $DIFFERWAIT
+
+		# diff here to /etc
+		$DIFFER $file /$file $DIFFERWAIT
 	fi
 done
 
