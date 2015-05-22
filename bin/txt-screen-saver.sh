dc1d19e7548280d8cdea8d3d2660685b1cbb6e08
Brent S.A. Cowgill <zardoz@infoserve.net>
2015-05-22 09:20:01 +0100

    txt-screen-saver.sh created and used in .screenrc

---
+++ bin/txt-screen-saver.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# a text mode screen saver
+
+function choose
+{
+	local run
+	run="$1"
+	if [ -z "$CHOICE" ]; then
+		perl -e 'exit 1 if rand() < 0.1; exit 0;' || CHOICE="$run"
+	fi
+}
+
+CHOICE=""
+while [ -z "$CHOICE" ];
+do
+	which cmatrix   > /dev/null && choose "cmatrix -b -s -C red"
+	which cmatrix   > /dev/null && choose "cmatrix -b -s -C white"
+	which cmatrix   > /dev/null && choose "cmatrix -b -s -C blue"
+	which cmatrix   > /dev/null && choose "cmatrix -b -s -C green"
+	which cmatrix   > /dev/null && choose "cmatrix -b -s -C yellow"
+	which cmatrix   > /dev/null && choose "cmatrix -b -s -C cyan"
+	which cmatrix   > /dev/null && choose "cmatrix -b -s -C magenta"
+	which cmatrix   > /dev/null && choose "cmatrix -b -s -C black"
+	which cacafire  > /dev/null && choose "cacafire"
+	which cacademo  > /dev/null && choose "cacademo"
+	which cacaclock > /dev/null && choose "cacaclock"
+done
+$CHOICE
