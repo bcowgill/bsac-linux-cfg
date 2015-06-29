df39b6e95e7dc0012118834fa0045e3c25d3170e
Brent Cowgill <brent.cowgill@workshare.com>
2015-06-29 17:34:35 +0100

    check-system workshare sublime latest pkg, crontab

--- bin/cfg/check-raspberrypi.sh
+++ bin/cfg/check-raspberrypi.sh
@@ -89,7 +89,7 @@ P4MERGE_PKG=p4v.tgz
 
 SUBLIME=subl
 SUBLIME_CFG=.config/sublime-text-3
-SUBLIME_PKG=sublime-text_build-3065_amd64.deb
+SUBLIME_PKG=sublime-text_build-3083_amd64.deb
 SUBLIME_URL=http://c758482.r82.cf2.rackcdn.com/$SUBLIME_PKG
 
 VSLICK=vs
