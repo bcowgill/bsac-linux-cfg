d25b5042a63c1a71e7fcfb55c7c4a43bde583089
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-07-15 16:49:53 +0100

    scan-code.sh added detection of short var names

--- bin/scan-code.sh
+++ bin/scan-code.sh
@@ -42,7 +42,7 @@ find $dir \
 -o -name '*.csv' -prune \
 -o -name '*.vars' -prune \
 -o \( -type f -exec egrep --with-filename --line-number \
-'\@todo|\b(MUSTDO|FIXME|REFACTOR|QN|[Hh]ack|HACK\b)|\b(maxcomplexity|maxstatements|maxlen|latedef|strict|unused)\s*:|\b(eqeqeq\s*:\s*false)|\bconsole\.(log|info|warn|error|dir|time|timeEnd|trace|assert)|\balert\(|\.(skip|only)\(|\((ev|e)\)' \
+'\@todo|\b(MUSTDO|FIXME|REFACTOR|QN|[Hh]ack|HACK\b)|\b(maxcomplexity|maxstatements|maxlen|latedef|strict|unused)\s*:|\b(eqeqeq\s*:\s*false)|\bconsole\.(log|info|warn|error|dir|time|timeEnd|trace|assert)|\balert\(|\.(skip|only)\(|\((ev|e)\)|var\s+([a-zA-Z]\w?|\w\w)\b' \
 {} \; \
 \)
 
