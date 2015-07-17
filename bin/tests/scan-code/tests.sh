f423488a5ede4115562fdd3bb654ed0ba0a5164a
Brent S.A. Cowgill <zardozcs@yahoo.com>
2015-07-17 06:53:08 +0100

    scan-code.sh test plan standardised

--- bin/tests/scan-code/tests.sh
+++ bin/tests/scan-code/tests.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
-[ ! -d out ] && mkdir out
-../../scan-code.sh in | perl -pne 's{\.js:\d+:}{.js:NN:}xmsg' > out/scanme.out
-diff out/scanme.out base/scanme.base
+
+#!/bin/bash
+# Shell script based test plan
+# set -e gotchas http://mywiki.wooledge.org/BashFAQ/105
+set -e
+
+# What we're testing and sample input data
+PROGRAM=../../scan-code.sh
+CMD=`basename $PROGRAM`
+SAMPLE=in
+DEBUG=--debug
+DEBUG=
+SKIP=0
+
+# Include testing library and make output dir exist
+source ../shell-test.sh
+PLAN 4
+
+[ -d out ] || mkdir out
+rm out/* > /dev/null 2>&1 || OK "output dir ready"
+
+# Do not terminate test plan if out/base comparison fails.
+ERROR_STOP=0
+
+echo TEST $CMD basic operation
+TEST=scanme
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG $SAMPLE"
+	$PROGRAM $ARGS > $OUT || assertCommandSuccess $? "$PROGRAM $ARGS"
+	# convert input file line numbers into NN for file comparison
+	perl -i -pne 's{\.js:\d+:}{.js:NN:}xmsg' $OUT
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
+#stop "simulating a failure so all .html output remains behind for front end behaviour verification"
+
+cleanUpAfterTests
 
