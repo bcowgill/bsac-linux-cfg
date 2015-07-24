e20e493621e06ff5ff0cffda814089232b23761c
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-07-24 23:18:37 +0100

    i3start.sh for 1 and 2 monitor setup with kb/mouse config

--- bin/i3start.sh
+++ bin/i3start.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-# set up single monitor and mouse, trackpad and keyboard settings
-xrandr --output eDP1 --mode 1920x1080
+# set up workspace for i3 window manager based on monitors attached
 
-# set up dual monitors to show in same resolution
-xrandr --output eDP1 --mode 1920x1080 \
-	--output DP1 --primary --mode 1920x1080 --left-of eDP1
+RES=1920x1080
 
-touch-off.sh
+xrandr | grep ' connected'
+MONITORS=`xrandr | grep ' connected' | wc -l`
+MONITOR_NAMES=`xrandr | grep ' connected' | perl -pne 's{\A (\w+) .+ \z}{$1\n}xms'`
 
-# xrandr | grep connected
 # eDP1 is laptop main screen
 #eDP1 connected primary 1920x1080+1920+0 (normal left inverted right x axis y axis) 346mm x 194mm
 #DP1 connected 1920x1080+0+0 (normal left inverted right x axis y axis) 477mm x 268mm
@@ -16,3 +14,30 @@ touch-off.sh
 #HDMI2 disconnected (normal left inverted right x axis y axis)
 #VGA1 disconnected (normal left inverted right x axis y axis)
 #VIRTUAL1 disconnected (normal left inverted right x axis y axis)
+
+if [ $MONITORS == 1 ]; then
+	# set up single monitor
+	MAIN=$MONITOR_NAMES
+	AUX=$MAIN
+	xrandr --output $MAIN --mode $RES
+else
+	if [ $MONITORS == 2 ]; then
+		# set up dual monitors to show in same resolution
+		for mon in $MONITOR_NAMES; do
+			MAIN=${AUX:-$mon}
+			AUX=$mon
+		done
+		xrandr --output $MAIN --mode $RES \
+			--output $AUX --primary --mode $RES --left-of $MAIN 
+	else
+		echo $MONITORS monitors found, unexpected
+		echo $MONITOR_NAMES
+		exit 1
+	fi
+fi
+
+echo MAIN=$MAIN
+echo AUX=$AUX
+
+touch-off.sh
+
