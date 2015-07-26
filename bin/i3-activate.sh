26bb34130c1890a32ab43a5419239f4e5e9547e7
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-07-26 01:59:04 +0100

    i3-config terminal started on workspace 1 instead of using a class match

--- bin/i3-activate.sh
+++ bin/i3-activate.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # after all of i3 startup activate the right workspace/window if possible
+build=$1
+shell=$2
 sleep 9
-i3-msg 'workspace 5; focus right; workspace 1'
+i3-msg "workspace $build; focus right; workspace $shell"
