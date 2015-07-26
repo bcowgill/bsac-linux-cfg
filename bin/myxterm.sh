8bce0f2d40f73451c692f0f09bcb88ba91b073dd
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-07-26 19:46:07 +0100

    myxterm.sh window title and class set to match the name of the command
    that will be run in the xterm

--- bin/myxterm.sh
+++ bin/myxterm.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# starts an xterm with a command to run.
+# Also sets the window title and class to match the command name
+
 # font family (-fa), font size (-fs)
 # reverse video (-rv), NOT show tool bar (-tb), set cursor to box (+uc), blink cursor (-bc), visual bell (-vb), sun/PC keyboard codes (-sp)
 # allow jump scroll (-j), Titled build (-T), class name XTermBuild
@@ -7,5 +10,5 @@ if [ -z $1 ]; then
 else
 	exec=-e
 fi
-xterm -fa ProFontWindows -fs 18 -rv +uc -bc -vb -sp -j -T build -class XTermBuild $exec $* &
+xterm -fa ProFontWindows -fs 18 -rv +uc -bc -vb -sp -j -T $1 -class $1 $exec $* &
 #-report-fonts -report-colors
