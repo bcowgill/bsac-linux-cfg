350ae32d8937e23e37e39fb1efb4d9ff9d9f2f1b
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-07-26 20:06:07 +0100

    xbuild-app/build-app/build-test scripts for building the new-ui from i3wm startup

--- bin/build-app.sh
+++ bin/build-app.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-# build and serve the browser tests.
+# build and serve the app.
 # this script is a helper for when the i3 window manager is starting
 # up all the windows that are needed.
 
 # TODO maybe start a screen/tmux session for two builds ??
+
+# wait a bit for build-test.sh to do its job
+sleep 5
+
 grunt build
 echo SHLVL=$SHLVL
-echo waiting to begin serving tests...
+echo waiting to begin serving the app...
 read wait
-grunt serve:test
+grunt serve
 echo SHLVL=$SHLVL
-echo 'grunt build; grunt serve:test'
+echo 'grunt build; grunt serve'
+
 # TODO skip $SHELL if in a deep shell already
 $SHELL
---
+++ bin/xbuild-app.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# use a terminal to build the new-ui
+mygterm.sh $HOME/projects/new-ui build-app.sh
