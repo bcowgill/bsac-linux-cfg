05dd190ef69aac04dcd2f2530e65020f5f0a2927
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-08-02 20:01:41 +0100

    ls-fonts.sh created to locate font related files on disk

---
+++ bin/ls-fonts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# list info about fonts
+locate fonts.dir
+locate encodings.dir
+locate fonts.alias
+locate fonts.scale
+locate .ttf
+locate .otf
+locate .pcf.gz
+locate .snf
+locate .enc.gz
+locate .bdf
+locate .pfb
+locate .afm
