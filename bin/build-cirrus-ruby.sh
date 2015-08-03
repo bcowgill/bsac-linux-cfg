f333d0cc3d678f9098d44a142ac8d635d95ed01d
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-08-03 16:32:01 +0100

    build-dealroom/cirrus-ruby.sh scripts to fire up the ruby back ends

--- bin/build-cirrus-ruby.sh
+++ bin/build-cirrus-ruby.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-# build and serve the ruby back end
+# build and serve the cirrus ruby back end
 # this script is a helper for when the i3 window manager is starting
 # up all the windows that are needed.
 
 # TODO maybe start a screen/tmux session for two builds ??
 
 source $HOME/.rvm/scripts/rvm
-redis-server &
 
 echo SHLVL=$SHLVL
-echo waiting to begin serving the dealroom ruby app...
+echo waiting to begin serving the cirrus ruby app...
 read wait
-bundle exec rails server -p 3001
+bundle exec rails server
 echo SHLVL=$SHLVL
-echo 'bundle exec rails server -p 3001'
+echo 'bundle exec rails server'
 
 # TODO skip $SHELL if in a deep shell already
 $SHELL
