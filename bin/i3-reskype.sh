002511dc94935bd3c991ae8b12a38408dc5bfd63
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-08-12 09:15:04 +0100

    i3-reskype.sh properly kill the skype process

--- bin/i3-reskype.sh
+++ bin/i3-reskype.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # kill and relaunch skype on the right workspace
-killall skype
+kill -9 `ps -ef | grep skype | egrep -v 'grep|/bin/' | perl -pne '@F=split(/\s+/); $_ = qq{$F[1]\n}'`
 sleep 1
 i3-msg 'workspace 6; exec skype'
