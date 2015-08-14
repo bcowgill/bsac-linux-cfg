eecc5cf6426534f613b614b0faa46d44fcbe0607
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-08-14 23:08:49 +0100

    i3-reskype.sh use pidof to locate the skype to kill

--- bin/i3-reskype.sh
+++ bin/i3-reskype.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # kill and relaunch skype on the right workspace
-kill -9 `ps -ef | grep skype | egrep -v 'grep|/bin/' | perl -pne '@F=split(/\s+/); $_ = qq{$F[1]\n}'`
+kill -9 `pidof skype`
 sleep 1
 i3-msg 'workspace 6; exec skype'
