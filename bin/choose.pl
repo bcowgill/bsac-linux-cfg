2919317a9828494e63522266304ebb0778a6cca9
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-08-15 17:10:27 +0100

    choose.pl can no select N random lines from file
    random-text.sh has a heavier distribution for plain text
    over system information. Adde a manual page lookup from random
    word in the dictionary.
    
    starwars files use ascii text to show the starwars logo

--- bin/choose.pl
+++ bin/choose.pl
@@ -1,6 +1,25 @@
 #!/usr/bin/env perl
-# choose a random line from standard input
+# choose a random up to N lines from standard input
 srand;
-my $line;
-rand($.) < 1 and ($line = $_) while <>;
-print "$line";
+
+my $MAX = shift || 1;
+my ($line, @Lines);
+
+while (<>)
+{
+	if (rand($.) < 1)
+	{
+		@Lines = ($_);
+		$line = $_;
+	}
+	else
+	{
+		if ($line)
+		{
+			push(@Lines, $_) unless scalar(@Lines) >= $MAX;
+		}
+	}
+}
+
+die "FAILED" unless scalar(@Lines <= $MAX);
+print join('', @Lines);
--- bin/random-text.sh
+++ bin/random-text.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 # choose a random fortune generating program
+
+END=
+LINES=15
+RATIO_TEXT=0.8
+RATE_SYSTEM="1/40"
+
+function pick_mode
+{
+	MODE=system
+	perl -e "exit 1 if rand() < $RATIO_TEXT; exit 0;" || MODE="text"
+}
+
+function roll
+{
+	if [ -z "$CHOICE" ]; then
+		perl -e "exit 1 if rand() < $RATE_SYSTEM; exit 0;" || CHOICE="chosen"
+	fi
+}
+
 function choose
 {
 	local run
 	run="$1"
-	if [ -z "$CHOICE" ]; then
-		perl -e 'exit 1 if rand() < 1/50; exit 0;' || CHOICE="chosen"
-	fi
+	roll
 	if [ ! -z "$CHOICE" ]; then
 		$run
+		echo $END
 		exit 0
 	fi
 }
@@ -17,11 +35,10 @@ function chooseN
 {
 	local run
 	run="$1"
-	if [ -z "$CHOICE" ]; then
-		perl -e 'exit 1 if rand() < 0.1; exit 0;' || CHOICE="chosen"
-	fi
+	roll
 	if [ ! -z "$CHOICE" ]; then
-		$run | head -20
+		$run | choose.pl $LINES
+		echo $END
 		exit 0
 	fi
 }
@@ -42,53 +59,61 @@ function pickN
 	which $run > /dev/null && chooseN "$run"
 }
 
+#CHOICE=chosen
+#cat $HOME/bin/template/text/starwars-episode-i.txt
+
+pick_mode
+
 while [ -z "$CHOICE" ];
 do
-	pick fortune
-	pick uptime
-	pick who
-	pick ifconfig
-	pick lspci
-	pick lsusb
-	pick lsblk
-	pick lscpu
-	pick date
-	pick networkctl
-	pickN vdir
-	pickN lsattr
-	pickN lshw
-	pickN lslocks
-	pickN lsmod
-	pickN dmesg
-	pickN netstat
-	pickN xlsfonts
-	pickN du-sk.sh
-	pickN ls-fonts.sh
-	pick what-apps.sh
+	if [ $MODE == text ]; then
+		pick fortune
+		which fortune   > /dev/null && choose "fortune -l"
+		which cat       > /dev/null && choose "cat `find $HOME/bin/template/text/ -type f | choose.pl`"
+		which cat       > /dev/null && chooseN "cat `find $HOME/bin/template/unicode/ -type f | choose.pl`"
+	else
+		pick date
+		pick uptime
+		pick who
+		pick ifconfig
+		pick lspci
+		pick lsusb
+		pick lsblk
+		pick lscpu
+		pick networkctl
+		pickN vdir
+		pickN lsattr
+		pickN lshw
+		pickN lslocks
+		pickN lsmod
+		pickN dmesg
+		pickN netstat
+		pickN xlsfonts
+		pickN du-sk.sh
+		pickN ls-fonts.sh
+		pick what-apps.sh
 
-	which fortune   > /dev/null && choose "fortune -l"
-	which top       > /dev/null && chooseN "top -bn1"
-	which cat       > /dev/null && choose "cat `find $HOME/bin/template/text/ -type f | choose.pl`"
-	which cat       > /dev/null && choose "cat `find $HOME/bin/template/unicode/ -type f | choose.pl`"
-	which strings   > /dev/null && choose "strings `find /usr/bin -type f | choose.pl`"
-	which df        > /dev/null && choose "df -k"
-	which uname     > /dev/null && choose "uname -a"
-	which stty      > /dev/null && choose "stty --all"
-	which ls        > /dev/null && chooseN "ls -al"
-	which ss        > /dev/null && chooseN "ss --all"
-	which getfacl   > /dev/null && chooseN "getfacl ."
-	which networkctl > /dev/null && chooseN "networkctl status --all"
-	which fuser     > /dev/null && chooseN "fuser -l"
-	which lsw       > /dev/null && chooseN "lsw -l"
-	which lsx       > /dev/null && chooseN "lsx /usr/local/sbin"
-	which lsx       > /dev/null && chooseN "lsx /usr/local/bin"
-	which lsx       > /dev/null && chooseN "lsx /usr/local/games"
-	which lsx       > /dev/null && chooseN "lsx /usr/sbin"
-	which lsx       > /dev/null && chooseN "lsx /usr/bin"
-	which lsx       > /dev/null && chooseN "lsx /usr/games"
-	which lsx       > /dev/null && chooseN "lsx /sbin"
-	which lsx       > /dev/null && chooseN "lsx /bin"
-	which lsb_release > /dev/null && choose "lsb_release --all"
-	which ping      > /dev/null && choose "ping -c 1 google.com"
+		which man       > /dev/null && chooseN "man -k `choose.pl < /usr/share/dict/american-english`"
+		which top       > /dev/null && chooseN "top -bn1"
+		which lsb_release > /dev/null && choose "lsb_release --all"
+		which strings   > /dev/null && chooseN "strings `find /usr/bin -type f | choose.pl`"
+		which df        > /dev/null && choose "df -k"
+		which uname     > /dev/null && choose "uname -a"
+		which stty      > /dev/null && choose "stty --all"
+		which ls        > /dev/null && chooseN "ls -al"
+		which ss        > /dev/null && chooseN "ss --all"
+		which locale    > /dev/null && choose "locale -a"
+		which getfacl   > /dev/null && chooseN "getfacl ."
+		which networkctl > /dev/null && chooseN "networkctl status --all"
+		which fuser     > /dev/null && chooseN "fuser -l"
+		which lsw       > /dev/null && chooseN "lsw -l"
+		which lsx       > /dev/null && chooseN "lsx /usr/local/bin"
+		which lsx       > /dev/null && chooseN "lsx /usr/sbin"
+		which lsx       > /dev/null && chooseN "lsx /usr/bin"
+		which lsx       > /dev/null && chooseN "lsx /usr/games"
+		which lsx       > /dev/null && chooseN "lsx /sbin"
+		which lsx       > /dev/null && chooseN "lsx /bin"
+		which ping      > /dev/null && choose "ping -c 1 google.com"
+	fi
 done
 
