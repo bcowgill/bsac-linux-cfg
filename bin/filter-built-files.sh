645a56db4719c7428cdc7cff8dcbbf07ceb593af
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-08-21 09:24:51 +0100

    filter-built-files.sh created to ignore built files/node modules,etc

---
+++ bin/filter-built-files.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# filter out the names of files which are built or part of bower/node component syste
+# usage egrep -rl something | filter-built-files.sh
+egrep -v '/(node_modules|bower_components|dist|cirrus/public|\.tmp|app/assets)/'
