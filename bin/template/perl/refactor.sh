661eb240107b08bbc687f93be7e218f141c27398
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-08-22 17:42:25 +0100

    refactor.sh to apply perl template changes to all dependent scripts

---
+++ bin/template/perl/refactor.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# refactor then perl template changes into all dependent scripts
+
+DIFF=refactor
+TOP=../..
+
+diff ()
+{
+    diffmerge --nosplash $TOP/$1 $TOP/$2
+}
+
+rvdiff ()
+{
+    diffmerge --nosplash $TOP/$2 $TOP/$1
+}
+
+refactor ()
+{
+    diff $1 $2
+    rvdiff $1 $2
+}
+
+$DIFF template/perl/perl.pl template/perl/perl-inplace.pl
+
+BASE=template/perl/perl.pl
+
+diff $BASE filter-css-colors.pl
+diff $BASE ls-tabs.pl
+diff $BASE ls-tt-tags.pl
+diff $BASE render-tt.pl
+diff $BASE scan-js.pl
+diff $BASE pretty-elements.pl
