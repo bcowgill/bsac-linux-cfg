0efaf8298a034ea3c7f6f9c39bb17dcd440743b9
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-08-25 14:58:54 +0100

    use-mocha-dark.sh, make-mocha-dark.sh better way to make the dark mocha
    mocha dark now created by regex replace rather than symlinking because
    could be different version of the js file in use.

---
+++ bin/make-mocha-dark.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# make-mocha-dark.sh mocha-light.js > mocha-dark.js
+# grab the mocha.js file and change the canvas progress indicator to
+# a dark colour scheme.
+FILE=${1:-../../node_modules/mocha/mocha.js}
+
+perl -pne 's{fillText}{fillStyle = "yellow"; // BSAC DARK SCHEME\n    ctx.fillText}xmsg' $FILE
--- bin/use-mocha-dark.sh
+++ bin/use-mocha-dark.sh
@@ -1,26 +1,50 @@
 #!/bin/bash
 # find all mocha.css/js files and replace with a link to mocha-dark.css
+# usage  use-mocha-dark.sh dark|light [dir-to-mocha-dark]
 
+DIR=`pwd`
 USE=${1:-dark}
+DARK_DIR=${2:-$DIR}
 #echo "USE=$USE"
 
+if [ $USE != dark ]; then
+	if [ $USE != light ]; then
+		echo First parameter must be dark or light, not $USE
+		exit 1
+	fi
+fi
+
+if [ ! -d "$DARK_DIR" ]; then
+	echo $DARK_DIR is not a directory
+	exit 1
+fi
+
+pushd $DARK_DIR > /dev/null
+	DARK_DIR=`pwd`
+popd > /dev/null
+
+if [ ! -d "$DARK_DIR" ]; then
+	echo $DARK_DIR is not a directory
+	exit 1
+fi
+echo Will look for mocha-dark files somewhere within $DARK_DIR
+
 # find the dark files to use
-DIR=`pwd`
-DARK_CSS=`find . -type f -name mocha-dark.css | head -1`
-DARK_JS=`find . -type f -name mocha-dark.js | head -1`
+DARK_CSS=`find $DARK_DIR -type f -name mocha-dark.css | head -1`
+DARK_JS=`find $DARK_DIR -type f -name mocha-dark.js | head -1`
+
+DARK_CSS="$DARK_CSS"
+DARK_JS="$DARK_JS"
 
 if [ -z $DARK_CSS ]; then
-	echo did not find a mocha-dark.css below $DIR
+	echo did not find a mocha-dark.css below $DARK_DIR
 	exit 1
 fi
 
 if [ -z $DARK_JS ]; then
-	echo did not find a mocha-dark.js below $DIR
+	echo did not find a mocha-dark.js below $DARK_DIR
 	exit 1
 fi
-
-DARK_CSS="$DIR/$DARK_CSS"
-DARK_JS="$DIR/$DARK_JS"
 
 echo DARK_CSS="$DARK_CSS"
 echo DARK_JS="$DARK_JS"
@@ -33,10 +57,10 @@ for dir in $DIRS_TO_DO; do
 	echo making mocha $USE in dir $dir
 #	ls $dir/mocha* 2> /dev/null
 	# make sure light/dark versions present
-	[ -e $dir/mocha-dark.js ]  || (echo put dark js version in $dir;  ln -s $DARK_JS $dir/mocha-dark.js)
-	[ -e $dir/mocha-dark.css ] || (echo put dark css version in $dir; ln -s $DARK_CSS $dir/mocha-dark.css)
 	[ -L $dir/mocha.js ]  || (echo move mocha.js file to light js version in $dir;  mv $dir/mocha.js $dir/mocha-light.js)
 	[ -L $dir/mocha.css ] || (echo move mocha.css file to light css version in $dir; mv $dir/mocha.css $dir/mocha-light.css)
+	[ -e $dir/mocha-dark.css ] || (echo put dark css version in $dir; ln -s $DARK_CSS $dir/mocha-dark.css)
+	[ -e $dir/mocha-dark.js ]  || (echo make dark js version in $dir; make-mocha-dark.sh $dir/mocha-light.js > $dir/mocha-dark.js)
 
 	# link to light/dark version
 	[ -L $dir/mocha.js ]   && (echo remove mocha.js link in $dir;  rm $dir/mocha.js)
