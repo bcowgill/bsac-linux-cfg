7975c4f9aace845de6d1c63a232230714517f435
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-08-28 21:44:26 +0100

    mk-fortune-starwars.sh proper build and install of starwars fortunes

--- bin/template/text/mk-fortune-starwars.sh
+++ bin/template/text/mk-fortune-starwars.sh
@@ -3,7 +3,12 @@
 # http://bradthemad.org/tech/notes/fortune_makefile.php
 
 FORTUNE=starwars.fortune
+LIB=/usr/share/games/fortunes
 rm $FORTUNE
+
+echo "May the force be with you" >> $FORTUNE
+echo '%' >> $FORTUNE
+
 for logo in `ls *.txt`
 do
 	cat $logo >> $FORTUNE
@@ -16,10 +21,18 @@ do
 	echo '%' >> $FORTUNE
 done
 
-echo "May the force be with you" >> $FORTUNE
+strfile -r $FORTUNE
 
-strfile $FORTUNE $FORTUNE.dat
-# install it in global dir
+echo "test fortune file locally"
+fortune `pwd`/starwars.fortune
+
+echo "install $FORTUNE in global dir"
+
+sudo cp starwars.fortune $LIB/starwars
+sudo cp starwars.fortune.dat $LIB/starwars.dat
+
+echo "test fortune in global dir"
+fortune starwars
 
 exit 0
 Makefile example
