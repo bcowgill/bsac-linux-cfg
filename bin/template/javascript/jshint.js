bd1d3835f58a4e6f92efbd784f43e48b7e7a5f58
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-09-03 16:27:11 +0100

    template jshint.js with maxcomplexity settings for classes and tests

---
+++ bin/template/javascript/jshint.js
@@ -0,0 +1,5 @@
+// complexity limitations for Clean Code Classes
+/*jshint maxcomplexity: 4, maxparams: 3, maxdepth: 2 */
+
+// complexity limitations for Clean Code Test Plans
+/*jshint maxcomplexity: 1, maxparams: 3, maxdepth: 2, expr: true */
