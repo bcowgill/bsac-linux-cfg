4a85a84dacbcf15c1fdb46c7977cab9223eb3b7b
Brent S.A. Cowgill <zardozcs@yahoo.com>
2015-09-03 07:24:39 +0100

    mk-fortune.sh to build personal fortune files and install them
    updated the clean-code fortune file

---
+++ bin/fortune/mk-fortune.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# construct fortune files
+# http://bradthemad.org/tech/notes/fortune_makefile.php
+
+LIB=/usr/share/games/fortunes
+FORTUNES=`ls *.fortune`
+
+for file in $FORTUNES
+do
+	strfile -r $file
+	echo "test fortune file locally"
+	fortune `pwd`/$file
+	BASE=`basename $file .fortune`
+	echo "install $file in global dir"
+	sudo cp $file $LIB/$BASE
+	sudo cp $file.dat $LIB/$BASE.dat
+	echo "test fortune in global dir"
+	fortune $BASE
+done
+
+exit 0
+Makefile example
+
+POSSIBLE += $(shell ls -1 | egrep -v '\.dat|README|Makefile' | sed -e 's/$$/.dat/g')
+
+all: ${POSSIBLE}
+
+%.dat : %
+        @strfile $< $@
