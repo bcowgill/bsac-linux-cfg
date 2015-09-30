a51a4f447589b307594b332dfe126c7b2e233c6b
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-09-30 09:33:29 +0100

    test file for law of demeter

---
+++ bin/tests/scan-js/demeter.js
@@ -0,0 +1,8 @@
+this.that.theother.thing(r); // maybe not law of demeter violation
+this.that(x).another(e).thing(f); // likely law of demeter violation
+this.that(f).another(r).thing(a).ok(c).now(k).it(i).is(n).getting(g).ridiculous(r); // likely law of demeter violation
+self.that.theother.thing(rr); // maybe not law of demeter violation
+self.that(r).another(r).thing(r); // likely law of demeter violation
+this.that(r).another(r); // likely law of demeter violation
+this.method(r);  // not a law of demeter violation
+this.value; // not a law of demeter violation
