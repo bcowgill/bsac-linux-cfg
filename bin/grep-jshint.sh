b5269dc1e7a5d5c6ae73a706316619cae5f04fa0
Brent S.A. Cowgill <zardozcs@yahoo.com>
2015-10-06 07:25:55 +0100

    grep-jshint.sh use default file locations before searching

--- bin/grep-jshint.sh
+++ bin/grep-jshint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 # grep the jshint messages for some text
 
-MESSAGES=`locate --regex 'messages.js$' | grep /node_modules/grunt-contrib-jshint/ | perl -pne 's{\A(.*)\z}{length($1) . " $1" }xmse' | sort -n | perl -pne 's{\A \d+ \s}{}xms' | head -1`
-JSHINT=`locate --regex 'jshint.js$' | grep /node_modules/grunt-contrib-jshint/node_modules/ | perl -pne 's{\A(.*)\z}{length($1) . " $1" }xmse' | sort -n | perl -pne 's{\A \d+ \s}{}xms' | head -1`
-#echo $MESSAGES
-#echo $JSHINT
-grep -i "$*" "$MESSAGES" || grep -i "$*" "$JSHINT"
+MESSAGES=node_modules/grunt-contrib-jshint/node_modules/jshint/src/messages.js
+JSHINT=node_modules/grunt-contrib-jshint/node_modules/jshint/src/jshint.js
+if [ ! -f $MESSAGES ]; then
+	MESSAGES=`locate --regex 'messages.js$' | grep /node_modules/grunt-contrib-jshint/ | perl -pne 's{\A(.*)\z}{length($1) . " $1" }xmse' | sort -n | perl -pne 's{\A \d+ \s}{}xms' | head -1`
+fi
+if [ ! -f $JSHINT ]; then
+	JSHINT=`locate --regex 'jshint.js$' | grep /node_modules/grunt-contrib-jshint/node_modules/ | perl -pne 's{\A(.*)\z}{length($1) . " $1" }xmse' | sort -n | perl -pne 's{\A \d+ \s}{}xms' | head -1`
+fi
+grep -i "$*" "$MESSAGES" || grep -i "$*" "$JSHINT" || (echo nothing found in $MESSAGES or $JSHINT)
