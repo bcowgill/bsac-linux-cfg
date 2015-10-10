de6ab1effe7eb2ff5861f28811c37b2154230434
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-10-10 07:30:13 +0100

    debug-js-on/off ignore .bak files when scanning

--- bin/workshare/debug-js-on.sh
+++ bin/workshare/debug-js-on.sh
@@ -5,6 +5,6 @@
 DIR=${1:-~/projects}
 
 pushd $DIR > /dev/null
-	FILES=`git grep -lE '/[/]\s*dbg:' 2> /dev/null || egrep -rl '/[/]\s*dbg:'`
+	FILES=`(git grep -lE '/[/]\s*dbg:' 2> /dev/null || egrep -rl '/[/]\s*dbg:') | egrep -v \.bak\$`
 	perl -i.bak -pne 's{//\s*dbg:(\s*)}{/*dbg:*/$1}xmsg' $FILES
 popd > /dev/null
