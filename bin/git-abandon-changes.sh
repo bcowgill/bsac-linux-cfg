8acb8e163a8a2f5a8f35e58560538f25dbfa5b47
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-10-28 21:04:14 +0000

    git-abandon-changes.sh fixed bug if press enter

--- bin/git-abandon-changes.sh
+++ bin/git-abandon-changes.sh
@@ -4,7 +4,7 @@
 git status --short --untracked-files=no
 echo WARNING: lose your uncommitted changes?  Ctrl-C to abort
 read prompt
-if [ $prompt == y ]; then
+if [ ${prompt:-n} == y ]; then
 	git stash save crap
 	git stash drop
 else
