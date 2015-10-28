49cb3661f480e59732ecd708cb22bd6e6aa84336
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-10-28 21:02:55 +0000

    git-abandon-changes.sh does a git stash save crap; git stash drop

---
+++ bin/git-abandon-changes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# stash and drop your git changes
+
+git status --short --untracked-files=no
+echo WARNING: lose your uncommitted changes?  Ctrl-C to abort
+read prompt
+if [ $prompt == y ]; then
+	git stash save crap
+	git stash drop
+else
+	echo aborted
+fi
+
