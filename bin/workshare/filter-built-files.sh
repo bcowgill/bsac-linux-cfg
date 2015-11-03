cda44b1cf49ffb8fba4b5746143f4bc4f53f33cf
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-11-03 15:13:24 +0000

    corrected filter-qa/filter-built-files problem

--- bin/workshare/filter-built-files.sh
+++ bin/workshare/filter-built-files.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-# filter out the fiels in the qa directory
-# usage egrep -rl something | filter-qa.sh
-egrep -v 'qa/'
+# filter out the names of files which are built or part of bower/node component syste
+# usage egrep -rl something | filter-built-files.sh
+egrep -v '/(node_modules|bower_components|dist|\.tmp|app/assets)/' \
+	| egrep -v 'cirrus/public/new-ui/'
--- bin/workshare/filter-qa.sh
+++ bin/workshare/filter-qa.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# filter out the names of files which are built or part of bower/node component syste
-# usage egrep -rl something | filter-built-files.sh
-egrep -v '/(node_modules|bower_components|dist|\.tmp|app/assets)/' \
-	| egrep -v 'cirrus/public/new-ui/' | egrep -v '\.log(:|$)'
+# filter out the files in the qa directory
+# usage egrep -rl something | filter-qa.sh
+egrep -v 'qa/'
