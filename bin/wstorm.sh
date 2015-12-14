5c9c85b8df5965dbde8f522516ab2a80e6c0a6e8
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-12-14 13:40:57 +0000

    wstorm.sh log webstorm to a logfile

--- bin/wstorm.sh
+++ bin/wstorm.sh
@@ -2,6 +2,13 @@
 # Launch WebStorm sidestepping ibus keyboard issue
 # https://youtrack.jetbrains.com/issue/IDEA-78860
 #IBus 1.5.9
+DIR=/tmp/$USER
+LOG=$DIR/wstorm.log
+
 TRUE=`ibus version | perl -ne 'if (m{IBus \s+ (\d+)\.(\d+)\.(\d+)}xms) { if ($1 <= 1 && $2 <= 5 && $3 < 11) { print "1"; exit 1; } else { print "0"; exit 0; } } '`
+
 export IBUS_ENABLE_SYNC_MODE=$TRUE
-wstorm&
+
+mkdir -p $DIR > /dev/null
+rm $LOG > /dev/null
+wstorm > $LOG 2>&1 &
