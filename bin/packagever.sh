45d32dd3501e89dfa4c17ca396abeea8dee1453e
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-12-14 13:54:23 +0000

    packagever.sh added to get version number from package.json or bower.json

---
+++ bin/packagever.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# show the version from package.json (or bower.json)
+
+FILE=package.json
+if [ ! -f $FILE ]; then
+    FILE=bower.json
+fi
+perl -ne 'sub END { exit !$version }; if (m{("version" \s* : \s* ")([\.0-9]+)(",)}xms) { $version = $2; print $2; exit 0 }' $FILE
