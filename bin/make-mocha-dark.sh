d91eae281120a5a444de366556bfa6172c2c58a7
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2015-12-15 10:56:27 +0000

    make-mocha-dark.sh refers to use-mocha-dark.sh

--- bin/make-mocha-dark.sh
+++ bin/make-mocha-dark.sh
@@ -2,6 +2,7 @@
 # make-mocha-dark.sh mocha-light.js > mocha-dark.js
 # grab the mocha.js file and change the canvas progress indicator to
 # a dark colour scheme.
+# SEE ALSO use-mocha-dark.sh
 FILE=${1:-../../node_modules/mocha/mocha.js}
 
 perl -pne 's{fillText}{fillStyle = "yellow"; // BSAC DARK SCHEME\n    ctx.fillText}xmsg' $FILE
