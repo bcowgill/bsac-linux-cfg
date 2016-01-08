70bbd47945279138ecae792e827f6fb089d86eef
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-01-08 22:26:02 +0000

    debug-css improved to prevent duplication of source-file-less line

--- bin/debug-css.sh
+++ bin/debug-css.sh
@@ -2,7 +2,12 @@
 # annotate css/less files with a class name to identify the source file.
 # useful when CSS is required into an app and gets shoved into a <style> element thereby obscuring where it came from.
 
-# debug-css.sh `find lib/styles -name '*.less'`
-
-WHERE=$(basename `pwd`) perl -i.bak -pne 'if ($ARGV ne $current) { $current = $ARGV; print STDERR "$current\n"; $_ = ".source-file-less { content: @{[chr(34)]}$ENV{WHERE}/$ARGV@{[chr(34)]}; }\n\n$_"; }' $*
-
+WHERE=$(basename `pwd`) perl -i.bak -pne '
+if ($ARGV ne $current) {
+	$current = $ARGV;
+	print STDERR "$current\n";
+	$end = "\n\n";
+	$end = "" if $_ =~ s{ \.source-file-less \s* \{ .* \} }{}xms;
+	$_ = ".source-file-less { content: @{[chr(34)]}$ENV{WHERE}/$ARGV@{[chr(34)]}; }$end$_";
+}
+' `find */styles -name '*.less'`
---
+++ bin/workshare/all-debug-css.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# annotate all less files for debugging in repos
+pushd ~/projects
+DIRS="core-ui files-ui groups-ui dealroom-ui new-ui"
+
+for dir in $DIRS
+do
+	echo " "
+	echo $dir ======================================================
+	pushd $dir > /dev/null
+		debug-css.sh`
+	popd > /dev/null
+done
+
