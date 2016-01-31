456c71d706628cfb6a0172b049f3689b15cceac9
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-01-31 13:57:31 +0000

    git-ls-code.sh filter-code-files.sh created
    git-ls-code.sh shows files under git control but not binaries
    filter-code-files.sh filters out file names which are source code

---
+++ bin/filter-code-files.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# filter out file names which are source code / configuration files
+egrep -vi '(robots\.txt|pre-commit|(Make|Gem|Rake)file)$' \
+	| egrep -vi '\.(p[lm]|sh|bash|bat|cmd|js|json|css|less|scss|html?|c?shtml?|x[ms]l|jade|hbs|asm?|c|cs|cpp|java|jsp|py|python|e?rb|rspec|rake|ru|coffee|eco|sql|md|conf|config|properties|yml|(git|npm|jshint)ignore|git(attributes|modules)|htaccess|editorconfig|(bower|csslint|jshint|vim)rc|jshintrc-\w+)$' | egrep -vi '\.(txt|log1?|jinja|jinger|tolerance|eml|iml|lst|sample|out|old|new|au3)$'
