7f4495b53dbd76a957d30eefea29bef1b2a752a2
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-02-03 11:34:58 +0000

    karma-debug.sh script to run karma with debug turned on

---
+++ bin/karma-debug.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# To debug problems with karma:
+# npm install -g karma karma-chrome-launcher karma-chai karma-mocha karma-phantomjs-launcher karma-requirejs karma-sinon karma-sinon-chai
+# (check package.json for most recent list of karma plugins)
+#
+# Then go to the chrome browser and click the Debug button.
+
+karma --log-level debug --browsers Chrome --single-run false start
