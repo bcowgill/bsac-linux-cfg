127eb81f04a2098f3aed98e42678e9f4df2450be
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-02-04 09:50:49 +0000

    karma-fix-timestamp.sh documentation

--- bin/karma-fix-timestamp.sh
+++ bin/karma-fix-timestamp.sh
@@ -1,4 +1,8 @@
-karma-webstorm.sh|perl -ne '
+#!/bin/bash
+# fix a karma error: There is no timestamp for ....
+# prints out lines which you add to karma.conf.js for require module configuration
+# files: [ {pattern: 'path/to/this.css', include: false} ... ]
+karma-webstorm.sh | perl -ne '
 $q=chr(39);
 $tab="    " x 5;
 if (s[.There \s+ is \s+ no \s+ timestamp \s+ for \s+ /base/ (.+) \!.]
--- bin/workshare/karma-webstorm.sh
+++ bin/workshare/karma-webstorm.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-# run the karma tests with webstorm runner but on the console
+# run the karma tests with webstorm runner but on the console so you can grep the log output
 NODE_DIR=$HOME/.nvm/versions/node/v4.2.1
 NODE=$NODE_DIR/bin/node
 RUNNER=$HOME/Downloads/WebStorm-143.381.46/plugins/js-karma/js_reporter/karma-intellij/lib/intellijRunner.js
 KARMA=$NODE_DIR/lib/node_modules/karma
 $NODE $RUNNER --karmaPackageDir=$KARMA --serverPort=9876 --urlRoot=/
-
-
-
