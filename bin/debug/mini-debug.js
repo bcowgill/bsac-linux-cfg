817f587e42fc778ead5addd9b6a2687163c138ad
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-02-06 20:20:24 +0000

    mini-debug small improvements

--- bin/debug/mini-debug.js
+++ bin/debug/mini-debug.js
@@ -3,7 +3,7 @@
 // set a desired log level with __DEBUG, set __BREAK to enter the debugger
 // set __MATCH to a regex to match against to filter logs
 
-var __DEBUG = 1, __BREAK = 0, __MATCH = /./i, __M = 'minidebug'; if (__DEBUG) { console.trace(__M+'#_debug level ' + __DEBUG + ' =~ ' + __MATCH.toString()); } function _debug(level, match) { 'use strict'; if (level <= __DEBUG && (!match || __MATCH.test(match))) { if (__DEBUG >= 10) { console.trace(__M+'?match['+match+']'); } if (__BREAK) { /*jshint -W087*/
+var __DEBUG = 1, __BREAK = 0, __MATCH = /./i, __M = 'minidebug', __LOG = 0; if (__DEBUG) { console.trace(__M+'#_debug level ' + __DEBUG + ' =~ ' + __MATCH.toString()); } function _debug(level, match) { 'use strict'; if (level <= __DEBUG && (!match || __MATCH.test(match))) { ++__LOG; if (__DEBUG >= 10) { console.trace(__M+'?match['+match+']'); } if (__BREAK) { /*jshint -W087*/
 	debugger; /*jshint +W087*/ }
 return true; } return false; }
 
@@ -12,4 +12,5 @@ return true; } return false; }
 	'use strict';
 	if(_debug(1,'hidden')){ console.log(__M+'#method1', this); }
 	if(_debug(1,'matched')){ console.log(__M+'#method2', this); }
+	console.log(__LOG + ' messages');
 })();
