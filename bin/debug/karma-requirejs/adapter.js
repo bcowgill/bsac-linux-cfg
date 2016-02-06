600a90e63b92cede7b79011ab32627a9c95aafc2
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-02-06 20:51:25 +0000

    karma-requirejs 0.2.2 adapter.js with debug instrumentation

--- bin/debug/karma-requirejs/adapter.js
+++ bin/debug/karma-requirejs/adapter.js
@@ -1,4 +1,8 @@
 (function(karma, requirejs, locationPathname) {
+// VERSION karma-requirejs 0.2.2
+var __DEBUG = 1, __BREAK = 0, __MATCH = /./i, __M = 'karma-requirejs/adapter', __LOG = 0; if (__DEBUG) { console.trace(__M+'#_debug level ' + __DEBUG + ' =~ ' + __MATCH.toString()); } function _debug(level, match) { 'use strict'; if (level <= __DEBUG && (!match || __MATCH.test(match))) { ++__LOG; if (__DEBUG >= 10) { console.trace(__M+'?match['+match+']'); } if (__BREAK) { /*jshint -W087*/
+  debugger; /*jshint +W087*/ }
+return true; } return false; }
 
 // monkey patch requirejs, to use append timestamps to sources
 // to take advantage of karma's heavy caching
@@ -28,16 +32,20 @@ var createPatchedLoad = function(files, originalLoadFn, locationPathname) {
   var IS_DEBUG = /debug\.html$/.test(locationPathname);
 
   return function(context, moduleName, url) {
-    console.log('karma/adapter#createPatchedLoad0 ' + moduleName + ' @ ' + url, context);
-    url = normalizePath(url); console.log('karma/adapter#createPatchedLoad1 ' + moduleName + ' @ ' + url);
+    if(_debug(1,moduleName)){ console.log(__M+'#createPatchedLoad0 ' + moduleName + ' @ ' + url, context); }
+    url = normalizePath(url);
+    if(_debug(2,moduleName)){ console.log(__M+'#createPatchedLoad1 ' + moduleName + ' @ ' + url); }
     if (files.hasOwnProperty(url)) {
       if (!IS_DEBUG) {
-        url = url + '?' + files[url]; console.log('karma/adapter#createPatchedLoad2 timestamped ' + moduleName + ' @ ' + url);
+        url = url + '?' + files[url];
+        if(_debug(3,moduleName)){ console.log(__M+'#createPatchedLoad2 ' + moduleName + ' @ ' + url); }
       }
     } else {
       if (!/https?:\/\/\S+\.\S+/i.test(url)) {
         console.error('There is no timestamp for ' + url + '!');
-      } else { console.log('karma/adapter#createPatchedLoad3 http ' + moduleName + ' @ ' + url); }
+      } else {
+        if(_debug(3,moduleName)){ console.log(__M+'#createPatchedLoad3 ' + moduleName + ' @ ' + url); }
+      }
     }
 
     return originalLoadFn.call(this, context, moduleName, url);
