6dfc5c66984e7262b0b4a09e3816be31e4dcf19e
Brent S.A. Cowgill <zardozcs@yahoo.com>
2016-02-07 16:27:50 +0000

    mv-to-year.sh usage information added

--- bin/mv-to-year.sh
+++ bin/mv-to-year.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-# move files with a year number in them into a year directory.
+# move files with a given year number in them into a year directory.
+
+if [ -z "$1" ]; then
+	echo usage $0 year
+	echo Moves files or directories with the given year number in their name into a year subdirectory
+	exit 1
+fi
+
 Y=$1
 mkdir $Y > /dev/null
 for f in *$Y*
