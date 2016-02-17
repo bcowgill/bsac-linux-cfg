ce9f88624482a79546672d4ffefc577fc187d4a4
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-02-17 14:11:23 +0000

    all-find-commits.sh handle PJ and REPO values

--- bin/all-find-commits.sh
+++ bin/all-find-commits.sh
@@ -8,6 +8,16 @@ if [ -z "$COMMIT" ]; then
 	exit 1
 fi
 
+if [ -z "$PJ" ]; then
+	echo NOT OK you must define the PJ environment variable to indicate where your git projects are.
+	exit 1
+else
+	if [ -z "$REPOS" ]; then
+		echo NOT OK you must define the REPOS environment variable to indicate which git repository directories to process
+		exit 2
+	fi
+	pushd $PJ
+fi
 for dir in $REPOS
 do
 	echo " "
