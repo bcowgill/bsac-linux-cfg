0b1d09e7f0820b70d0e3bcc6b1529410c6c31cee
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-04-11 09:03:08 +0100

    i3 config work

--- bin/i3-launch.sh
+++ bin/i3-launch.sh
@@ -10,11 +10,11 @@ source detect-monitors.sh
 # define vars for workspaces (updated by i3-config-update.sh)
 #WORKSPACEDEF
 #  do not edit settings here...
-	    # 1 or 2 monitors
+	    # 3 monitors
 		shell=1
 		edit=2
 		app=3
-		email=4
+		email=9
 		build=5
 		chat=6
 		vbox=7
