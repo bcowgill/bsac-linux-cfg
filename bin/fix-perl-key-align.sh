6d2e9627e496b2685aee860a14334db21b939501
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-04-16 13:26:26 +0100

    fix-perl-key-align.sh aligns perl => key values at same column

---
+++ bin/fix-perl-key-align.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# align all perl => keys at the same column
+
+function usage {
+	echo $0 [-N] filename
+	echo " "
+	echo "align all perl => key values at the same column."
+	echo if no column number is given it scans the input to find the ideal column to use.
+	exit 1
+}
+
+if echo "$1" | egrep -- '-[0-9]+' > /dev/null; then
+	COL="$1"
+	shift
+	if [ -z "$1" ]; then
+		usage
+	fi
+	COLUMN="$COL" perl -i.bak -pne '
+	$max = abs($ENV{COLUMN});
+	if (m{\A(.+)(=>.*)\z}xms)
+	{
+		$prefix = length($1);
+		$_ = $1 . (" " x ($max - $prefix)) . $2;
+	}' $*
+else
+	if [ -z "$1" ]; then
+		usage
+	fi
+	perl -ne 'if (m{\A(.+)=>})
+	{
+		$prefix = length($1);
+		$max = $prefix if $prefix > $max;
+		$_ = qq($.:$prefix> $_);
+		print;
+	}
+	END {
+		print "max column for => is $max\n";
+	}' $*
+fi
