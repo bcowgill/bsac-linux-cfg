35a02ab23ba7900b40a543d1f2fe720219e49b9c
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-04-16 13:28:26 +0100

    workshare build scripts for docuzilla

--- bin/workshare/xbuild-xilla.sh
+++ bin/workshare/xbuild-xilla.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # use a terminal to build the docuzilla
 #mygterm.sh $HOME/projects/docuzilla/webui build-test.sh
-mygterm.sh $HOME/projects/docuzilla/webui
+mygterm.sh $HOME/projects/docuzilla/webui build-xilla.sh
