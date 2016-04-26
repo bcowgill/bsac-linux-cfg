7cc534f3f7347c3a9d3c19ceb3e4ab4dc9e939d9
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-04-26 14:28:42 +0100

    slay.sh kills a process softly at first before trying harder methods

---
+++ bin/slay.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# slay a process, gently at first but more forcefully if that doesn't work
+
+PID=$1
+WAIT=2
+if [ -z "$PID" ]; then
+	ps -ef
+	echo "PID to kill? "
+	read PID
+fi
+
+function slay
+{
+	local signal pid
+	signal=$1
+	pid=$2
+	kill -0 $pid 2> /dev/null && (echo kill $pid $signal; kill $signal $pid; sleep $WAIT)
+}
+
+if [ ! -z "$PID" ]; then
+	slay -HUP $PID
+	slay -TERM $PID
+	slay -SIGINT $PID
+	slay -KILL $PID
+fi
+
+
