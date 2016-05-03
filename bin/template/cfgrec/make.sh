1ce86c5c2f2944e0129c129d26254b2bfcbc652b
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-05-03 23:40:42 +0100

    emacsercises for moving around by specific amounts

--- bin/template/cfgrec/make.sh
+++ bin/template/cfgrec/make.sh
@@ -1,3 +1,4 @@
 ./emacs-card.pl > emacs-keyref.html 2>&1
 ./emacs-card.pl ergo > emacs-ergo-keyref.html 2>&1
 ./emacs-card.pl emacs-tutorial.markup.txt > emacs-tutorial.txt
+./emacs-card.pl emacsercises.markup.txt > emacsercises.txt
