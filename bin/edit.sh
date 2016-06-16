fad7b4f0501c263263f555fd0bdc75e32a17cd0d
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-06-16 12:19:04 +0100

    edit.sh strip trailing : from file name so pastes from a grep list are easier

--- bin/edit.sh
+++ bin/edit.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 # send files to a running emacs instance or start up a new one if the server is not running
-emacsclient --no-wait --alternate-editor=emacs $*
+# strip any trailing : from a single file name in case you pasted from a grep listing
+
+LAUNCH="emacsclient --no-wait --alternate-editor=emacs"
+
+if [ -z "$2" ]; then
+	DIR=`dirname $1`
+	FILE=`basename $1 :`
+	#echo edit [$DIR] [$FILE]
+	$LAUNCH "$DIR/$FILE"
+else
+	$LAUNCH $*
+fi
