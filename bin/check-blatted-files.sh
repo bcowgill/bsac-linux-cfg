1d2aa469862996a0909dd50cbc25c9cca376518a
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-07-05 17:03:47 +0100

    check-blatted-files.sh ignore .tmp dir

--- bin/check-blatted-files.sh
+++ bin/check-blatted-files.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # check for zero size files which have been blatted somehow
 BLATTED=`\
-find . \( -name node_modules -o -path ./dist \) \
+find . \( -name .tmp -o -name node_modules -o -path ./dist \) \
 -prune -o -size 0 \
-| egrep -v '(/dist|node_modules|\.log$|\.timestamp$)' \
+| egrep -v '(/\.tmp|dist|node_modules|\.log$|\.timestamp$)' \
 `
 if [ ! -z "$BLATTED" ]; then
 	echo WARNING some of your files are zero sized! have they been blatted?
