8f96e0ca756cf8f2c53180043811b717ed7f7aa7
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-07-12 09:42:35 +0100

    screenshot.sh keeps the screenshot app open once started

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-# take a screenshot interactively
-gnome-screenshot --interactive
+# start tool to take a screenshot interactively and keep it running
+
+while [ /bin/true ]
+do
+	if [ `ps -ef | grep gnome-screenshot | grep -v grep | wc -l` == "0" ]; then
+		gnome-screenshot --interactive
+	fi
+done
