360e2ca9542cb2069f814e4a622cd1ff52f27e5c
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-07-15 15:21:10 +0100

    edit.sh strip off :number to paste grep results easily

--- bin/edit.sh
+++ bin/edit.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # send files to a running emacs instance or start up a new one if the server is not running
-# strip any trailing : from a single file name in case you pasted from a grep listing
+# strip any trailing :\d+ from a single file name in case you pasted from a grep listing
 
 LAUNCH="emacsclient --no-wait --alternate-editor=emacs"
 
 if [ -z "$2" ]; then
-	DIR=`dirname $1`
-	FILE=`basename $1 :`
-	#echo edit [$DIR] [$FILE]
-	$LAUNCH "$DIR/$FILE"
+	FILE=`echo $1 | perl -pne 's{:[:0-9]* \s* \z}{}xmsg'`
+	#echo edit [$FILE]
+	$LAUNCH "$FILE"
 else
 	$LAUNCH $*
 fi
