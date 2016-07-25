bfb215333ccebbef8868e1304f42e5a8fe489334
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-07-25 22:33:16 +0100

    battery.sh etc corrected to work on dell 7510

--- bin/battery-percent.sh
+++ bin/battery-percent.sh
@@ -1,3 +1,4 @@
-upower --show-info /org/freedesktop/UPower/devices/battery_BAT1 \
+#upower --show-info /org/freedesktop/UPower/devices/battery_BAT1 \
+upower --show-info `upower --enumerate` \
     | egrep 'percentage' \
     | perl -pne 'chomp; s{\s*(percentage:\s*)}{}xms; $_ .= "🔋 "'
--- bin/battery.sh
+++ bin/battery.sh
@@ -1,3 +1,4 @@
-upower --show-info /org/freedesktop/UPower/devices/battery_BAT1 \
+#upower --show-info /org/freedesktop/UPower/devices/battery_BAT1 \
+upower --show-info `upower --enumerate` \
     | egrep 'percentage|capacity' \
     | perl -pne 's{(percentage)}{battery $1}xms'
