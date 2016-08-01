c85f68bc49e6c140732a526513f78aa735cd00ab
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-08-01 17:28:33 +0100

    git-mustdo.sh list MUSTDO items from within git files

---
+++ bin/git-mustdo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Show what must be done to some git files
+# Looks for MUSTDO markers and lists file/line number nicely
+
+LABEL=${1:-MUSTDO}
+git grep -En $LABEL | perl -pne 's{\A \.+/}{}xms; s{:(\d+):\s*}{ : $1\t}xms'
