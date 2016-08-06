7880cc6cc7dcadeb7d7bd28aa9340d57efe6af8a
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-08-06 06:33:04 +0100

    git-untracked.sh show only untracked files

---
+++ bin/git-untracked.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# show only untracked files from git
+git status | perl -ne 'if (!$found) {$found = 1 if m{\A Untracked \s files:}xms} if (!$print && $found) { $print = 1 if m{\A \t}xms; } print if $print;'
