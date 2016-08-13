cc1dacff420af9a00cc5f1f7b04fbe6d15427f12
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-08-13 21:08:38 +0100

    try-console-fonts.sh/set-console-font.sh and a large font gzip

---
+++ bin/set-console-font.sh
@@ -0,0 +1,3 @@
+# set the linux console font to something with a large number of pixels
+# so that it can be read on a high resolution video display
+/bin/setfont /usr/share/consolefonts/Uni1-VGA32x16.psf.gz
---
+++ bin/try-console-fonts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# cycle through all console fonts of a certain size
+# asking Y or N for a font you like
+SETFONT=/bin/setfont
+DIR='/usr/share/consolefonts/Uni*32*'
+
+for font in $DIR
+do
+	echo $font
+	$SETFONT $font
+	echo "Do you like this font [y/N] ? "
+	read OK
+	if [ "$OK" == "y" ]; then
+		echo adding to set-console-font.sh
+		echo $SETFONT $font >> set-console-font.sh
+	fi
+done
+
