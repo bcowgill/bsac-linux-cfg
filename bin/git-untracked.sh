5ea7781542283dbcf2ad8d369e9a91a5a182a6c6
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-08-14 17:02:35 +0100

    git-untracked.sh suppress a status mesage from git

--- bin/git-untracked.sh
+++ bin/git-untracked.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 # show only untracked files from git
-git status | perl -ne 'if (!$found) {$found = 1 if m{\A Untracked \s files:}xms} if (!$print && $found) { $print = 1 if m{\A \t}xms; } print if $print;'
+git status | perl -ne '
+if (!$found) {
+	$found = 1 if m{
+		\A (Untracked \s files:)
+	}xms
+}
+if (!$print && $found) {
+	$print = 1 if m{\A \t}xms;
+}
+print if $print && !m{
+	no \s changes \s added \s to \s commit
+}xms;
+'
