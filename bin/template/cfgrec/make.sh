f0db6bb32f8728ca8b15529eb21e26172cd56ab3
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-08-23 22:58:32 +0100

    emacs card make to launch editor and browser on files

--- bin/template/cfgrec/make.sh
+++ bin/template/cfgrec/make.sh
@@ -2,3 +2,8 @@
 ./emacs-card.pl ergo > emacs-ergo-keyref.html 2>&1
 ./emacs-card.pl emacs-tutorial.markup.txt > emacs-tutorial.txt
 ./emacs-card.pl emacsercises.markup.txt > emacsercises.txt
+
+if [ "$1" == "launch" ]; then
+	edit.sh emacs-card.pl emacs-tutorial.markup.txt emacsercises.markup.txt emacs-keyref.html emacs-ergo-keyref.html emacs-tutorial.txt emacsercises.txt
+	browser.sh emacs-keyref.html emacs-ergo-keyref.html
+fi
