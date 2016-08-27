1d13db7ecf907857a8ccf8b7765c62e1c0f44d77
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-08-27 20:51:38 +0100

    retag.sh all option to auto-update tag files
    also use osd notifications when tagging dirs with all

--- bin/retag.sh
+++ bin/retag.sh
@@ -2,11 +2,33 @@
 # retag files for emacs/vim
 
 #DEBUG="--verbose --fields=+afikKlmnsSzt"
-XREF=1
+#XREF=1
 LANGOPTS="--langmap=JavaScript:.js.jsx.json"
 # +fq adds a tag for the filename and qualified class members
-TAGOPTS="--recurse --links=no --exclude=node_modules $LANGOPTS --extra=+fq"
-ETAGOPTS="$TAGOPTS --etags-include=$HOME/bin/TAGS"
+TAGOPTS="--recurse --links=no --exclude=node_modules $LANGOPTS --extra=+fq --totals=yes"
+ETAGOPTS="$TAGOPTS"
+# --etags-include=$HOME/bin/TAGS"
+
+function say {
+	local message
+	message="$1"
+	echo "$message"
+	which notify > /dev/null && notify -t "retag.sh" -m "$message"
+}
+
+if [ "${1:-single}" == "all" ]; then
+	# find all tag files and update tags if any newer files are present
+	for tagfile in `locate --wholename --regex '\bTAGS$' | grep $HOME | sort -r`; do
+		dir=`dirname "$tagfile"`
+		pushd $dir > /dev/null
+			echo $dir
+			if [ `find . -newer TAGS -type f | wc -l` != 0 ]; then
+				say "tagging $dir"
+				retag.sh
+			fi
+		popd > /dev/null
+	done
+else
 
 # show what kind of tags are supported by each language supported
 if [ ! -z "$DEBUG" ]; then
@@ -14,8 +36,6 @@ if [ ! -z "$DEBUG" ]; then
 	ctags $LANGOPTS --list-languages
 	ctags $LANGOPTS --list-kinds
 	ctags $LANGOPTS --list-maps
-	# find all known tags files
-	locate --wholename --regex '\bTAGS$'
 fi
 
 if [ ! -z "$XREF" ]; then
@@ -25,12 +45,11 @@ if [ ! -z "$XREF" ]; then
 	ctags $TAGOPTS -x --c-kinds=v --file-scope=no > ctags-global-reference.txt
 fi
 
-if [ -e TAGS ]; then
-	echo retag for emacs
-else
-	echo intial tag for emacs
+if [ -e .gitignore ]; then
+	echo check .gitignore
+	egrep '^TAGS\b' .gitignore > /dev/null || perl -e 'print qq{# ignore editor tag files\nTAGS\n}' >> .gitignore
+	egrep '^tags\b' .gitignore > /dev/null || echo tags >> .gitignore
 fi
-ctags -e $DEBUG $ETAGOPTS
 
 if [ -e tags ]; then
 	echo retag for vim
@@ -39,8 +58,11 @@ else
 fi
 ctags $DEBUG $TAGOPTS
 
-if [ -e .gitignore ]; then
-	echo check .gitignore
-	egrep '^TAGS\b' .gitignore || perl -e 'print qq{# ignore editor tag files\nTAGS\n}' >> .gitignore
-	egrep '^tags\b' .gitignore || echo tags >> .gitignore
+if [ -e TAGS ]; then
+	echo retag for emacs
+else
+	echo intial tag for emacs
+fi
+ctags -e $DEBUG $ETAGOPTS
+
 fi
