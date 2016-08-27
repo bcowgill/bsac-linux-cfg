c71b45087a09d871351ac96c6f3be15cdacaf9bb
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-08-27 17:08:17 +0100

    emacs make file clean up

--- bin/template/emacs/make.sh
+++ bin/template/emacs/make.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# make various emacs references based on key configuration
+
 ./emacs-card.pl > emacs-keyref.html 2>&1
 ./emacs-card.pl ergo > emacs-ergo-keyref.html 2>&1
 ./emacs-card.pl emacs-tutorial.markup.txt > emacs-tutorial.txt
@@ -6,6 +9,8 @@
 pushd ../../fortune && ./mk-fortune.sh && popd
 
 if [ "$1" == "launch" ]; then
-	edit.sh emacs-card.pl emacs-tutorial.markup.txt emacsercises.markup.txt emacs-keyref.html emacs-ergo-keyref.html emacs-tutorial.txt emacsercises.txt emacs.fortune.txt
+	edit.sh emacs-card.pl emacs-tutorial.markup.txt emacsercises.markup.txt \
+		emacs-keyref.html emacs-ergo-keyref.html emacs-tutorial.txt \
+		emacsercises.txt emacs.fortune.txt
 	browser.sh emacs-keyref.html emacs-ergo-keyref.html
 fi
