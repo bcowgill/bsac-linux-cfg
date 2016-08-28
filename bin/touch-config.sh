9c31ce805bf0771c3a8154dcd410b0839c1dafe6
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-08-28 19:27:45 +0100

    home touch-config corrected

--- bin/touch-config.sh
+++ bin/touch-config.sh
@@ -9,11 +9,10 @@ export TOUCH_NIPPLE=
 export TOUCH_SCREEN=13
 
 if [ $HOSTNAME == "akston" ]; then
-	export MOUSE=15
+	export MOUSE=11
 	export TOUCH_PAD=12
-	export TOUCH_NIPPLE=12
+	export TOUCH_NIPPLE=14
 	export TOUCH_SCREEN=
 fi
 
 export TOUCH="$TOUCH_PAD $TOUCH_NIPPLE $TOUCH_SCREEN"
-
