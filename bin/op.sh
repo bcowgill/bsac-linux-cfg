0fe5ced6e68ec5d2cb972fcaaf8aa0d34e17a242
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-08-28 12:32:48 +0100

    op.sh show space used on ELEMENTS backup device

--- bin/op.sh
+++ bin/op.sh
@@ -1,5 +1,8 @@
 # some operation you want to monitor with watcher.sh
-#ps -ef | egrep \\bcp\\b
+if [ -e /media/me/ELEMENTS-2TB ]; then
+	ps -ef | egrep \\bcp\\b
+	df -h /media/me/ELEMENTS-2TB
+fi
 if ps -ef | egrep \\bcvzf\\b; then
 	ls -al /data/me/backup/ezbackup/*.tgz
 fi
