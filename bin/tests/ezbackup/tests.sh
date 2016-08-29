8440276121275e947a3c3261e2419b126f2d7812
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-08-29 17:49:59 +0100

    ezbackup tests for simple partial and full backup

--- bin/tests/ezbackup/tests.sh
+++ bin/tests/ezbackup/tests.sh
@@ -197,6 +197,7 @@ if [ 0 == "$SKIP" ]; then
 	BK_DISK=./out/$TEST/external
 	[ -d "$BK_DIR" ] && rm -rf "$BK_DIR"
 	ARGS="$DEBUG full $SAMPLE $BK_DIR $BK_DISK"
+	# set up full and partial backups
 	mkdir -p $BK_DIR
 	touch "$BK_DIR/full-backup.timestamp"
 	for backup in 1 2 3 4 5; do
@@ -225,6 +226,82 @@ if [ 0 == "$SKIP" ]; then
 else
 	echo SKIP $TEST "$SKIP"
 fi
+
+echo TEST $CMD simple backup does partial
+TEST=simple-partial
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	BK_DIR=./out/$TEST
+	[ -d "$BK_DIR" ] && rm -rf "$BK_DIR"
+	ARGS="$DEBUG partial $SAMPLE $BK_DIR"
+	# set up full backup
+	mkdir -p $BK_DIR
+	touch "$BK_DIR/full-backup.timestamp"
+	touch "$BK_DIR/ezbackup.tgz"
+	sleep 1;
+	touch "$SAMPLE/1.txt"
+	setup_warning
+	$PROGRAM $ARGS > "$OUT" || assertCommandSuccess $? "$PROGRAM $ARGS"
+	restore_warning
+	echo "==========" >> "$OUT"
+	ls -1 $BK_DIR >> "$OUT"
+	clean_output "$OUT"
+	clean_output "$BK_DIR/say.log"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+	assertFilesEqual "$BK_DIR/say.log" "base/$TEST/say.base" "$TEST say"
+	assertFilesEqual "out/$TEST/warnings.log" "base/$TEST/warnings.base" "$TEST home warning"
+	assertFilesEqual "$BK_DIR/backup.1.log" "base/$TEST/backup.base" "$TEST backup"
+	assertFilesEqual "$BK_DIR/stderr.1.log" "base/$TEST/stderr.base" "$TEST stderr"
+	assertFilesEqual "$BK_DIR/errors.1.log" "base/$TEST/errors.base" "$TEST errors"
+	assertFilesEqual "$BK_DIR/ignored-errors.1.log" "base/$TEST/ignored-errors.base" "$TEST ignored"
+	assertFilesEqual "$BK_DIR/files.1.log" "base/$TEST/files.base" "$TEST files"
+	assertFilesEqual "$BK_DIR/filenames.1.log" "base/$TEST/filenames.base" "$TEST filenames"
+	assertFilesEqual "$BK_DIR/directories.1.log" "base/$TEST/directories.base" "$TEST directories"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
+echo TEST $CMD simple backup forces full when all partials done
+TEST=simple-force-full
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	BK_DIR=./out/$TEST
+	[ -d "$BK_DIR" ] && rm -rf "$BK_DIR"
+	ARGS="$DEBUG partial $SAMPLE $BK_DIR"
+	# set up full and partial backups
+	mkdir -p $BK_DIR
+	touch "$BK_DIR/full-backup.timestamp"
+	for backup in 1 2 3 4 5; do
+		touch "$BK_DIR/ezbackup.$backup.tgz"
+	done
+	touch "$BK_DIR/partial.5.timestamp"
+	sleep 1;
+	touch "$SAMPLE/1.txt"
+	setup_warning
+	$PROGRAM $ARGS > "$OUT" || assertCommandSuccess $? "$PROGRAM $ARGS"
+	restore_warning
+	echo "==========" >> "$OUT"
+	ls -1 $BK_DIR >> "$OUT"
+	clean_output "$OUT"
+	clean_output "$BK_DIR/say.log"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+	assertFilesEqual "$BK_DIR/say.log" "base/$TEST/say.base" "$TEST say"
+	assertFilesEqual "out/$TEST/warnings.log" "base/$TEST/warnings.base" "$TEST home warning"
+	assertFilesEqual "$BK_DIR/backup.log" "base/$TEST/backup.base" "$TEST backup"
+	assertFilesEqual "$BK_DIR/stderr.log" "base/$TEST/stderr.base" "$TEST stderr"
+	assertFilesEqual "$BK_DIR/errors.log" "base/$TEST/errors.base" "$TEST errors"
+	assertFilesEqual "$BK_DIR/ignored-errors.log" "base/$TEST/ignored-errors.base" "$TEST ignored"
+	assertFilesEqual "$BK_DIR/files.log" "base/$TEST/files.base" "$TEST files"
+	assertFilesEqual "$BK_DIR/filenames.log" "base/$TEST/filenames.base" "$TEST filenames"
+	assertFilesEqual "$BK_DIR/directories.log" "base/$TEST/directories.base" "$TEST directories"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
 
 #stop "simulating a failure so all output remains behind"
 
