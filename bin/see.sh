7574264d85fb43aeceb9d1e0bd7228e05606573a
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-09-03 16:50:30 +0100

    see.sh special handling for some archive types which less does not automatically handle

--- bin/see.sh
+++ bin/see.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
-# see info about a file/dir
-ls -al $*
-if [ ! -d "$1" ]; then
-	less "$1"
-	file $*
-	md5sum $*
-fi
+# see info about a file/dir/archive
+
+function see_archive {
+	local file
+	file="$1"
+
+	if file "$file" | grep 'bzip2 compressed data'; then
+		( echo "$file" contents: ; tar tvf "$file" ) | less
+		return 0
+	fi
+	if file "$file" | grep 'XZ compressed data'; then
+		( echo "$file" contents: ; tar tvf "$file" ) | less
+		return 0
+	fi
+	return 1
+}
+
+for file in $* ; do
+	if [ ! -e "$file" ]; then
+		echo "$file" No such file or directory
+		exit 1
+	else
+		if [ ! -d "$file" ]; then
+			file $file
+			md5sum $file
+			see_archive "$file" || less "$file"
+		else
+			echo "$file"
+		fi
+		ls -al "$file"
+	fi
+done
+
