524a579f4384e5164a8684f33a9f45f8bde5dc14
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-09-03 16:51:00 +0100

    ark.sh create every type of archive file so you can see compression efficiency

---
+++ bin/ark.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# create every type of archive for something to see what's best
+
+out=$1
+shift
+
+function usage {
+	cmd=`basename $0`
+	echo "$cmd name-sans-extension file-or-dir [...]
+
+create every known type of archive for something
+"
+	exit 1
+}
+
+if [ -z "$out" ]; then
+	usage
+fi
+if [ -z "$1" ]; then
+	usage
+fi
+
+for ext in tar tgz bz2 xz;
+do
+	file="$out.$ext"
+	echo $file
+	tar acf "$file" $*
+done
+
+file="$out.zip"
+echo $file
+zip "$file" $*
+
+file="$out.ar"
+echo $file
+ar qs "$file" $*
+
+ls -aloS ${out}.*
