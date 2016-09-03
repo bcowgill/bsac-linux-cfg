870d7897d3556fc2a53e9ac3ebacd6c475439d7a
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-09-03 17:23:35 +0100

    see.sh supports viewing list of files in archive and binary files as hexdump

--- bin/see.sh
+++ bin/see.sh
@@ -5,6 +5,7 @@ function see_archive {
 	local file
 	file="$1"
 
+	# less supports archive types: ar, tar, tgz, zip, deb
 	if file "$file" | grep 'bzip2 compressed data'; then
 		( echo "$file" contents: ; tar tvf "$file" ) | less
 		return 0
@@ -13,7 +14,42 @@ function see_archive {
 		( echo "$file" contents: ; tar tvf "$file" ) | less
 		return 0
 	fi
+	if file "$file" | grep 'cpio archive'; then
+		( echo "$file" contents: ; cpio --list --file="$file" ) | less
+		return 0
+	fi
+	if file "$file" | grep 'current ar archive'; then
+		less "$file"
+		return 0
+	fi
+	if file "$file" | grep 'tar archive'; then
+		less "$file"
+		return 0
+	fi
+	if file "$file" | grep 'gzip compressed data'; then
+		less "$file"
+		return 0
+	fi
+	if file "$file" | grep 'Zip archive data'; then
+		less "$file"
+		return 0
+	fi
+	if file "$file" | grep 'Debian binary package'; then
+		less "$file"
+		return 0
+	fi
 	return 1
+}
+
+function see_binary {
+	local file
+	file="$1"
+
+	 if perl -e 'exit (-B $ARGV[0] ? 0 : 1 )' "$file"; then
+		 ( echo "$file" contents: ; hexdump -C "$file" ; echo "strings:" ; strings "$file" ) | less
+		 return 0
+	 fi
+	 return 1
 }
 
 for file in $* ; do
@@ -24,7 +60,7 @@ for file in $* ; do
 		if [ ! -d "$file" ]; then
 			file $file
 			md5sum $file
-			see_archive "$file" || less "$file"
+			see_archive "$file" || see_binary "$file" || less "$file"
 		else
 			echo "$file"
 		fi
