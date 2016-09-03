c9aba44f62956799353b6056fb81f2418f1345a3
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-09-03 20:08:00 +0100

    see.sh handle seeing various image formats with see command

--- bin/see.sh
+++ bin/see.sh
@@ -6,35 +6,35 @@ function see_archive {
 	file="$1"
 
 	# less supports archive types: tar, tgz, zip, deb
-	if file "$file" | grep 'bzip2 compressed data'; then
+	if file "$file" | grep 'bzip2 compressed data' > /dev/null; then
 		( echo "$file" contents: ; tar tvf "$file" ) | less
 		return 0
 	fi
-	if file "$file" | grep 'XZ compressed data'; then
+	if file "$file" | grep 'XZ compressed data'> /dev/null; then
 		( echo "$file" contents: ; tar tvf "$file" ) | less
 		return 0
 	fi
-	if file "$file" | grep 'cpio archive'; then
+	if file "$file" | grep 'cpio archive'> /dev/null; then
 		( echo "$file" contents: ; cpio --list --file="$file" ) | less
 		return 0
 	fi
-	if file "$file" | grep 'current ar archive'; then
+	if file "$file" | grep 'current ar archive'> /dev/null; then
 		( echo "$file" contents: ; ar t "$file" ) | less
 		return 0
 	fi
-	if file "$file" | grep 'tar archive'; then
+	if file "$file" | grep 'tar archive'> /dev/null; then
 		less "$file"
 		return 0
 	fi
-	if file "$file" | grep 'gzip compressed data'; then
+	if file "$file" | grep 'gzip compressed data'> /dev/null; then
 		less "$file"
 		return 0
 	fi
-	if file "$file" | grep 'Zip archive data'; then
+	if file "$file" | grep 'Zip archive data'> /dev/null; then
 		less "$file"
 		return 0
 	fi
-	if file "$file" | grep 'Debian binary package'; then
+	if file "$file" | grep 'Debian binary package'> /dev/null; then
 		less "$file"
 		return 0
 	fi
@@ -52,6 +52,47 @@ function see_binary {
 	 return 1
 }
 
+function see_image {
+	# PNG image data
+	if file "$file" | egrep 'MS Windows icon resource'> /dev/null; then
+		base=`basename "$file"`
+		ppm=`mktemp /tmp/$base.XXXXXX.ppm`
+		winicontoppm "$file" > $ppm
+		see $ppm
+		rm $ppm
+		return 0
+	fi
+	if file "$file" | egrep 'pixmap image'> /dev/null; then
+		see "$file"
+		return 0
+	fi
+	if file "$file" | egrep 'PC bitmap'> /dev/null; then
+		see "$file"
+		return 0
+	fi
+	if file "$file" | egrep 'PostScript'> /dev/null; then
+		see "$file"
+		return 0
+	fi
+	if file "$file" | egrep 'PDF document'> /dev/null; then
+		see "$file"
+		return 0
+	fi
+	if file "$file" | egrep 'Targa image data'> /dev/null; then
+		feh "$file"
+		return 0
+	fi
+	if file "$file" | egrep 'image (data|text)'> /dev/null; then
+		see "$file"
+		return 0
+	fi
+	if [ "`basename $file`" == "`basename $file .xbm`.xbm" ]; then
+		see "$file"
+		return 0
+	fi
+	return 1
+}
+
 for file in $* ; do
 	if [ ! -e "$file" ]; then
 		echo "$file" No such file or directory
@@ -60,11 +101,10 @@ for file in $* ; do
 		if [ ! -d "$file" ]; then
 			file $file
 			md5sum $file
-			see_archive "$file" || see_binary "$file" || less "$file"
+			see_archive "$file" || see_image "$file" || see_binary "$file" || less "$file"
 		else
 			echo "$file"
 		fi
 		ls -al "$file"
 	fi
 done
-
