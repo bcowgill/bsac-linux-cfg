5a31de55ff4de26c26c3201588f0b3e0ed77e815
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-10-18 15:05:14 +0100

    git-abandon-changes.sh safely stash/drop so you do not lose your top stash if there are no changes

--- bin/git-abandon-changes.sh
+++ bin/git-abandon-changes.sh
@@ -2,11 +2,10 @@
 # stash and drop your git changes
 
 git status --short --untracked-files=no
-echo WARNING: lose your uncommitted changes?  Ctrl-C to abort
+echo WARNING: lose your uncommitted changes? [y/Ctrl-C to abort]
 read prompt
 if [ ${prompt:-n} == y ]; then
-	git stash save crap
-	git stash drop
+	git stash save crap | grep 'No local changes' || git stash drop;
 else
 	echo aborted
 fi
