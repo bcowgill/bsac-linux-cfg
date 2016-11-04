a2c97d7b30b1eaec59d31e69b574ea44bcf01ea4
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-11-04 13:39:36 +0000

    ls-props.sh list react props by name and indicate if used as PropTypes, props, state or context.

--- bin/ls-props.sh
+++ bin/ls-props.sh
@@ -1,2 +1,15 @@
 # list props/state/types/context definitions/uses from react components
-perl -ne 's{((?:types|props|state|context)\.)(\w+)}{$Found{$1.$2} = 1}xmsge; END { print join("\n", sort keys %Found)}' $*
+perl -ne '
+s{
+	(types|props|state|context)\.(\w+)
+}{
+	my $type = $1 eq "types" ? "PropTypes" : $1;
+	$Found{$2}{$type} = 1
+}xmsge;
+
+END {
+	print join("\n", map {
+		".$_\t" . join(",", sort(keys %{$Found{$_}}))
+	} sort keys %Found)
+}
+' $*
