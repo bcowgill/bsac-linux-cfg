aa6c103213331a029e69b22eb45685c04f72a2f7
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-11-06 20:06:43 +0000

    emacs-cfg.sh ga.sh diff emacs configs, commit gconf

---
+++ bin/emacs-cfg.sh
@@ -0,0 +1,2 @@
+vdiff.sh ~/.emacs ~/bin/cfg/.emacs
+vdiff.sh ~/.emacs.d/lisp/bsac.el ~/bin/cfg/.emacs.d/lisp/bsac.el
---
+++ bin/ga.sh
@@ -0,0 +1,5 @@
+git add cfg/.gconf/apps/gnome-terminal/profiles/Default/%gconf.xml
+clean.sh
+git checkout -- cfg/raspberrypi/etc/rc.local.orig
+git commit
+
