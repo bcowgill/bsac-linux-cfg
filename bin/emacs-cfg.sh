aa4517f9ba52e7feff5690de35b61d03f3f93925
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-11-07 09:10:04 +0000

    emacs-cfg.sh add a reverse parameter

--- bin/emacs-cfg.sh
+++ bin/emacs-cfg.sh
@@ -1,2 +1,10 @@
-vdiff.sh ~/.emacs ~/bin/cfg/.emacs
-vdiff.sh ~/.emacs.d/lisp/bsac.el ~/bin/cfg/.emacs.d/lisp/bsac.el
+#!/bin/bash
+# compare current emacs config with version under git control
+
+DIFF=vdiff.sh
+if [ "${1:-normal}" == "reverse" ]; then
+	DIFF=rvdiff.sh
+fi
+
+$DIFF ~/.emacs ~/bin/cfg/.emacs
+$DIFF ~/.emacs.d/lisp/bsac.el ~/bin/cfg/.emacs.d/lisp/bsac.el
