f8982d8f40bd9b00926470f04c709daecff16da4
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-11-14 15:43:17 +0000

    fix-dates.pl change timestamp on files to a random value in the last 90 days

---
+++ bin/fix-dates.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/env perl
+# read filenames from standard input and update file timestamp to within the last 90 days
+
+use strict;
+use warnings;
+use File::stat;
+use Time::Local;
+use Time::localtime;
+use Time::Piece;
+use Data::Dumper;
+use Time::Seconds;
+
+my $DAYS = 90;
+
+foreach my $filename (<STDIN>) {
+	chomp($filename);
+
+	eval {
+		my $filedate = Time::Piece->new(stat($filename)->mtime);
+		my $rightnow = localtime;
+		my $daysago = int($DAYS * rand());
+		my $then = $rightnow - (ONE_DAY * $daysago);
+
+		my $newtime = Time::Piece->new(
+			timelocal(
+				$filedate->sec,
+				$filedate->min,
+				$filedate->hour,
+				$then->mday,
+				$then->mon - 1,
+				$then->year
+			));
+
+		print qq{$filename @{[$newtime->datetime]}\n};
+		system qq{touch "$filename" --date=@{[$newtime->datetime]}};
+	};
+	if ($@) {
+		print STDERR qq{$filename $@\n};
+	}
+}
