14a9cca0ebcc4d16cb710a167ba437df7ca6784c
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-11-27 23:55:26 +0000

    utf8locale.sh sets locale values for unicode

---
+++ bin/utf8locale.sh
@@ -0,0 +1,20 @@
+# set up locale for unicode
+# https://perlgeek.de/en/article/set-up-a-clean-utf8-environment
+export LANG=en_GB.UTF-8
+export LANGUAGE=en_GB:en
+export LC_CTYPE="$LANG"
+export LC_NUMERIC="$LANG"
+export LC_TIME="$LANG"
+export LC_COLLATE="$LANG"
+export LC_MONETARY="$LANG"
+export LC_MESSAGES="$LANG"
+export LC_PAPER="$LANG"
+export LC_NAME="$LANG"
+export LC_ADDRESS="$LANG"
+export LC_TELEPHONE="$LANG"
+export LC_MEASUREMENT="$LANG"
+export LC_IDENTIFICATION="$LANG"
+export LC_ALL=
+
+# check the locale output is good
+perl -Mcharnames=:full -CS -wle 'print "\N{EURO SIGN}"'
