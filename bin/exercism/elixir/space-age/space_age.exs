46bd4c909fe6b38914346e99a9ec82c0552952b7
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-11-28 21:20:59 +0000

    space_age first solution

--- bin/exercism/elixir/space-age/space_age.exs
+++ bin/exercism/elixir/space-age/space_age.exs
@@ -8,6 +8,26 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
-
+    years_for_seconds(seconds, planet)
   end
+
+  def years_for_seconds(seconds, :earth) do
+    secs_per_earth_year = 31557600
+    seconds / secs_per_earth_year
+  end
+
+  def years_for_seconds(seconds, planet) do
+    years_to_orbit = %{
+      earth: 365.25, # Earth days
+      mercury: 0.2408467, # Earth years
+      venus: 0.61519726,
+      mars: 1.8808158,
+      jupiter: 11.862615,
+      saturn: 29.447498,
+      uranus: 84.016846,
+      neptune: 164.79132
+    }
+    years_for_seconds(seconds, :earth) / years_to_orbit[planet]
+  end
+
 end
