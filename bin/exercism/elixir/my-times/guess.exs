b7e729df872ed51bbbddfdebea9761046647f9fd
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-11-28 00:58:20 +0000

    guess.exs added module documentation

--- bin/exercism/elixir/my-times/guess.exs
+++ bin/exercism/elixir/my-times/guess.exs
@@ -1,6 +1,16 @@
 #!/usr/bin/env elixir
 defmodule Guess
 do
+  @moduledoc """
+  Plays the guess my secret number game.
+
+  Guess.within will play the game given the actual number and the range of values.
+
+  Guess.secret_number will create a number guessing function which responds with :low, :high or :true based on the guess value related to the secret number.
+
+  Guess.my_number will play the game using a given number guessing function.
+  """
+
   # Done like a function that knows it all...
   def within(actual, range \\ 1..1000)
 
@@ -37,23 +47,23 @@ do
   end
 
   # private functions here on down...
-  def make_guess(is_it_this, guess, min..max)
+  defp make_guess(is_it_this, guess, min..max)
   do
     next_guess(is_it_this.(guess), is_it_this, guess, min..max)
   end
 
-  def next_guess(:true, _, guess, _)
+  defp next_guess(:true, _, guess, _)
   do
     IO.puts("Is it #{guess}? yes!\n#{guess}")
   end
 
-  def next_guess(:low, is_it_this, guess, _..max)
+  defp next_guess(:low, is_it_this, guess, _..max)
   do
     IO.puts("Is it #{guess}? too low.")
     my_number(is_it_this, (guess + 1) .. max)
   end
 
-  def next_guess(:high, is_it_this, guess, min.._)
+  defp next_guess(:high, is_it_this, guess, min.._)
   do
     IO.puts("Is it #{guess}? too high.")
     my_number(is_it_this, min .. (guess - 1))
