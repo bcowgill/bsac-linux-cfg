628c9ce0dc04a62813256ed398349d1dce40a54c
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-11-29 13:16:42 +0000

    elixir exercise bob solution unicode regexes

--- bin/exercism/elixir/bob/bob.exs
+++ bin/exercism/elixir/bob/bob.exs
@@ -1,8 +1,16 @@
 defmodule Bob do
+  @silence ~r{\A\s*\z}u
+  @question ~r{[ \? ¿ ; ՞ ؟ ፧ ᥅   ⁇   ⁈  ⁉ ❓ ❔ ⳺  ⳻  ⸮ ㉄ ꘏  ꛷  ︖ 𑅃    ]\s*$}ux
+  @letters ~r[\p{L}]ui
+
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
+        Regex.run(@silence, input) -> "Fine. Be that way!"
+        Regex.run(@question, input) -> "Sure."
+        Regex.run(@letters, input)
+          && input === String.upcase(input) -> "Whoa, chill out!"
 
+        :otherwise -> "Whatever."
     end
   end
 end
