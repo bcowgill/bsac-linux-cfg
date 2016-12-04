5de1ff8ca148ceaa6d68d285bb3570015aef9137
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-12-04 13:28:15 +0000

    sum.exs elixir exercises from programming elixir book on lists

--- bin/exercism/elixir/my-times/sum.exs
+++ bin/exercism/elixir/my-times/sum.exs
@@ -1,12 +1,38 @@
-defmodule Sums
+defmodule Sum
 do
-  def sum (1)
-  do
-    1
+  def mapsum([], _func), do: 0
+  def mapsum(list, func) do
+    func.(hd(list)) + mapsum(tl(list), func)
   end
 
-  def sum (number)
-  do
-    sum(number - 1) + number
+  def max([]), do: nil
+  def max([only | []]), do: only
+  def max([head | tail]) do
+    tailmax = max(tail)
+    cond do
+      head > tailmax -> head
+      :otherwise -> tailmax
+    end
+  end
+
+  def caesar([], _n), do: []
+  def caesar(list, n) do
+    [ rot(hd(list), n) ] ++ caesar(tl(list), n)
+  end
+
+  def rot(char, n) when
+    ('a' <= char) and (char <= 'z') do
+    shift(char, 'a', n)
+  end
+
+  def rot(char, n) when
+    ('A' <= char) and (char <= 'Z') do
+    shift(char, 'a', n)
+  end
+
+  def rot(char, _n), do: char
+
+  defp shift(char, a, n) do
+    char + rem(a + n, 26)
   end
 end
