5146b9ec7ce4a3e8ada09df31335bf47d1a7c6c6
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-12-04 20:35:34 +0000

    elixir exercise raindrops complete

--- bin/exercism/elixir/raindrops/raindrops.exs
+++ bin/exercism/elixir/raindrops/raindrops.exs
@@ -8,8 +8,20 @@ defmodule Raindrops do
   - If the number does not contain 3, 5, or 7 as a prime factor,
     just pass the number's digits straight through.
   """
+
+  @factors [3, 5, 7]
+  @speak %{ 3 => "Pling", 5 => "Plang", 7 => "Plong" }
+
   @spec convert(pos_integer) :: String.t
   def convert(number) do
+    speak = for prime <- @factors,
+      rem(number, prime) == 0,
+      do: @speak[prime]
 
+    case speak do
+      [] -> to_string(number)
+      _ -> to_string(speak)
+    end
   end
+
 end
