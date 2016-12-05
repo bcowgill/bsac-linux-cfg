9d48d9860748b357196a762da9c6ea3559680829
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-12-05 03:13:57 +0000

    exercism sublist progressing the solution

--- bin/exercism/elixir/sublist/sublist.exs
+++ bin/exercism/elixir/sublist/sublist.exs
@@ -3,7 +3,56 @@ defmodule Sublist do
   Returns whether the first list is a sublist or a superlist of the second list
   and if not whether it is equal or unequal to the second list.
   """
-  def compare(a, b) do
-
+  def compare([ a | [] ], [ b | [] ]) do
+    cond do
+      (a === b) -> :equal
+      :otherwise -> :unequal
+    end
   end
+
+  def compare(a, b) when
+    is_list(a) and is_list(b) do
+    cond do
+      (a === b) -> :equal
+      sublist(a, b) -> :sublist
+      superlist(a, b) -> :superlist
+      :otherwise -> :unequal
+    end
+  end
+
+  def sublist([], _), do: true
+  def sublist(_, []), do: false
+
+  def sublist([ a | a_tail ], [ a | b_tail ]) do
+    sublist(a_tail, b_tail) or scan(a, b_tail)
+  end
+
+  def sublist(a, [ _b | b_tail ]) do
+    sublist(a, b_tail)
+  end
+
+  def scan(_a, []), do: false
+
+  def scan(a = [ a_first | _ ], b = [ a_first | b_tail ]) do
+    sublist(a, b) or scan(a, b_tail)
+  end
+
+  def scan(a, [ _b | b_tail ]) do
+    false # scan(a, b_tail)
+  end
+
+  def superlist(_, []), do: true
+
+  def superlist([ a_first | a_tail], [a_first | b_tail ]) do
+    superlist(a_tail, b_tail)
+  end
+
+  def superlist([ _a | [] ], [ _b | _b_tail ]), do: false
+  def superlist([ _a | [] ], [ _b | [] ]), do: false
+
+  def superlist([ _a_first | a_tail], b) do
+    superlist(a_tail, b)
+  end
+
+
 end
