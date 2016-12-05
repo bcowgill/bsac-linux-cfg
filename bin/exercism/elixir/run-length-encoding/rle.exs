151d5ae25726ce323c004e70581680c937a1319b
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-12-05 00:11:55 +0000

    elixir exercise for run length encoding

--- bin/exercism/elixir/run-length-encoding/rle.exs
+++ bin/exercism/elixir/run-length-encoding/rle.exs
@@ -6,13 +6,25 @@ defmodule RunLengthEncoder do
   It should also be able to reconstruct the data into its original form.
   "1H1O1R1S1E" => "HORSE"
   """
+
+  @run_of_same ~r/(.)\1*/
+  @num_repeats ~r/(\d+)(.)/
+
   @spec encode(String.t) :: String.t
   def encode(string) do
-
+    parts = Regex.scan(@run_of_same, string)
+    encoded = for [run, letter] <- parts, do: do_encode(String.length(run), letter)
+    to_string(encoded)
   end
+
+  defp do_encode(repeats, letter), do: to_string(repeats) <> letter
 
   @spec decode(String.t) :: String.t
   def decode(string) do
-
+    parts = Regex.scan(@num_repeats, string)
+    decoded = for [_, repeats, letter] <- parts, do: do_decode(String.to_integer(repeats), letter)
+    to_string(decoded)
   end
+
+  defp do_decode(repeats, letter), do: String.pad_leading("", repeats, letter)
 end
--- bin/exercism/elixir/run-length-encoding/rle_test.exs
+++ bin/exercism/elixir/run-length-encoding/rle_test.exs
@@ -13,27 +13,22 @@ defmodule RunLengthEncoderTest do
     assert RunLengthEncoder.encode("") === ""
   end
 
-  @tag :pending
   test "simple string gets encoded" do
     assert RunLengthEncoder.encode("AAA") === "3A"
   end
 
-  @tag :pending
   test "more complicated string" do
     assert RunLengthEncoder.encode("HORSE") == "1H1O1R1S1E"
   end
 
-  @tag :pending
   test "an even more complex string" do
     assert RunLengthEncoder.encode("WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB") === "12W1B12W3B24W1B"
   end
 
-  @tag :pending
   test "it decodes an encoded simple string" do
     assert RunLengthEncoder.decode("3A") === "AAA"
   end
 
-  @tag :pending
   test "it decodes a more complicated string" do
     assert RunLengthEncoder.decode("12W1B12W3B24W1B") === "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB"
   end
