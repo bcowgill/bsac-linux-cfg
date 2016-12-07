47a107a4d3c6f22e0f6c16f69a46b7f005f9efa2
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-12-07 17:59:34 +0000

    sublist.exs solution final, better names

--- bin/exercism/elixir/sublist/sublist.exs
+++ bin/exercism/elixir/sublist/sublist.exs
@@ -10,38 +10,41 @@ defmodule Sublist do
   def compare(a, b) when
     is_list(a) and is_list(b) do
     trace("cmp1", a, b)
-	result = scan_forward(a, b, :equal)
+    result = scan_forward(a, b, :equal)
     case result do
-	  false -> not_equal(scan_forward(b, a, :superlist))
+      false -> not_equal(scan_forward(b, a, :superlist))
       _ -> result
     end
   end
 
-  defp scan_forward(a, b, default \\ :sublist)
-  defp scan_forward(a, [], _), do: trace("sfA", false)
+  # scan_mode = :equal, :sublist, :superlist depending on where we started
+  defp scan_forward(a, b, scan_mode)
+  defp scan_forward(_a, [], _), do: trace("sfA", false)
 
-  defp scan_forward(a = [ first | a_tail ], b = [ first | b_tail ], default ) do
+  defp scan_forward(a = [ first | a_tail ], b = [ first | b_tail ], scan_mode ) do
     trace("sf0", a, b)
-	result = sublist(a_tail, b_tail)
-	case result do
-	  :equal -> default
-	  true -> equal(default)
-	  false -> scan_forward(a, b_tail, not_equal(default))
-	  _ -> result
-	end
+    result = sublist(a_tail, b_tail)
+    case result do
+      :equal -> scan_mode
+      true -> is_sublist(scan_mode)
+      false -> scan_forward(a, b_tail, not_equal(scan_mode))
+      _ -> result
+    end
   end
 
-  defp scan_forward(a, b =[ _b_first | b_tail ], default) do
+  defp scan_forward(a, b =[ _b_first | b_tail ], scan_mode) do
     trace("sf1", a, b)
-    scan_forward(a, b_tail, not_equal(default))
+    scan_forward(a, b_tail, not_equal(scan_mode))
   end
 
-  defp equal(:equal), do: :sublist
-  defp equal(default), do: default
+  # result is a sublist, convert as needed
+  defp is_sublist(:equal), do: :sublist
+  defp is_sublist(scan_mode), do: scan_mode
 
+  # result cannot be :equal so convert as needed
   defp not_equal(false), do: :unequal
   defp not_equal(:equal), do: :sublist
-  defp not_equal(default), do: default
+  defp not_equal(scan_mode), do: scan_mode
 
   defp sublist([], []), do: trace("sblA", :equal)
   defp sublist([], _), do: trace("sblB", true)
@@ -52,21 +55,21 @@ defmodule Sublist do
     sublist(a_tail, b_tail)
   end
 
-  defp sublist(a, b), do: trace("sblC", false)
+  defp sublist(_a, _b), do: trace("sblC", false)
 
-  defp trace(msg, value) do
+  defp trace(_msg, value) do
 #    IO.puts("\n" <> msg)
 #    IO.inspect(value)
     value
   end
 
-  defp trace(msg, a, b) do
+  defp trace(_msg, _a, _b) do
 #    IO.puts("\n" <> msg)
 #    IO.inspect(%{ a: a, b: b })
   end
 
-  defp trace(msg, a, b, c) do
+#  defp trace(msg, a, b, c) do
 #    IO.puts("\n" <> msg)
 #    IO.inspect(%{ a: a, b: b, c: c })
-  end
+#  end
 end
