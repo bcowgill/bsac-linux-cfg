a0f7bc5f85f2d4d5c82b0d298bd65c6dc1a34d3c
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-12-07 17:43:36 +0000

    sublist solution, bit better performance

--- bin/exercism/elixir/sublist/sublist.exs
+++ bin/exercism/elixir/sublist/sublist.exs
@@ -10,28 +10,42 @@ defmodule Sublist do
   def compare(a, b) when
     is_list(a) and is_list(b) do
     trace("cmp1", a, b)
-    cond do
-      (a === b) -> :equal
-      scan_forward(a, b) -> :sublist
-      scan_forward(b, a) -> :superlist
-      :otherwise -> :unequal
+	result = scan_forward(a, b, :equal)
+    case result do
+	  false -> not_equal(scan_forward(b, a, :superlist))
+      _ -> result
     end
   end
 
-  defp scan_forward(a, []), do: trace("sfA", false)
+  defp scan_forward(a, b, default \\ :sublist)
+  defp scan_forward(a, [], _), do: trace("sfA", false)
 
-  defp scan_forward(a = [ first | a_tail ], b = [ first | b_tail ] ) do
+  defp scan_forward(a = [ first | a_tail ], b = [ first | b_tail ], default ) do
     trace("sf0", a, b)
-    sublist(a_tail, b_tail) or scan_forward(a, b_tail)
+	result = sublist(a_tail, b_tail)
+	case result do
+	  :equal -> default
+	  true -> equal(default)
+	  false -> scan_forward(a, b_tail, not_equal(default))
+	  _ -> result
+	end
   end
 
-  defp scan_forward(a, b =[ _b_first | b_tail ]) do
+  defp scan_forward(a, b =[ _b_first | b_tail ], default) do
     trace("sf1", a, b)
-    scan_forward(a, b_tail)
+    scan_forward(a, b_tail, not_equal(default))
   end
 
-  defp sublist([], _), do: trace("sblA", true)
-  defp sublist(_, []), do: trace("sblB", false)
+  defp equal(:equal), do: :sublist
+  defp equal(default), do: default
+
+  defp not_equal(false), do: :unequal
+  defp not_equal(:equal), do: :sublist
+  defp not_equal(default), do: default
+
+  defp sublist([], []), do: trace("sblA", :equal)
+  defp sublist([], _), do: trace("sblB", true)
+  defp sublist(_, []), do: trace("sblC", false)
 
   defp sublist(a = [ a_first | a_tail ], b = [ a_first | b_tail ]) do
     trace("sbl0", a, b)
@@ -41,13 +55,18 @@ defmodule Sublist do
   defp sublist(a, b), do: trace("sblC", false)
 
   defp trace(msg, value) do
-    #IO.puts("\n" <> msg)
-    #IO.inspect(value)
+#    IO.puts("\n" <> msg)
+#    IO.inspect(value)
     value
   end
 
   defp trace(msg, a, b) do
-    #IO.puts("\n" <> msg)
-    #IO.inspect(%{ a: a, b: b })
+#    IO.puts("\n" <> msg)
+#    IO.inspect(%{ a: a, b: b })
+  end
+
+  defp trace(msg, a, b, c) do
+#    IO.puts("\n" <> msg)
+#    IO.inspect(%{ a: a, b: b, c: c })
   end
 end
