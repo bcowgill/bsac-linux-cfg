670426695b9dd83113d39c0b71fc9e71a215f815
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2016-12-08 17:35:00 +0000

    exercism initial exercises for flatten-array, list-ops, roman-numerals

---
+++ bin/exercism/elixir/flatten-array/flatten_array.exs
@@ -0,0 +1,20 @@
+#!/usr/bin/env elixir
+defmodule FlattenArray do
+  @doc """
+    Accept a list and return the list flattened without nil values.
+
+    ## Examples
+
+      iex> FlattenArray.flatten([1, [2], 3, nil])
+      [1,2,3]
+
+      iex> FlattenArray.flatten([nil, nil])
+      []
+
+  """
+
+  @spec flatten(list) :: list
+  def flatten(list) do
+
+  end
+end
