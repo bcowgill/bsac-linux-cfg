1dc8dbe937dff81da3a639816674ffa3dad64c08
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-12-08 21:39:09 +0000

    exercism roman numerals done

--- bin/exercism/elixir/roman-numerals/roman.exs
+++ bin/exercism/elixir/roman-numerals/roman.exs
@@ -2,7 +2,65 @@ defmodule Roman do
   @doc """
   Convert the number to a roman number.
   """
+  #         1 5 10 50 100 500 1000
+  @roman ~w(I V  X  L  C   D    M)
+
   @spec numerals(pos_integer) :: String.t
-  def numerals(number) do
+  def numerals(0), do: raise("Romans didn't know about zero!")
+  def numerals(number) when number >= 4000 do
+     raise("Romans couldn't count that high!")
   end
+  def numerals(number) when number < 0 do
+     raise("Romans didn't have negative numbers!")
+  end
+
+  def numerals(number) do
+    roman(number)
+  end
+
+  defp roman(number, alphabet \\ @roman)
+
+  defp roman(0, _), do: ""
+
+  defp roman(number, alphabet) when number >= 10 do
+     ones = rem(number, 10)
+     tens = div(number, 10)
+     roman(tens, tl(tl(alphabet))) <> roman(ones, alphabet)
+  end
+
+  defp roman(number, [ i | _alphabet ]) when 3 >= number do
+    repeat(i, number)
+  end
+
+  defp roman(number, [ i, v | _alphabet ]) when 4 === number do
+    i <> v
+  end
+
+  defp roman(number, [ i, v | _alphabet ]) when 8 >= number do
+    v <> repeat(i, number - 5)
+  end
+
+  defp roman(number, [ i, _v, x | _alphabet ]) when 9 == number do
+    i <> x
+  end
+
+  def sigils(), do: @roman
+
+  defp repeat(c, n), do: String.duplicate(c, n)
+
 end
+
+# unit testing raise'd errors:
+# @tag :done
+# test "-1" do
+#   assert glove(fn () -> Roman.numerals(-1) end) == "ERROR %RuntimeError{message: \"Romans didn't have negative numbers!\"}"
+# end
+
+# # catch any error from raise()
+# def glove(func) do
+#   try do
+#     func.()
+#   rescue
+#     error -> "ERROR #{inspect error}"
+#   end
+# end
--- bin/exercism/elixir/roman-numerals/roman_numerals_test.exs
+++ bin/exercism/elixir/roman-numerals/roman_numerals_test.exs
@@ -9,93 +9,123 @@ ExUnit.configure exclude: :pending, trace: true
 defmodule RomanTest do
   use ExUnit.Case
 
-  # @tag :pending
+  @tag :done
   test "1" do
     assert Roman.numerals(1) == "I"
   end
 
-  @tag :pending
+  @tag :done
   test "2" do
     assert Roman.numerals(2) == "II"
   end
 
-  @tag :pending
+  @tag :done
   test "3" do
     assert Roman.numerals(3) == "III"
   end
 
-  @tag :pending
+  @tag :done
   test "4" do
     assert Roman.numerals(4) == "IV"
   end
 
-  @tag :pending
+  @tag :done
   test "5" do
     assert Roman.numerals(5) == "V"
   end
 
-  @tag :pending
+  @tag :done
   test "6" do
     assert Roman.numerals(6) == "VI"
   end
 
-  @tag :pending
+  @tag :done
   test "9" do
     assert Roman.numerals(9) == "IX"
   end
 
-  @tag :pending
+  @tag :done
+  test "10" do
+    assert Roman.numerals(1) == "I"
+  end
+
+  @tag :done
   test "27" do
     assert Roman.numerals(27) == "XXVII"
   end
 
-  @tag :pending
+  @tag :done
   test "48" do
     assert Roman.numerals(48) == "XLVIII"
   end
 
-  @tag :pending
+  @tag :done
   test "59" do
     assert Roman.numerals(59) == "LIX"
   end
 
-  @tag :pending
+  @tag :done
   test "93" do
     assert Roman.numerals(93) == "XCIII"
   end
 
-  @tag :pending
+  @tag :done
   test "141" do
     assert Roman.numerals(141) == "CXLI"
   end
 
-  @tag :pending
+  @tag :done
   test "163" do
     assert Roman.numerals(163) == "CLXIII"
   end
 
-  @tag :pending
+  @tag :done
   test "402" do
     assert Roman.numerals(402) == "CDII"
   end
 
-  @tag :pending
+  @tag :done
   test "575" do
     assert Roman.numerals(575) == "DLXXV"
   end
 
-  @tag :pending
+  @tag :done
   test "911" do
     assert Roman.numerals(911) == "CMXI"
   end
 
-  @tag :pending
+  @tag :done
   test "1024" do
     assert Roman.numerals(1024) == "MXXIV"
   end
 
-  @tag :pending
-  test "3000" do
-    assert Roman.numerals(3000) == "MMM"
+  @tag :done
+  test "3999" do
+    assert Roman.numerals(3999) == "MMMCMXCIX"
   end
+
+  @tag :done
+  test "4000" do
+    assert glove(fn () -> Roman.numerals(4000) end) == "ERROR %RuntimeError{message: \"Romans couldn't count that high!\"}"
+  end
+
+  @tag :done
+  test "0" do
+    assert glove(fn () -> Roman.numerals(0) end) == "ERROR %RuntimeError{message: \"Romans didn't know about zero!\"}"
+  end
+
+  @tag :done
+  test "-1" do
+    assert glove(fn () -> Roman.numerals(-1) end) == "ERROR %RuntimeError{message: \"Romans didn't have negative numbers!\"}"
+  end
+
+  # catch any error from raise()
+  def glove(func) do
+    try do
+      func.()
+    rescue
+      error -> "ERROR #{inspect error}"
+    end
+  end
+
 end
