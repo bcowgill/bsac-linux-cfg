3cfa1e5cc8369fb2aad04f9c29882059d51199a4
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-12-10 22:56:27 +0000

    elixir book issues app, install httpoison and JSON library 'poison' and fetch issues from github

---
+++ bin/exercism/issues/lib/issues/github_issues.ex
@@ -0,0 +1,26 @@
+defmodule Issues.GithubIssues do
+  @user_agent  [ {"User-agent", "Elixir dave@pragprog.com"} ]
+
+  # use a module attribute to fetch the value at compile time
+  @github_url Application.get_env(:issues, :github_url)
+
+  def fetch(user, project) do
+    issues_url(user, project)
+    |> HTTPoison.get(@user_agent)
+    |> handle_response
+  end
+
+  def issues_url(user, project) do
+    "#{@github_url}/repos/#{user}/#{project}/issues"
+  end
+
+  def handle_response({ :ok, %{status_code: 200, body: body}}) do
+    { :ok, Poison.Parser.parse!(body) }
+  end
+
+  def handle_response({ _,   %{status_code: _,   body: body}}) do
+    { :error, Poison.Parser.parse!(body) }
+  end
+
+end
+
