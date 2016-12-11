ca02f237ca1b485df47cd0fe56d0e855268ef2c2
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-12-11 03:07:34 +0000

    elixir book issues program added Logging

--- bin/exercism/issues/lib/issues/github_issues.ex
+++ bin/exercism/issues/lib/issues/github_issues.ex
@@ -4,7 +4,11 @@ defmodule Issues.GithubIssues do
   # use a module attribute to fetch the value at compile time
   @github_url Application.get_env(:issues, :github_url)
 
+  require Logger
+
   def fetch(user, project) do
+    Logger.info "Fetching user #{user}'s project #{project}"
+
     issues_url(user, project)
     |> HTTPoison.get(@user_agent)
     |> handle_response
@@ -15,10 +19,15 @@ defmodule Issues.GithubIssues do
   end
 
   def handle_response({ :ok, %{status_code: 200, body: body}}) do
+    Logger.info "Successful reponse"
+    Logger.debug fn ->inspect (body) end
+
     { :ok, Poison.Parser.parse!(body) }
   end
 
-  def handle_response({ _,   %{status_code: _,   body: body}}) do
+  def handle_response({ _,   %{status_code: status,   body: body}}) do
+    Logger.error "Error #{status} returned"
+
     { :error, Poison.Parser.parse!(body) }
   end
 
