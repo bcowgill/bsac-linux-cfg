07ec6fb380f0741e9053157996f0b89b53ffe4b0
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-12-11 18:00:34 +0000

    exercism elixir scrabble implemented

--- bin/exercism/elixir/scrabble-score/scrabble.exs
+++ bin/exercism/elixir/scrabble-score/scrabble.exs
@@ -1,9 +1,73 @@
 defmodule Scrabble do
+  @number ~r{\d}
+  @letter ~r{([a-z])}i
+  @scores ~W[
+    1 A E I O U L N R S T
+    2 D G
+    3 B C M P
+    4 F H V W Y
+    5 K
+    8 J X
+    10 Q Z
+  ]
+
   @doc """
   Calculate the scrabble score for the word.
   """
   @spec score(String.t) :: non_neg_integer
   def score(word) do
-    0
+    with values = get_values(%{}, @scores) do
+      word
+      |> get_letters
+      |> compute_score(values)
+    end
   end
+
+  @doc """
+  Get the letters of the word so we can score them
+  """
+  @spec get_letters(String.t) :: [String.t]
+  def get_letters(word) do
+    Regex.scan(@letter, word, capture: :all_but_first, trim: true)
+    |> List.flatten
+  end
+
+  @doc """
+  Compute the score from list of letters
+  """
+  @spec compute_score(String.t, Map.t) :: non_neg_integer
+  def compute_score(letters, values) do
+      letters
+      |> Enum.reduce(0, fn letter, score -> score + score_this(values, letter) end)
+  end
+
+  @doc """
+  Score the value of a single letter
+  """
+  @spec score_this(Map.t, String.t) :: non_neg_integer
+  def score_this(values, letter) do
+    Map.get(values, letter, 0)
+  end
+
+  @doc """
+  Get the map of scrabble values by letter.
+  """
+  @spec get_values(Map.t, [String.t]) :: Map.t
+  def get_values(map, [ value | tail ]) do
+    put_value(map, String.to_integer(value), tail)
+  end
+
+  # convert list of value, letters into a Map of letter => value
+  defp put_value(map, value, [letter | tail]) do
+    if (Regex.match?(@number, letter)) do
+      map
+      |> put_value(String.to_integer(letter), tail)
+    else
+      map
+      |> Map.put(letter, value)
+      |> Map.put(String.downcase(letter), value)
+      |> put_value(value, tail)
+    end
+  end
+  defp put_value(map, _, []), do: map
 end
--- bin/exercism/elixir/scrabble-score/scrabble_score_test.exs
+++ bin/exercism/elixir/scrabble-score/scrabble_score_test.exs
@@ -19,32 +19,32 @@ defmodule ScrabbleScoreTest do
     assert Scrabble.score(" \t\n") == 0
   end
 
-  @tag :pending
+  @tag :done
   test "scores very short word" do
     assert Scrabble.score("a") == 1
   end
 
-  @tag :pending
+  @tag :done
   test "scores other very short word" do
     assert Scrabble.score("f") == 4
   end
 
-  @tag :pending
+  @tag :done
   test "simple word scores the number of letters" do
     assert Scrabble.score("street") == 6
   end
 
-  @tag :pending
+  @tag :done
   test "complicated word scores more" do
     assert Scrabble.score("quirky") == 22
   end
 
-  @tag :pending
+  @tag :done
   test "scores are case insensitive" do
     assert Scrabble.score("OXYPHENBUTAZONE") == 41
   end
 
-  @tag :pending
+  @tag :done
   test "convenient scoring" do
     assert Scrabble.score("alacrity") == 13
   end
