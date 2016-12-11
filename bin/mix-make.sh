7dd0d1be7cd9eee9e5541cce1a5567e03ef1c5ec
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-12-11 11:21:38 +0000

    mix-make.sh run tests with coverage and show where to view it.

--- bin/mix-make.sh
+++ bin/mix-make.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # run all mix tasks to make project up to date.
 mix deps.get \
-&& mix test \
+&& mix test --cover \
 && mix escript.build \
 && mix docs
+
+rm cover/index.html 2> /dev/null
+ln -s `ls cover/ | head -1` cover/index.html
+echo View coverage results at cover/
 
 # mix run -e "Issues.default()"
 # mix profile.fprof -e "Issues.default()"
