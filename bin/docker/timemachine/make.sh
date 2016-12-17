6dee2020a8a0aa64f85a9c802a648c2d7b70a83b
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-12-17 18:14:45 +0000

    docker make.sh updated with ability to push when given an image ID

--- bin/docker/timemachine/make.sh
+++ bin/docker/timemachine/make.sh
@@ -4,14 +4,15 @@ HUBNAME=zardozcs/$NAME
 
 cp /home/me/workspace/play/timemachine/go.js .
 
-sudo docker build -t $NAME . \
-&& sudo docker run $NAME \
-&& sudo docker run -it $NAME bash
-
-# pushing the final build to docker hub if an image hash given
-sudo docker images | grep $NAME
 if [ ! -z $1 ]; then
+	# pushing the final build to docker hub if an image hash given
 	sudo docker tag $1 $HUBNAME
 	sudo docker login
 	sudo docker push $HUBNAME
+else
+	sudo docker build -t $NAME . \
+	&& sudo docker run $NAME \
+	&& sudo docker run -it $NAME bash
+
+	sudo docker images | grep $NAME
 fi
--- bin/docker/timezone/america-vancouver/make.sh
+++ bin/docker/timezone/america-vancouver/make.sh
@@ -2,14 +2,15 @@ ZONE=america-vancouver
 NAME=timezone-$ZONE
 HUBNAME=zardozcs/$NAME
 
-sudo docker build -t $NAME . \
-&& sudo docker run $NAME \
-&& sudo docker run -it $NAME bash
-
-# pushing the final build to docker hub if an image hash given
-sudo docker images | grep $NAME
 if [ ! -z $1 ]; then
+	# pushing the final build to docker hub if an image hash given
 	sudo docker tag $1 $HUBNAME
 	sudo docker login
 	sudo docker push $HUBNAME
+else
+	sudo docker build -t $NAME . \
+	&& sudo docker run $NAME \
+	&& sudo docker run -it $NAME bash
+
+	sudo docker images | grep $NAME
 fi
