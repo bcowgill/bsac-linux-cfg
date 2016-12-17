400c8e5583aed863941ed498d77affbdbf95024b
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-12-17 22:34:02 +0000

    vmtest dockerfile completed, example of every Docker instruction.

--- bin/docker/vmtest/make.sh
+++ bin/docker/vmtest/make.sh
@@ -1,7 +1,9 @@
 NAME=vmtest
+VOLUME="--volume `pwd`/mountintovm:/mount/vmtest"
+#VOLUME=
 
-sudo docker build -t $NAME . \
-&& sudo docker run --rm $NAME \
-&& sudo docker run -it --rm --entrypoint bash $NAME
+sudo docker build --build-arg greets="hola!" --tag $NAME . \
+&& sudo docker run --rm $VOLUME $NAME \
+&& sudo docker run --interactive --tty --rm $VOLUME --entrypoint bash $NAME
 
 sudo docker images | grep $NAME
