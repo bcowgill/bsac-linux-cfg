109412f115c832bb9fb4125cae3b56a8750c3b6c
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-12-17 16:55:33 +0000

    docker make.sh files detail how to push your image to docker hub

--- bin/docker/timemachine/make.sh
+++ bin/docker/timemachine/make.sh
@@ -3,3 +3,8 @@ cp /home/me/workspace/play/timemachine/go.js .
 sudo docker build -t timemachine-$ZONE . \
 && sudo docker run timemachine-$ZONE \
 && sudo docker run -it timemachine-$ZONE bash
+# pushing the final build to docker hub
+sudo docker images | grep timemachine-$ZONE
+# sudo docker tag [IMAGE ID] zardozcs/timemachine-$ZONE
+# sudo docker login
+# sudo docker push [IMAGE ID]
--- bin/docker/timezone/europe-london/make.sh
+++ bin/docker/timezone/europe-london/make.sh
@@ -2,3 +2,9 @@ ZONE=europe-london
 sudo docker build -t timezone-$ZONE . \
 && sudo docker run timezone-$ZONE \
 && sudo docker run -it timezone-$ZONE bash
+
+# pushing the final build to docker hub
+sudo docker images | grep timezone-$ZONE
+# sudo docker tag [IMAGE ID] zardozcs/timezone-$ZONE
+# sudo docker login
+# sudo docker push [IMAGE ID]
