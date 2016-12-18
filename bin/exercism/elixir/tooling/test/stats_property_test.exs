5d119657cb2bc01aa77cef446cfc7a0eb815c300
Brent S.A. Cowgill <zardoz@infoserve.net>
2016-12-18 21:35:07 +0000

    elixir book tooling excheck property testing

--- bin/exercism/elixir/tooling/test/stats_property_test.exs
+++ bin/exercism/elixir/tooling/test/stats_property_test.exs
@@ -7,11 +7,46 @@ defmodule StatsPropertyTest do
   use ExUnit.Case
   use ExCheck
 
+  @epsilon 1.0e-6
+
   describe "list of integer" do
+
+    property "count not negative" do
+      for_all int_list in list(int), do: Stats.count(int_list) >= 0
+    end
+
     property "single element lists are their own sum" do
       for_all number in real do
         Stats.sum([number]) == number
       end
     end
+
+    # implies will show a red X for tests which violate implies
+    property "sum equals average times count (implies)" do
+      for_all int_list in list(int) do
+        implies length(int_list) > 0 do
+          close_to(
+            Stats.sum(int_list),
+            Stats.count(int_list) * Stats.average(int_list),
+            @epsilon)
+        end
+      end
+    end
+
+    # such that will never run tests which violate condition
+    property "sum equals average times count (such_that)" do
+      for_all int_list in such_that(int_list in list(int) when length(int_list) > 0) do
+          close_to(
+            Stats.sum(int_list),
+            Stats.count(int_list) * Stats.average(int_list),
+            @epsilon)
+      end
+    end
+
   end
+
+  defp close_to(actual, expect, epsilon) do
+    abs(actual - expect) < epsilon
+  end
+
 end
--- bin/template/elixir/elixir_property_test.exs
+++ bin/template/elixir/elixir_property_test.exs
@@ -15,11 +15,42 @@ defmodule StatsPropertyTest do
   use ExUnit.Case
   use ExCheck
 
+  @epsilon 1e-6
+
   describe "list of integer" do
+
     property "single element lists are their own sum" do
       for_all number in real do
-        Stats.sum([number]) == number
+        implies number > 0 do
+          Stats.sum([number]) == number
+        end
       end
     end
+
+    # implies will show a red X for tests which violate implies
+    property "sum equals average times count (implies)" do
+      for_all int_list in list(int) do
+        close_to(
+          Stats.sum(int_list),
+          Stats.count(int_list) * Stats.average(int_list),
+          @epsilon)
+      end
+    end
+
+    # such that will never run tests which violate condition
+    property "sum equals average times count (such_that)" do
+      for_all int_list in such_that(int_list in list(int) when length(int_list) > 0) do
+          close_to(
+            Stats.sum(int_list),
+            Stats.count(int_list) * Stats.average(int_list),
+            @epsilon)
+      end
+    end
+
   end
+
+  defp close_to(actual, expect, epsilon) do
+    abs(actual - expect) < epsilon
+  end
+
 end
