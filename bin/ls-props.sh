d8c80a6baecf4d81412804093f88aa91fd9e1586
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2017-01-12 15:47:09 +0000

    ls-props.sh assume app/components/F/F.js as file name

--- bin/ls-props.sh
+++ bin/ls-props.sh
@@ -1,15 +1,21 @@
 # list props/state/types/context definitions/uses from react components
-perl -ne '
-s{
-	(types|props|state|context)\.(\w+)
-}{
-	my $type = $1 eq "types" ? "PropTypes" : $1;
-	$Found{$2}{$type} = 1
-}xmsge;
+for file in $*
+do
+	if [ ! -e $file ]; then
+		file=app/components/$file/$file.js
+	fi
+	perl -ne '
+	s{
+		(types|props|state|context)\.(\w+)
+	}{
+		my $type = $1 eq "types" ? "PropTypes" : $1;
+		$Found{$2}{$type} = 1
+	}xmsge;
 
-END {
-	print join("\n", map {
-		".$_\t" . join(",", sort(keys %{$Found{$_}}))
-	} sort keys %Found)
-}
-' $*
+	END {
+		print join("\n", map {
+			".$_\t" . join(",", sort(keys %{$Found{$_}}))
+		} sort keys %Found) . "\n"
+	}
+	' $file
+done
