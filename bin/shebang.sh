62bfb617004a7a6d7423b32f4ef3873c9aafc9b6
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2017-01-12 14:06:52 +0000

    shebang.sh script to add #! lines to a bunch of files if needed

---
+++ bin/shebang.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# add shebang line to file types
+
+EXT=${1:-*.exs}
+SHEBANG="${2:-/usr/local/bin/elixir -r}"
+
+for f in $EXT
+do
+	chmod +x $f
+	SHEBANG="$SHEBANG" perl -i.bak -pne '
+	if (!$body) {
+		$body = 1;
+		unless (m{\A\#!}xms) {
+			s{\A}{\#!$ENV{SHEBANG}\n}xms
+		}
+	}
+	' $f
+	echo $f: `head -1 $f`
+done
