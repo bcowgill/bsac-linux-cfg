0de2d906498a116f5446f722bd545fe17ef75743
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-01-12 20:41:43 +0000

    elixir book example fibonacci calcuator server made Scheduler generic so it can be a module

--- bin/exercism/elixir/spawn/fib.exs
+++ bin/exercism/elixir/spawn/fib.exs
@@ -23,26 +23,26 @@ defmodule Scheduler do
   def run(num_processes, module, func, to_calculate) do
     (1..num_processes)
     |> Enum.map(fn(_) -> spawn(module, func, [self]) end)
-    |> schedule_processes(to_calculate, [])
+    |> schedule_processes(func, to_calculate, [])
   end
 
-  defp schedule_processes(processes, queue, results) do
+  defp schedule_processes(processes, token, queue, results) do
     receive do
       {:ready, pid} when length(queue) > 0 ->
         [ next | tail ] = queue
-        send pid, {:fib, next, self}
-        schedule_processes(processes, tail, results)
+        send pid, {token, next, self}
+        schedule_processes(processes, token, tail, results)
 
       {:ready, pid} ->
         send pid, {:shutdown}
         if length(processes) > 1 do
-          schedule_processes(List.delete(processes, pid), queue, results)
+          schedule_processes(List.delete(processes, pid), token, queue, results)
         else
           Enum.sort(results, fn {n1,_}, {n2,_}  -> n1 <= n2 end)
         end
 
       {:answer, number, result, _pid} ->
-        schedule_processes(processes, queue, [ {number, result} | results ])
+        schedule_processes(processes, token, queue, [ {number, result} | results ])
     end
   end
 end
