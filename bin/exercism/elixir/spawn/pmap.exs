7ba003c58f937d0fd822d1e75d23ec36752375b8
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-01-12 19:57:40 +0000

    elixir book exercise pmap

--- bin/exercism/elixir/spawn/pmap.exs
+++ bin/exercism/elixir/spawn/pmap.exs
@@ -1,14 +1,30 @@
 #!/usr/local/bin/elixir -r
 
 defmodule Parallel do
+  #import :timer, only: [ sleep: 1 ]
+
   def pmap(collection, fun) do
     me = self
     collection
     |> Enum.map(fn (elem) ->
-         spawn_link fn -> (send me, { self, fun.(elem) }) end
+         spawn_link fn -> (sleepy_send me, { self, fun.(elem) }) end
        end)
     |> Enum.map(fn (pid) ->
          receive do { ^pid, result } -> result end
        end)
   end
+
+  defp sleepy_send(pid, message) do
+    #sleep random(100..500)
+    send pid, message
+  end
+
+  defp random(min..max)
+  do
+    range = max - min + 1
+    :rand.uniform(range) + min - 1
+  end
+
 end
+
+IO.puts inspect Parallel.pmap 1..10, &(&1 * &1)
