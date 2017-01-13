57b2a2d49c703672389429d6b62011cd65d7d5a9
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2017-01-13 16:51:03 +0000

    elixir erl_help.exs to give rudimentary erlang help in your iex sessions

--- bin/exercism/elixir/my-times/erl_help.exs
+++ bin/exercism/elixir/my-times/erl_help.exs
@@ -1,11 +1,15 @@
-# Get some help on an erlang module
+# Provide quick access to help pages on erlang functions
 
-# Erl.h :io
-# s :io.columns
+# Erl.h :io        shows all :io methods with direct url to erlang help
+# s :io.columns    shows @spec for specific function call
 defmodule Erl do
+  @url "http://erlang.org/doc/man/"
   def h(module) do
+    help = "#{@url}#{module}.html"
+    IO.puts help
     module.module_info(:exports)
-    |> Enum.map( fn {func, arity} -> ":#{module}.#{func}/#{arity}" end )
+    |> Enum.map( fn {func, arity} -> ":#{module}.#{func}/#{arity}\t\t#{help}##{func}-#{arity}" end )
     |> Enum.sort( &(&1 < &2) )
+    |> Enum.each( fn func -> IO.puts "#{func}" end )
   end
 end
