71aa2acd362d72e6f9ec366c29439e7cbd05a5e9
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2017-01-16 18:06:33 +0000

    elixir exercise scheduler cat.exs find_files recursively

--- bin/exercism/elixir/spawn/cat.exs
+++ bin/exercism/elixir/spawn/cat.exs
@@ -15,14 +15,34 @@ defmodule CatFinder do
     end
   end
 
+  def find_files(dir) do
+    do_find_files(dir)
+  end
+
+  defp do_find_files(dir, result \\ [])
+
+  defp do_find_files(dir, result) do
+    found = File.ls!(dir)
+	  |> Enum.map(fn file -> recurse(dir, file) end)
+      |> Enum.filter(fn file -> ! File.dir? file end)
+
+    List.flatten(result ++ found)
+  end
+
+  defp recurse(dir, file) do
+    full = dir <> "/" <> file
+    cond do
+	  File.regular? full -> full
+	  File.dir? full -> find_files(full)
+	end
+  end
+
   defp cat_find(file_name) do
     length(Regex.scan(~r/\bcat\b/i, File.read!(file_name)))
   end
 end
 
-dir = "/home/me/bin/english/"
+dir = "/home/me/bin/english"
 
-to_process = File.ls!(dir)
-  |> Enum.map(fn file -> dir <> file end)
-  |> Enum.filter(fn file -> ! File.dir? file end)
+to_process = CatFinder.find_files(dir)
 Scheduler.benchmark(10, CatFinder, :cat, to_process)
