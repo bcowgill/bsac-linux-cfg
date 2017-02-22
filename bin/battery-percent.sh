93d7fb038db67733e5316838e4e0797ff6d3c71a
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-02-22 21:10:43 +0000

    battery-percent.sh fix

--- bin/battery-percent.sh
+++ bin/battery-percent.sh
@@ -1,6 +1,6 @@
 #upower --show-info /org/freedesktop/UPower/devices/battery_BAT1 \
 for power in `upower --enumerate`; do
-	upower --show-info $power | perl -ne 's{\s+}{ }xmsg; print if m{BAT|percent|capacity}xms' \
+	upower --show-info $power | perl -ne 's{\s+}{ }xmsg; print qq{$_\n} if m{BAT|percent|capacity}xms' \
 		| perl -ne 'chomp; if (s{\A\s*(percentage:\s*)}{}xms) { $_ .= "🔋 "; print }'
 done
 echo " "
