a7e61f36c84517d56b8188c17d56629c00a0d6fa
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-02-26 17:48:34 +0000

    filter-script.pl filters the output of the script command (tuned to elixir iex output)

---
+++ bin/cfg/.iex.exs
@@ -0,0 +1,17 @@
+IEx.configure colors: [ eval_result: [ :cyan, :bright ] ]
+
+# Provide quick access to help pages on erlang functions
+
+# Erl.h :io        shows all :io methods with direct url to erlang help
+# s :io.columns    shows @spec for specific function call
+defmodule Erl do
+  @url "http://erlang.org/doc/man/"
+  def h(module) do
+    help = "#{@url}#{module}.html"
+    IO.puts help
+    module.module_info(:exports)
+    |> Enum.map( fn {func, arity} -> ":#{module}.#{func}/#{arity}\t\t#{help}##{func}-#{arity}" end )
+    |> Enum.sort( &(&1 < &2) )
+    |> Enum.each( fn func -> IO.puts "#{func}" end )
+  end
+end
---
+++ bin/filter-script.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/env perl
+# filter the output of the script command to remove color control codes etc
+
+use English qw(-no_match_vars);
+use utf8;      # so literals and identifiers can be in UTF-8
+use v5.12;     # or later to get "unicode_strings" feature
+use strict;    # quote strings, declare variables
+use warnings;  # on by default
+use warnings  qw(FATAL utf8);    # fatalize encoding glitches
+use open      qw(:std :utf8);    # undeclared streams in UTF-8
+use charnames qw(:full :short);  # unneeded in v5.16
+use Unicode::Normalize; # to check normalisation
+
+# Filter out ANSI color change control sequences
+my $esc = "\N{U+001B}"; # ^[ = escape
+my $ctrlg = "\N{U+0007}" ; # ^G = tab
+my $bs = "\N{U+0008}"; # ^H = backspace
+my $exfn = qr{\b\w+[\?\!]?(?:/\d+)?}xms;
+
+while (my $line = <>) {
+	$line = NFD($line);   # decompose + reorder canonically
+	chomp($line);
+
+	#debugUTF8($line);
+
+	# cursor jumping on the prompt
+	$line =~ s{$esc\]0; (.+) $ctrlg .+ $esc\[00m}{$1}xmsg;
+	# ANSI control sequences for color
+	$line =~ s{$esc\[\d+m}{}xmsg;
+	$line =~ s{$esc\[C}{}xmsg;
+
+	# handle backspacing by deleting character
+	$line =~ s{$ctrlg+}{}xmsg;
+	while ($line =~ s{[^$bs] $bs}{}xmsg) {}
+
+	$line =~ s{\A iex \(\d+\)>}{\niex(n)>}xmsg;
+	# iex list of functions
+	$line =~ s{$ctrlg+}{}xmsg;
+	$line =~ s{\A ($exfn) \s+ ($exfn) \s+ ($exfn) \s+ ($exfn) \s+ ($exfn) \s* \z}{$1\n$2\n$3\n$4\n$5\n}xmsg;
+	$line =~ s{\A ($exfn) \s+ ($exfn) \s+ ($exfn) \s+ ($exfn) \s* \z}{$1\n$2\n$3\n$4\n}xmsg;
+	$line =~ s{\A ($exfn) \s+ ($exfn) \s+ ($exfn) \s* \z}{$1\n$2\n$3\n}xmsg;
+	$line =~ s{\A ($exfn) \s+ ($exfn) \s* \z}{$1\n$2\n}xmsg;
+
+	$line =~ s{\s+\z}{}xms;
+
+	$line .= "\n";
+} continue {
+	print NFC($line);  # recompose (where possible) + reorder canonically
+}
+
+sub debugUTF8 {
+	my ($canonical) = @ARG;
+
+	# http://www.regular-expressions.info/posixbrackets.html
+	foreach my $class ( qw(alnum alpha ascii blank cntrl digit graph lower
+		print punct space upper word xdigit ) )
+	{
+		print "line below matches [[:$class:]] ? " . (($canonical =~ m{[[:$class:]]}xms) ? 1: 0) . "\n";
+	}
+}
