0de0f89672b3de4d6bb07a1544e83d145aa5bf89
Brent S.A. Cowgill <brent.cowgill@workshare.com>
2017-03-10 09:31:09 +0000

    began work on ls-types.sh to list file types found within directory.

---
+++ bin/ls-types.sh
@@ -0,0 +1,8 @@
+# List interesting file types within directories.
+# Work in progress.
+# want something like:
+
+# ./documents 44 docs; 13 images;
+# docs === .doc .docx  images === .jpg .gif, etc
+find . -type f | perl -pne 'chomp; $_ =~ m{\A (.*? /) ([^/]+) \z}xms or die qq{no match for path/filename: [$_]\n}; my ($path, $filename) = ($1, $2); $_ = "$path $filename\n"'
+
