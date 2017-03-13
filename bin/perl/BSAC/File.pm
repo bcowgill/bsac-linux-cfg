d5c445d94b9b3f39670560f0c911f34e856c8134
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-03-13 20:30:50 +0000

    ls-types.pl BSAC::File created, save ext and description of files we scan

---
+++ bin/perl/BSAC/File.pm
@@ -0,0 +1,209 @@
+package BSAC::File;
+
+=head1 NAME
+
+BSAC::File - Provides access to all properties of a file in one place.
+
+=head1 SYNOPSIS
+
+A quick overview of using the module in the common case.
+
+	use BSAC::File;
+
+	# something class wide
+	BSAC::File->HANDLES;
+	$obj = BSAC::File->new('filename');
+	$obj->accessor('filename'); # => 'filename'
+	$obj->mutator('newFileName');
+
+=head1 DESCRIPTION
+
+C<BSAC::File> is a full on description of the module.
+
+Use podchecker to check pod syntax for your module.
+
+=head1 EXAMPLES
+
+=cut
+
+{ use 5.006; }
+use strict;
+use warnings;
+use Carp;
+use English -no_match_vars;
+use Cwd;
+use File::Spec;
+
+our $VERSION = '1.00';
+
+require Exporter;
+our @ISA = ('Exporter');
+our @EXPORT = qw();
+our @EXPORT_OK = qw();
+our @EXPORT_FAIL = qw();
+
+=head1 METHODS
+
+=over
+
+=item CONSTRUCTOR
+
+new ($FILENAME)
+
+Creates an C<BSAC::File> from the given $FILENAME.
+
+=cut
+
+sub new
+{
+	my $type = shift;
+	my $class = ref($type) || $type || __PACKAGE__;
+	@ARG == 1
+		or croak "usage: $class->new(FILENAME)";
+
+	my $full_file_path = shift;
+	my $is_absolute = File::Spec->file_name_is_absolute($full_file_path);
+	my ($volume, $directories, $filename) = File::Spec->splitpath($full_file_path);
+	my ($volume2, $directories2, $filename2) = File::Spec->splitpath($full_file_path, 'no file');
+	my ($name_part, $extension) = $class->split_extension($filename);
+
+	my $self = {
+		original_path => $full_file_path,
+		volume => $volume,
+		directories => $directories,
+        all_directories => $directories2,
+		filename => $filename,
+		no_filename => $filename2,
+		name_part => $name_part,
+		extension => $extension,
+		relative_path => File::Spec->abs2rel($full_file_path),
+		absolute_path => File::Spec->rel2abs($full_file_path),
+        real_path => Cwd::realpath($full_file_path),
+		canonpath => File::Spec->canonpath($full_file_path),
+		no_upwards => File::Spec->no_upwards($full_file_path),
+		filename_is_absolute => $is_absolute,
+		curdir => Cwd::cwd(),
+		sys_curdir => File::Spec->curdir(),
+		sys_devnull => File::Spec->devnull(),
+		sys_rootdir => File::Spec->rootdir(),
+		sys_tmpdir => File::Spec->tmpdir(),
+		sys_updir => File::Spec->updir(),
+		sys_case_tolerant => File::Spec->case_tolerant(),
+		sys_path => [File::Spec->path()],
+	};
+
+	return bless($self, $class);
+}
+
+sub split_extension
+{
+	my $type = shift;
+	my $class = ref($type) || $type || __PACKAGE__;
+	@ARG == 1
+		or croak "usage: $class->split_extension(FILENAME)";
+
+	my ($filename) = shift;
+	my $prefix = '';
+	if ($filename =~ s{ \A \. }{}xms)
+	{
+		$prefix = '.';
+	}
+	my ($name_part, $extension) = split(/\./, $filename, 2);
+	return ($prefix . $name_part, $extension);
+}
+
+#catdir
+#catfile,join
+#catpath
+#abs2rel $base
+
+=item IMMUTABLE ACCESSORS
+
+=over 4
+
+=item accessor($FIELD)
+
+method is a property accessor.
+
+=cut
+
+sub accessor
+{
+	my ($self, $field) = @ARG;
+	my $class = ref($self) || $self || __PACKAGE__;
+	@ARG == 2
+		or croak "usage: \$obj@{[__PACKAGE__]}->accessor(FIELD)";
+	return $self->{$field};
+}
+
+=back
+
+=item MUTATORS
+
+=over 4
+
+=item mutator($VALUE)
+
+method is a class mutator.
+
+=cut
+
+sub mutator
+{
+	my ($self, $value) = @ARG;
+	my $class = ref($self) || $self || __PACKAGE__;
+	@ARG == 2
+		or croak "usage: \$obj@{[__PACKAGE__]}->mutator(VALUE)";
+	$self->{filename} = $value;
+	return $self;
+}
+
+=back
+
+=back
+
+=cut
+
+#===========================================================================
+# PRIVATE METHODS
+
+
+
+=head1 BUGS
+
+Any known bugs.
+
+=head1 CAVEATS
+
+List any gotchas.
+
+=head1 TO DO
+
+=over
+
+=item * What's left to do.
+
+=back
+
+
+=head1 AUTHOR
+
+Brent S.A. Cowgill
+
+=head1 COPYRIGHT and LICENSE
+
+Copyright (c) Brent S.A. Cowgill. All rights reserved.
+
+This library is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+See L<"BUGS"> to link to another part of the pod.
+
+Perl module style guide.
+L<http://perldoc.perl.org/perlmodstyle.html#NAME>
+
+=cut
+
+1;
--- bin/perl/BSAC/FileTypes.pm
+++ bin/perl/BSAC/FileTypes.pm
@@ -7,8 +7,10 @@ use warnings;
 use Carp;
 use English -no_match_vars;
 use Data::Dumper;
-
+use File::MimeInfo qw();
+#use File::Type;
 use BSAC::FileTypesFound qw(save_extension_description);
+use BSAC::File;
 
 our $VERSION = '1.00';
 
@@ -17,6 +19,9 @@ our @ISA = ('Exporter');
 our @EXPORT = qw();
 our @EXPORT_OK = qw(check_path);
 our @EXPORT_FAIL = qw();
+
+#my $fileTypes = File::Type->new();
+my $mimeInfo = File::MimeInfo->new();
 
 my %FileTypes = (
 	'documents' => [qw(doc docx dot dotm dotx xls xlsx xltx ppt ppd pptx odf odt ods odp rtf pdf eps ps)],
@@ -39,50 +44,84 @@ my %FileTypes = (
 
 my %FileTypeRegex;
 
-sub init {
+sub init
+{
 	%FileTypeRegex = map {
 		($ARG, list_to_regex($FileTypes{$ARG}))
 	} keys(%FileTypes);
 }
 
-sub get_types {
+sub get_types
+{
 	return keys(%FileTypes);
 }
 
 #save_extension_description('csv', 'text with comma separated values');
 #save_extension_description('TXT', 'ascii text with CR/LF line endings');
 
-sub check_path {
+sub check_path
+{
 	my ($path, $rhCounts) = @ARG;
 	$rhCounts ||= {};
 	my @Types = grep {
 		$path =~ $FileTypeRegex{$ARG}
 	} keys(%FileTypeRegex);
+	save_extension($path);
 	return count_matches($path, \@Types, $rhCounts);
 }
 
-sub count_matches {
+sub save_extension
+{
+	my ($path) = @ARG;
+	my $File = BSAC::File->new($path);
+	my $description = file_description($path);
+	save_extension_description($File->{extension}, $description);
+}
+
+sub file_description
+{
+	my ($path) = @ARG;
+
+	my $description = '';
+	if (-r $path) {
+		$description = `file "$path"`;
+		chomp $description;
+		$description =~ s{\A [^:]+ :}{}xms;
+		$description = ' ' . $description if length($description);
+	}
+	else {
+		carp "$path is unreachable, cannot determine file type";
+	}
+	return $mimeInfo->mimetype($path) . $description;
+}
+
+sub count_matches
+{
 	my ($path, $raTypes, $rhCounts) = @ARG;
 	$path =~ s{/([^/]+)\z}{/}xms;
 	my $file = $1;
-	foreach my $type (@$raTypes) {
+	foreach my $type (@$raTypes)
+{
 		push(@{$rhCounts->{$path}{$type}}, $file);
 	}
 	return @$raTypes;
 }
 
-sub list_to_regex {
+sub list_to_regex
+{
 	my $raList = shift;
 	my $regex = list_to_regex_string($raList);
 	return qr{$regex}xmsi;
 }
 
-sub list_to_regex_string {
+sub list_to_regex_string
+{
 	my $raList = shift;
 	return '\.(' . join('|', map { quotemeta($ARG) } @$raList) . ')\z';
 }
 
-sub trace {
+sub trace
+{
 	print Dumper(\@ARG);
 	return @ARG
 }
