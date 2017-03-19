cd3a1614ebc6b988bafe7c044ba624fcbe7ea93c
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-03-19 14:42:54 +0000

    mocha-dark added a mocha-test.html for running tests in browser

---
+++ bin/mocha-dark/test/mocha-test-require.js
@@ -0,0 +1,40 @@
+requirejs.config({
+    // By default load any module IDs from src/
+    baseUrl: 'src',
+
+    // except, if the module ID starts with "app",
+    // load it from the js/app directory. paths
+    // config is relative to the baseUrl, and
+    // never includes a ".js" extension since
+    // the paths config could be for a directory.
+    paths: {
+        mocha: '../node_modules/mocha/mocha',
+        chai: '../node_modules/chai/chai'
+    },
+
+	shim: {
+		mocha: {
+			exports: 'mocha'
+		}
+	}
+});
+
+// Start the tests after loading testing libraries
+requirejs(['require', 'mocha', 'chai'],
+function (require, mocha, chai) {
+    // mocha, chai module are all
+    // loaded and can be used here now.
+
+	mocha.setup('bdd');
+
+	// require test plans here.
+	require([
+		'./test/a-first-test.test.js'
+//		'./src/object.js',
+//		'./src/object.spec.js'
+	], function () {
+		mocha.checkLeaks();
+		mocha.globals(['jQuery']);
+		mocha.run();
+	});
+});
