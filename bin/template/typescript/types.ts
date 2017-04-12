e8f1b5836b738a7ccc5ff070959af66078bd257f
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-04-12 07:20:13 +0100

    typescript templates updated tsconfig.json5

--- bin/template/typescript/types.ts
+++ bin/template/typescript/types.ts
@@ -2,9 +2,9 @@
 	types.ts defines some useful types
 	tsc --project tsconfig.json5
 */
-declare BooleanArray Array<boolean>;
-declare NumberArray Array<number>;
-declare StringArray Array<string>;
+//declare BooleanArray Array<boolean>;
+//declare NumberArray Array<number>;
+//declare StringArray Array<string>;
 
 enum Ordinal { MINUS_TWO = -2, MINUS_ONE, ZERO, ONE, TWO };
 let ord : Ordinal = Ordinal.ONE;
