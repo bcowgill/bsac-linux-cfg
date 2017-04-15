9ba1f760c20e0b8a22552a83cf524ff298c1c8f0
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-04-15 07:37:49 +0100

    typescript templates for class interfaces

--- bin/template/typescript/class-interface.ts
+++ bin/template/typescript/class-interface.ts
@@ -67,3 +67,74 @@ class AnalogClock implements ClockInterface3 {
 let digital = createClock(DigitalClock, 12, 17);
 let analog = createClock(AnalogClock, 7, 32);
 
+// Extending multiple interfaces
+interface Shape {
+    color: string;
+}
+
+interface Square extends Shape {
+    sideLength: number;
+}
+
+let square = <Square>{};
+square.color = "blue";
+square.sideLength = 10;
+
+interface PenStroke {
+    penWidth: number;
+}
+
+interface Square2 extends Shape, PenStroke {
+    sideLength: number;
+}
+
+let square2 = <Square2>{};
+square2.color = "blue";
+square2.sideLength = 10;
+square2.penWidth = 5.0;
+
+// Functions with properties
+interface Counter {
+    (start: number): string;
+    interval: number;
+    reset(): void;
+}
+
+function getCounter(): Counter {
+    let counter = <Counter>function (start: number) { };
+    counter.interval = 123;
+    counter.reset = function () { };
+    return counter;
+}
+
+let c = getCounter();
+c(10);
+c.reset();
+c.interval = 5.0;
+
+// Class inheritance of private properties
+class Control {
+    private state: any;
+}
+
+interface SelectableControl extends Control {
+    select(): void;
+}
+
+class Button extends Control {
+    select() { }
+}
+
+class TextBox extends Control {
+    select() { }
+}
+
+// Not inheriting fron control, so will not have the private state
+class ImageNotControl {
+    select() { }
+}
+
+class LocationNotControl {
+    select() { }
+}
+
--- bin/template/typescript/make.sh
+++ bin/template/typescript/make.sh
@@ -1,3 +1,3 @@
 rm -rf dist;
-json5 -c tsconfig*.json5 && tsc --newline LF --project tsconfig-debug.json ;
+json5 -c tsconfig*.json5 && tsc --newline LF --project tsconfig.json ;
 rm *.json
