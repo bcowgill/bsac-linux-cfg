5eb5beabb806278fd63fdc30201ee52aecaed980
Brent S.A. Cowgill <brent@clearbooks.co.uk>
2017-04-16 18:53:33 +0100

    browse.sh supports nautilus and nemo

--- bin/browse.sh
+++ bin/browse.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 # open a gui file browser
-nautilus --no-desktop $* &
+
+if which nautilus; then
+	nautilus --no-desktop $* &
+	exit 0
+fi
+if which nemo; then
+	nemo --no-desktop $* &
+	exit 0
+fi
+echo NOT OK unable to find a file browser.
+exit 1
