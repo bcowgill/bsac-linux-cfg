5c4537e66c9b53cbcd344da23b39d72b50cdaafe
Brent S.A. Cowgill <brent@clearbooks.co.uk>
2017-04-21 11:52:08 +0100

    debug-js-on/off moved to bin dir from workshare

---
+++ bin/debug-js-off.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# debug-js-off.sh turn off debugging statements marked /*dbg:*/
+# by replacing them with //dbg:
+
+DIR=${1:-~/projects}
+
+pushd $DIR > /dev/null
+	FILES=`(git grep -lE '/\*\s*dbg:' 2> /dev/null || egrep -rl '/\*\s*dbg:') | egrep -v \.bak\$`
+	perl -i.bak -pne 's{/\* \s* dbg: \s* \*\/(\s*)}{//dbg:$1}xmsg' $FILES
+popd > /dev/null
---
+++ bin/debug-js-on.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# debug-js-on.sh turn on debugging statements marked //dbg:
+# by replacing them with /*dbg:*/
+
+DIR=${1:-~/projects}
+
+pushd $DIR > /dev/null
+	FILES=`(git grep -lE '/[/]\s*dbg:' 2> /dev/null || egrep -rl '/[/]\s*dbg:') | egrep -v \.bak\$`
+	perl -i.bak -pne 's{//\s*dbg:(\s*)}{/*dbg:*/$1}xmsg' $FILES
+popd > /dev/null
