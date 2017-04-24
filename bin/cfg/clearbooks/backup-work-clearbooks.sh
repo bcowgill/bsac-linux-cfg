c55dc1fc8353d3ebbf2d454dca5541a35df86967
Brent S.A. Cowgill <brent@clearbooks.co.uk>
2017-04-24 02:01:08 +0100

    work backup

--- bin/cfg/clearbooks/backup-work-clearbooks.sh
+++ bin/cfg/clearbooks/backup-work-clearbooks.sh
@@ -13,6 +13,13 @@ mkdir -p workspace/backup
 popd
 
 pushd ~/workspace
+pushd projects/clearbooks-micro-api-accounting
+tar cvzf $DROP/typescript-build-acct.tgz `grep -v '#' common.lst`
+popd
+
+pushd projects/clearbooks-micro-api-auth
+tar cvzf $DROP/typescript-build-auth.tgz `grep -v '#' common.lst`
+popd
 
 date
 echo Backup complete: $DROP
