2703021c53cb29dfc5adf82441c653883f8d9e49
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-04-28 23:19:20 +0100

    fix-commas.pl converts leading , to trailing ,

--- bin/fix-commas.pl
+++ bin/fix-commas.pl
@@ -8,18 +8,18 @@ use File::Slurp;
 use open IN => ':raw';
 
 my $LEADING = 0;
-my $NL = m{\n};
+my $NL = qr{\n};
 
 sub fix_commas
 {
 	my ($content) = @ARG;
 	if ($LEADING)
 	{
-		$content = fix_leading_spaces($content);
+		$content = fix_leading_commas($content);
 	}
 	else
 	{
-		$content = fix_trailing_spaces($content);
+		$content = fix_trailing_commas($content);
 	}
 	print $content;
 }
@@ -34,8 +34,69 @@ sub fix_leading_commas
 sub fix_trailing_commas
 {
 	my ($content) = @ARG;
-	print STDERR "fixing trailing commas are not yet implemented.";
-	return $content;
+	my @lines = split($NL, $content);
+	my $lines = scalar(@lines);
+	if ($lines > 1)
+	{
+		for (my $idx = 1; $idx < $lines; $idx++)
+		{
+			my ($prev, $line, $moved) = move_leading_comma_to_previous_line(
+				$lines[$idx - 1],
+				$lines[$idx]
+			);
+			$lines[$idx - 1] = $prev;
+			if ($moved)
+			{
+				$line = add_trailing_comma($line) if (($idx + 1 < $lines) && in_a_list($line, $lines[$idx + 1]));
+			}
+			$lines[$idx] = $line;
+		}
+		$content = join("\n", @lines);
+		chomp($content);
+	}
+	return "$content\n";
+}
+
+sub move_leading_comma_to_previous_line
+{
+	my ($prev, $line) = @ARG;
+	my $moved = 0;
+	if ($line =~ s{\A (\s*) , (\s*)}{$1}xms)
+	{
+		$moved = 1;
+		$prev = add_trailing_comma($prev);
+	}
+	return ($prev, $line, $moved);
+}
+
+sub add_trailing_comma
+{
+	my ($line) = @ARG;
+	unless ($line =~ s{ (\s* /\* .*? \*/ \s*) \z}{,$1}xms)
+	{
+		unless ($line =~ s{ (\s* // .*?) \z}{,$1}xms)
+		{
+			$line .= ',';
+		}
+	}
+	return $line;
+}
+
+# Does next line begin with ] ) or } and current line does not end with , ;
+sub in_a_list
+{
+	my ($line, $next) = @ARG;
+	my $in_a_list = ($next =~ m{\A \s* [\)\}\]]}xms)
+		&& !has_trailing_punctuation($line);
+	return $in_a_list;
+}
+
+sub has_trailing_punctuation
+{
+	my ($line) = @ARG;
+	my $has_trailing = ($line =~ m{ [,;] (\s* /\* .*? \*/ \s*) \z}xms
+		|| $line =~ m{ [,;] (\s* // .*?)? \z}xms);
+	return $has_trailing;
 }
 
 if (scalar(@ARGV))
--- bin/tests/fix-commas/tests.sh
+++ bin/tests/fix-commas/tests.sh
@@ -12,7 +12,7 @@ SKIP=0
 
 # Include testing library and make output dir exist
 source ../shell-test.sh
-PLAN 6
+PLAN 5
 
 [ -d out ] || mkdir out
 rm out/* > /dev/null 2>&1 || OK "output dir ready"
@@ -20,7 +20,7 @@ rm out/* > /dev/null 2>&1 || OK "output dir ready"
 # Do not terminate test plan if out/base comparison fails.
 ERROR_STOP=0
 
-echo TEST $CMD open file
+echo TEST $CMD leading commas convert - open file
 TEST=commas-leading
 if [ 0 == "$SKIP" ]; then
 	ERR=0
@@ -28,6 +28,20 @@ if [ 0 == "$SKIP" ]; then
 	BASE=base/$TEST.base
 	ARGS="$DEBUG $SAMPLE"
 	$PROGRAM $ARGS > $OUT || assertCommandSuccess $? "$PROGRAM $ARGS"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
+SAMPLE=in/commas-trailing.txt
+echo TEST $CMD trailing commas leave as is - stdin
+TEST=commas-trailing-remains
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG"
+	$PROGRAM $ARGS < $SAMPLE > $OUT || assertCommandSuccess $? "$PROGRAM $ARGS"
 	assertFilesEqual "$OUT" "$BASE" "$TEST"
 else
 	echo SKIP $TEST "$SKIP"
