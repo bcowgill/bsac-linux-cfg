53c590e5980a79be34533886bc1cc96ced66471f
Brent S.A. Cowgill <brent@clearbooks.co.uk>
2017-05-12 09:51:30 +0100

    git-rename.sh rename a file in git and fix import references

---
+++ bin/git-rename.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# rename a git file in same dir and adjust import references to it
+# git-rename.sh src/path/filenmae.js js filename
+# git-rename.sh src/path/filenmae.js js filename.js
+
+FULLPATH="$1"
+EXT="$2"
+NEWNAME="$3"
+
+if [ -z $NEWNAME ]; then
+	echo usage: $0 src/path/filenmae.js .js filename
+	echo will rename/move the file in git and correct import references to it.
+	exit 1
+fi
+
+DIR=`dirname $FULLPATH`
+FILENAME=`basename $FULLPATH`
+NAME=`basename $FILENAME $EXT`
+NEWNAME=`basename $NEWNAME $EXT`
+NEWPATH="$DIR/$NEWNAME$EXT"
+REPLACE="$NEWNAME"
+echo will replace /$NAME with /$REPLACE in source code.
+echo git mv "$FULLPATH" "$NEWPATH"
+git mv "$FULLPATH" "$NEWPATH" \
+	&& NAME="$NAME" NEWNAME="$REPLACE" perl -i.bak -pne 's{/$ENV{NAME}}{/$ENV{NEWNAME}}g' `git grep -l "/$NAME"`
