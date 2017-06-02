9be471b65d7eee3f7a6fec121beaedbe41fbac31
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-06-02 23:39:35 +0100

    typescript make config for es2015.es5

--- bin/template/typescript/make.sh
+++ bin/template/typescript/make.sh
@@ -1,17 +1,28 @@
 rm -rf dist;
-json5 -c tsconfig*.json5 && tsc --newline LF --project tsconfig.json ;
+#json5 -c tsconfig*.json5 && tsc --newline LF --project tsconfig.json ;
 #json5 -c tsconfig*.json5 && tsc --newline LF --project tsconfig-debug.json ;
 #rm *.json
 
-exit 0
 mkdir trans
+SRC=weird
 OUT=weird
 #OUT=index-debug
-MOD=amd.es6
+MOD=es2015.es5
+
+tar czf typescript.tgz *.ts modules/
+rm -rf src
+mkdir src/
+cp $SRC.ts src/
+rm -rf *.ts modules/
+
+json5 -c tsconfig*.json5 && tsc --newline LF --project tsconfig-$MOD.json ;
+
 (\
-	echo "// weird.d.ts - $MOD";\
+	echo "// $SRC.d.ts - $MOD";\
 	cat dist/$OUT.d.ts;\
 	echo " ";\
-	echo "// weird.js - $MOD";\
+	echo "// $SRC.js - $MOD";\
 	cat dist/$OUT.js\
-) > trans/weird.$MOD
+) > trans/$SRC.$MOD
+tar xzf typescript.tgz
+echo created trans/$SRC.$MOD
