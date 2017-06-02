87954ac43800b329452e8fd57735ae51bc5ba6d9
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-06-03 00:10:20 +0100

    typescript make es5,6 es2015,amd modules

--- bin/template/typescript/make.sh
+++ bin/template/typescript/make.sh
@@ -3,26 +3,46 @@ rm -rf dist;
 #json5 -c tsconfig*.json5 && tsc --newline LF --project tsconfig-debug.json ;
 #rm *.json
 
-mkdir trans
-SRC=weird
-OUT=weird
-#OUT=index-debug
-MOD=es2015.es5
+function transpile
+{
+	local modes source out
+	modes=$1
+	source=$2
+	out=${3:-$source}
 
+	echo src/$source.ts to dist/$out.js as $modes
+
+	rm -rf src && mkdir src/
+	tar xvzf typescript.tgz --directory src/ $source.ts
+
+	tsc --newline LF --project tsconfig-$modes.json
+
+	(\
+		echo "// $source.d.ts - $modes";\
+		cat dist/$out.d.ts;\
+		echo " ";\
+		echo "// $source.js - $modes";\
+		cat dist/$out.js\
+	) > trans/$source.$modes
+
+	echo created trans/$source.$modes
+}
+
+# setup for building various modules from source
 tar czf typescript.tgz *.ts modules/
-rm -rf src
-mkdir src/
-cp $SRC.ts src/
 rm -rf *.ts modules/
+json5 -c tsconfig*.json5
+[ -d trans ] || mkdir trans
 
-json5 -c tsconfig*.json5 && tsc --newline LF --project tsconfig-$MOD.json ;
+for es in es5 es6
+do
+	for mod in es2015 commonjs
+	do
+		transpile $mod.$es weird
+	done
+done
 
-(\
-	echo "// $SRC.d.ts - $MOD";\
-	cat dist/$OUT.d.ts;\
-	echo " ";\
-	echo "// $SRC.js - $MOD";\
-	cat dist/$OUT.js\
-) > trans/$SRC.$MOD
+# restore source code after
 tar xzf typescript.tgz
-echo created trans/$SRC.$MOD
+rm typescript.tgz
+rm -rf src/
