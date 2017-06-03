b32a82e7db228625fa43b41681c6d8c5980964f8
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-06-03 19:30:47 +0100

    touch config improvement

--- bin/touch-config.sh
+++ bin/touch-config.sh
@@ -6,28 +6,30 @@
 
 function get_usb_id
 {
-	local text
-	text="$1"
+	local type text
+	type="$1"
+	text="$2"
+	echo $type:
 	RET=`xinput --list | grep "$text" | perl -pne 's{\A .+ id=(\d+) .+ \z}{$1}xms;'`
 	xinput --list | grep "$text"
 }
 
 # Mouse/touch configuration default
-get_usb_id "Logitech MX Anywhere"
+get_usb_id mouse "Logitech MX Anywhere"
 export MOUSE=$RET
-get_usb_id "Logitech USB Receiver"
+get_usb_id mouse "Logitech USB Receiver"
 export MOUSE="$MOUSE $RET"
-get_usb_id "Mitsumi Electric Apple Optical USB Mouse"
+get_usb_id mouse "Mitsumi Electric Apple Optical USB Mouse"
 export MOUSE="$MOUSE $RET"
 
-get_usb_id Touchpad
+get_usb_id touch Touchpad
 export TOUCH_PAD=$RET
 export TOUCH_NIPPLE=
 export TOUCH_SCREEN=
 
 if [ $HOSTNAME == "akston" ]; then
 	# Mouse/touch configuration for my dell laptop
-	get_usb_id "ImPS/2 Generic Wheel Mouse"
+	get_usb_id touch/nipple "ImPS/2 Generic Wheel Mouse"
 	export TOUCH_PAD=$RET
 	export TOUCH_NIPPLE=$RET
 fi
@@ -41,3 +43,4 @@ fi
 export TOUCH="$TOUCH_PAD $TOUCH_NIPPLE $TOUCH_SCREEN"
 echo "xinput mouse device ids: $MOUSE"
 echo "xinput touch device ids: $TOUCH"
+
