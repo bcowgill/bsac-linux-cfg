d62f173dca04be4fc2be7b844df97730f75110dd
Brent S.A. Cowgill <brent@clearbooks.co.uk>
2017-06-03 16:48:03 +0100

    touch-config.sh finds touchpad usb ID by using xinput --list

--- bin/touch-config.sh
+++ bin/touch-config.sh
@@ -3,6 +3,14 @@
 #xinput --list
 #xinput --list --long 15
 
+function get_usb_id
+{
+	local text
+	text="$1"
+	RET=`xinput --list | grep "$text" | perl -pne 's{\A .+ id=(\d+) .+ \z}{$1}xms;'`
+
+}
+
 # Mouse/touch configuration for workshare laptop
 export MOUSE=
 export TOUCH_PAD=15
@@ -19,10 +27,13 @@ fi
 
 if [ "$HOSTNAME" == "brent-Aspire-VN7-591G" ]; then
 	# Mouse/touch configuration for clearbooks laptop
-	export MOUSE=13
-	export TOUCH_PAD=14
+	get_usb_id "Logitech MX Anywhere"
+	export MOUSE=$RET
+	get_usb_id Touchpad
+	export TOUCH_PAD=$RET
 	export TOUCH_NIPPLE=
 	export TOUCH_SCREEN=
 fi
 
 export TOUCH="$TOUCH_PAD $TOUCH_NIPPLE $TOUCH_SCREEN"
+
