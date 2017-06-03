593076a014547cb5018e7c983354c9c98571788b
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-06-03 21:58:20 +0100

    typescript module make.sh ignore module examples which cannot do in es2015

--- bin/template/typescript/modules/make.sh
+++ bin/template/typescript/modules/make.sh
@@ -8,11 +8,13 @@ do
 	[ -d $T ] || mkdir $T
 	rm *.js 2> /dev/null
 	if [ $T == es2015 ]; then
-		mv ModuleExportEq.ts ignore
+		mv ModuleExportEq.ts ignore1
+		mv use-module-export-eq.ts ignore2
 	fi
 	tsc --project tsconfig-$T.json 2>&1 | tee $T/build.log
 	mv *.js $T
 	if [ $T == es2015 ]; then
-		mv ignore ModuleExportEq.ts
+		mv ignore1 ModuleExportEq.ts
+		mv ignore2 use-module-export-eq.ts
 	fi
 done
--- bin/template/typescript/modules/use-module-export-eq.ts
+++ bin/template/typescript/modules/use-module-export-eq.ts
@@ -1,7 +1,10 @@
 // import the default export only, cannot use normal syntax as
 // module has export = in it.
 //import CDefault from './ModuleExportEq'
+
+// cannot use this import method in es2015 module formats (use import * as X ... or import {X} ..., or import X ...)
 import ModuleExports = require('./ModuleExportEq')
+
 new ModuleExports.CExported ('the name')
 
 // import the exports as named, cannot use this syntax either with export =
@@ -22,3 +25,4 @@ const CCme = ModuleExports.CExported
 call1()
 call2()
 new CCme('name2')
+
