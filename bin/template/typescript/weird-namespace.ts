c482cf55a78ccdcaa873cba829f157196bae99a5
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-06-20 21:38:41 +0100

    typescript weird,weird-namespace, additional run time testing of functions, arrow functions, statics etc

--- bin/template/typescript/weird-namespace.ts
+++ bin/template/typescript/weird-namespace.ts
@@ -1,5 +1,7 @@
+   // becomes Weird as default export
    export namespace Weird
    {
+	  // becomes Weird.FWeird exported on the namespace
 	  export function FWeird( value: any )
 	  {
 		 console.log( 'FWeird was called', value );
@@ -11,6 +13,7 @@
 
 		 public readonly description: string;
 
+		 // becomes Weird.CWeird() constructor function
 		 constructor(
 			public name: string,
 			protected prot: string = 'protected string',
@@ -21,41 +24,87 @@
 			this.description = `I am ${name} and I keep ${this.priv} a secret but share ${this.prot} with friends`;
 		 }
 
+		 // becomes Weird.CWeird.statics() as a static method
 		 static statics()
 		 {
 			console.log( `${CWeird.klass} statics called` );
 		 }
 
-		 whatever() // also an instance method
+		 // becomes Weird.CWeird.staticArrow() as a static method
+		 static staticArrow = () =>
+		 {
+			console.log( `${CWeird.klass} staticArrow called` );
+		 }
+
+		 // becomes Weird.CWeird.prototype.whatever() as an instance method
+		 whatever()
 		 {
 			console.log( `${this.name} whatever called` );
 		 }
+
+		 // becomes instance.arrow() as a direct instance method
+		 public arrow = () =>
+		 {
+			console.log( `${this.name} arrow called` );
+			this.protecteds()
+		 }
+
+		 // becomes Weird.CWeird.prototype.publics() as an instance method
 		 public publics()
 		 {
 			console.log( `${this.name} publics called` );
 			console.log( `description: ${this.description}` );
 			this.protecteds()
+			this.justAFunction( 'from publics()' );
+			this.equalsFunction( 'from publics()' );
 		 }
+
+		 // becomes Weird.CWeird.prototype.privates() as an instance method
 		 private privates()
 		 {
 			console.log( `${this.name} privates called` );
 			FWeird( 'from privates' );
 		 }
+
+		 // becomes Weird.CWeird.prototype.protecteds() as an instance method
 		 protected protecteds()
 		 {
 			console.log( `${this.name} protecteds called` );
 			this.privates();
 		 }
+
+		 // becomes instance.justAFunction() with no access to this
+		 justAFunction = function ( name: string )
+		 {
+			console.log( `CWeird.justAFunction(${name}) called - no ref to this` );
+		 }
+
+		 // becomes instance.equalsFunction() as a direct instance method with this accessible
+		 equalsFunction = function ( this: CWeird, name: string )
+		 {
+			console.log( `CWeird.equalsFunction(${name}) called desc: ${this.description}` );
+			this.privates();
+		 }
 	  }
    }
+
    const iWeird = new Weird.CWeird( 'bill' );
    iWeird.publics();
+   iWeird.whatever();
+   iWeird.arrow();
+   iWeird.justAFunction( 'from iWeird' );
+   iWeird.equalsFunction( 'from iWeird' );
+   console.log( `iWeird.description: ${iWeird.description}` );
    // iWeird.description = 'time cannot change me';  // read only, cannot
+   // iWeird.klass // statics not accessible directly from instance
+   const konst: any = iWeird.constructor;
+   console.log( `iWeird access static from instance: ${konst.klass}` ); // access a static from an instance
    // iWeird.protecteds(); // not accessible from instance
    // iWeird.privates(); // not accessible from instance
 
-   console.log( Weird.CWeird.klass );
+   console.log( `Weird.CWeird.klass = ${Weird.CWeird.klass}` );
    Weird.CWeird.statics();
+   Weird.CWeird.staticArrow();
    // iWeird.statics(); // not an instance method
    // Weird.CWeird.klass = 'not allowed'; // read only, cannot
 
--- bin/template/typescript/weird.ts
+++ bin/template/typescript/weird.ts
@@ -1,4 +1,5 @@
-   export function FWeird( value: any ) // becomes a function exported directly
+   // becomes a function exported directly
+   export function FWeird( value: any )
    {
 	  console.log( 'FWeird was called', value );
    }
@@ -9,7 +10,8 @@
 
 	  public readonly description: string;
 
-	  constructor( // becomes CWeird() function default export
+	  // becomes CWeird() constructor function default export
+	  constructor(
 		 public name: string,
 		 protected prot: string = 'protected string',
 		 private priv: string = 'private string'
@@ -19,58 +21,86 @@
 		 this.description = `I am ${name} and I keep ${this.priv} a secret but share ${this.prot} with friends`;
 	  }
 
-	  static statics() // becomes CWeird.statics() as a static method
+	  // becomes CWeird.statics() as a static method
+	  static statics()
 	  {
 		 console.log( `${CWeird.klass} statics called` );
 	  }
 
-	  static staticArrow = () => // becomes CWeird.staticArrow() as a static method
+	  // becomes CWeird.staticArrow() as a static method
+	  static staticArrow = () =>
 	  {
-		 console.log( `${CWeird.klass} statics called` );
+		 console.log( `${CWeird.klass} staticArrow called` );
 	  }
 
-	  whatever() // becomes CWeird.prototype.whatever() as an instance method
+	  // becomes CWeird.prototype.whatever() as an instance method
+	  whatever()
 	  {
 		 console.log( `${this.name} whatever called` );
 	  }
 
-	  public arrow = () => // becomes instance.arrow() as a direct instance method
+	  // becomes instance.arrow() as a direct instance method
+	  public arrow = () =>
 	  {
 		 console.log( `${this.name} arrow called` );
 		 this.protecteds()
 	  }
-	  public publics() // becomes CWeird.prototype.publics() as an instance method
+
+	  // becomes CWeird.prototype.publics() as an instance method
+	  public publics()
 	  {
 		 console.log( `${this.name} publics called` );
 		 console.log( `description: ${this.description}` );
 		 this.protecteds()
+		 this.justAFunction( 'from publics()' );
+		 this.equalsFunction( 'from publics()' );
 	  }
-	  private privates() // becomes CWeird.prototype.privates() as an instance method
+
+	  // becomes CWeird.prototype.privates() as an instance method
+	  private privates()
 	  {
 		 console.log( `${this.name} privates called` );
 		 FWeird( 'from privates' );
 	  }
-	  protected protecteds() // becomes CWeird.prototype.protecteds() as an instance method
+
+	  // becomes CWeird.prototype.protecteds() as an instance method
+	  protected protecteds()
 	  {
 		 console.log( `${this.name} protecteds called` );
 		 this.privates();
 	  }
 
-	  thing = function ( this: CWeird ) // becomes instance.thing() as a direct instance method
+	  // becomes instance.justAFunction() with no access to this
+	  justAFunction = function ( name: string )
 	  {
-		 console.log( `CWeird.thing called ${this}` );
-		 this.publics();
+		 console.log( `CWeird.justAFunction(${name}) called - no ref to this` );
+	  }
+
+	  // becomes instance.equalsFunction() as a direct instance method with this accessible
+	  equalsFunction = function ( this: CWeird, name: string )
+	  {
+		 console.log( `CWeird.equalsFunction(${name}) called desc: ${this.description}` );
+		 this.privates();
 	  }
    }
 
    const iWeird = new CWeird( 'bill' );
    iWeird.publics();
+   iWeird.whatever();
+   iWeird.arrow();
+   iWeird.justAFunction( 'from iWeird' );
+   iWeird.equalsFunction( 'from iWeird' );
+   console.log( `iWeird.description: ${iWeird.description}` );
    // iWeird.description = 'time cannot change me';  // read only, cannot
+   // iWeird.klass // statics not accessible directly from instance
+   const konst: any = iWeird.constructor;
+   console.log( `iWeird access static from instance: ${konst.klass}` ); // access a static from an instance
    // iWeird.protecteds(); // not accessible from instance
    // iWeird.privates(); // not accessible from instance
 
-   console.log( CWeird.klass );
+   console.log( `CWeird.klass = ${CWeird.klass}` );
    CWeird.statics();
+   CWeird.staticArrow();
    // iWeird.statics(); // not an instance method
    // CWeird.klass = 'not allowed'; // read only, cannot
 
