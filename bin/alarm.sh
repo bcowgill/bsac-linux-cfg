c78d416f9e835de7335d2c0469488855e656950e
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-09-02 15:52:50 +0100

    alarm.sh complete

--- bin/alarm.sh
+++ bin/alarm.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
-# play a sound file as an alarm
+# play a sound file as an alarm now, or at a specified time
+# alarm.sh "2017-01-02 10:01" [sound-file]
 
 CARD=1
-SOUND=${1:-$HOME/bin/sounds/laser.wav}
+WHEN=$1
+SOUND=${2:-$HOME/bin/sounds/chime.wav}
+POLL=5
+WAIT=1
+
+if [ ! -z "$WHEN" ]; then
+	NOW=`date --rfc-3339=seconds | cut -c 1-16`
+	echo $NOW: alarm will go off at $WHEN
+	while [ "$NOW" != "$WHEN" ]; do
+		sleep $POLL
+		NOW=`date --rfc-3339=seconds | cut -c 1-16`
+	done
+fi
+
 echo playing alarm file "$SOUND"
 while /bin/true; do
 	aplay -D sysdefault:CARD=PCH "$SOUND"
-	sleep 1
+	sleep $WAIT
 done
