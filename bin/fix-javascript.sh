47a94a170d9b4cd71762d1b1960b3b04e50c55b0
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-09-30 19:40:51 +0100

    fix-javascript.sh FIX_INDENT FIX_TAB settings to control space fixing

--- bin/fix-javascript.sh
+++ bin/fix-javascript.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 # fix up code after a save
 
+# example use in autofix.sh
+# FIX_INDENT=2 FIX_TAB=0 auto-build.sh fix-javascript.sh 'package.json5 app internals server src test config scripts'
+
+FIX_INDENT=${FIX_INDENT:-4}
+FIX_TAB=${FIX_TAB:-1}
+
+FILES=.js
+#FILES=".js, .scss"
+
 touch auto-build.log
 check-blatted-files.sh > check-blatted-files.log
 if [ $? == 1 ]; then
@@ -12,11 +21,18 @@ else
 	cat check-blatted-files.log
 	rm check-blatted-files.log
 	FILES=`egrep \.js auto-build.log`
+#	FILES=`egrep '\.(js|scss)' auto-build.log`
 	if [ ! -z "$FILES" ]; then
-		echo converting tabs to spaces in changed .js files
+		echo converting tabs to spaces in changed $FILES files
 		echo $FILES
-		INDENT_TAB=0 INPLACE=1 fix-spaces.sh `egrep \.js auto-build.log`
+		INDENT=$FIX_INDENT INDENT_TAB=$FIX_TAB INPLACE=1 fix-spaces.sh `egrep \.js auto-build.log`
+#		INDENT=$FIX_INDENT INDENT_TAB=$FIX_TAB INPLACE=1 fix-spaces.sh `egrep '\.(js|scss)' auto-build.log`
 
 		npm run jsfix
+#		npm run json5 && npm run allfix 2> /dev/null
+#		npm run test:single 2>&1 | grep -v 'npm ERR!'
+#		npm run test 2>&1 | grep -v 'npm ERR!'
+#		npm run json5 && npm run lint 2> /dev/null
+#		npm run json5 && npm run jscheck:config 2> /dev/null
 	fi
 fi
