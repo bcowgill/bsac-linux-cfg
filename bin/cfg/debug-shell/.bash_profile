a4e51171bec60260c6a3cc8395500fb9636b84de
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-10-09 20:28:36 +0100

    debug-shell log.sh to log some basic info about the shell

--- bin/cfg/debug-shell/.bash_profile
+++ bin/cfg/debug-shell/.bash_profile
@@ -1,4 +1,8 @@
-echo `date` debug-shell:.bash_profile entered >> ~/startup.log
+echo === `date` debug-shell:.bash_profile entered ========================= >> ~/startup.log
+echo `date` debug-shell: HOSTNAME: $HOSTNAME TYPE: $MACHTYPE $HOSTTYPE $OSTYPE LOGNAME: $LOGNAME USER: $USER HOME: $HOME >> ~/startup.log
+echo `date` debug-shell: DISPLAY: $DISPLAY TERM: $TERM LANG: $LANG TZ: $TZ PWD: $PWD >> ~/startup.log
+echo `date` debug-shell: SHLVL: $SHLVL -: $- UID: $UID EUID: $EUID PPID: $PPID WINDOWID: $WINDOWID COL: $COLUMNS LINES: $LINES $BASH $BASH_VERSION >> ~/startup.log
+echo `date` debug-shell: PATH: $PATH >> ~/startup.log
 
 # grabbed from .profile file.
 # ~/.profile: executed by the command interpreter for login shells.
--- bin/cfg/debug-shell/log.sh
+++ bin/cfg/debug-shell/log.sh
@@ -1,13 +1,5 @@
-# ~/.bash_logout: executed by bash(1) when login shell exits.
-
-# when leaving the console clear the screen to increase privacy
-
-echo === `date` debug-shell:.bash_logout entered ========================= >> ~/startup.log
+echo === `date` debug-shell:.bash_login entered ========================= >> ~/startup.log
 echo `date` debug-shell: HOSTNAME: $HOSTNAME TYPE: $MACHTYPE $HOSTTYPE $OSTYPE LOGNAME: $LOGNAME USER: $USER HOME: $HOME >> ~/startup.log
 echo `date` debug-shell: DISPLAY: $DISPLAY TERM: $TERM LANG: $LANG TZ: $TZ PWD: $PWD >> ~/startup.log
 echo `date` debug-shell: SHLVL: $SHLVL -: $- UID: $UID EUID: $EUID PPID: $PPID WINDOWID: $WINDOWID COL: $COLUMNS LINES: $LINES $BASH $BASH_VERSION >> ~/startup.log
 echo `date` debug-shell: PATH: $PATH >> ~/startup.log
-
-if [ "$SHLVL" = 1 ]; then
-    [ -x /usr/bin/clear_console ] && /usr/bin/clear_console -q
-fi
