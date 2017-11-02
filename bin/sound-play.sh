3021c75ddd9274c043a621f5759d2cf1c05f706a
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-11-02 07:31:08 +0000

    sound-play.sh fixed to play -my-sound-files

--- bin/sound-play.sh
+++ bin/sound-play.sh
@@ -4,7 +4,7 @@
 CARD=1
 SOUND=${1:-$HOME/bin/sounds/laser.wav}
 if which play > /dev/null; then
-	play -q "$SOUND"
+	play -q -- "$SOUND"
 	exit 0
 fi
 if which afplay > /dev/null ; then
@@ -12,7 +12,7 @@ if which afplay > /dev/null ; then
 	exit 0
 fi
 if which aplay > /dev/null; then
-	aplay -q -D sysdefault:CARD=PCH "$SOUND"
+	aplay -q -D sysdefault:CARD=PCH -- "$SOUND"
 	exit 0
 fi
 echo NOT OK unable to find a sound player
