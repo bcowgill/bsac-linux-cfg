a1083164a85d62fd7ee0d42212c9bb0240a401b7
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-11-07 19:49:55 +0000

    datestamp.sh fixed Mac version of output to match linux exactly

--- bin/datestamp.sh
+++ bin/datestamp.sh
@@ -1,8 +1,8 @@
-# output an rfc-3339 ish timestamp. exact on linux, almost on Mac.
-# 2017-11-07 12:32:52-00:00 -- linux output
-# 2017-11-07 12:32:52+0000  -- mac output
+# output an rfc-3339 timestamp.
+# 2017-11-07 12:32:52+00:00
 if date --rfc-3339=seconds 2> /dev/null; then
 	exit 0
 else
-	date '+%Y-%m-%d %H:%M:%S%z'
+#	date '+%Y-%m-%d %H:%M:%S%:z'
+	date '+%Y-%m-%d %H:%M:%S%z' | perl -pne 's{(\d\d \s*) \z}{:$1}xmsg'
 fi
