f874030eb89d665624f462d43f4585f22dd359ff
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-11-20 07:52:48 +0000

    moon-shot updated to grab url from img preload tag

--- bin/moon-shot.sh
+++ bin/moon-shot.sh
@@ -2,8 +2,16 @@
 # save a png of what the moon currently looks like.
 OUTPUT=$HOME/d/Dropbox/Photos/Wallpaper/WorkSafe/astronomy-photos/earth-moon
 URL=http://aa.usno.navy.mil/imagery/moon
-URL="http://api.usno.navy.mil/imagery/moon.png?&ID=AA-URL"
+#URL="http://api.usno.navy.mil/imagery/moon.png?sequence=15&ID=AA-URL"
 FILE=$OUTPUT/moon-phase-`datestampfn.sh`.png
+TMP=/tmp/moon.html
 
-echo $FILE
+wget --output-document $TMP "$URL"
+URL=`perl -ne 'if (m{"preload" [^>]+ src="([^"]+)"}xms) { print $1; exit 0; } END { exit 1; }' $TMP`
+echo image: $URL
 wget --output-document $FILE "$URL"
+echo $FILE
+
+exit 0
+
+<img id="preload" class="alt" src="http://api.usno.navy.mil/imagery/moon.png?sequence=18&amp;ID=AA-URL" alt="What the Moon looks like now">
