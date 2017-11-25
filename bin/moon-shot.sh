a93b09d7137c5a27165da74b2b0d41a2afb6b013
Brent S.A. Cowgill <zardoz@infoserve.net>
2017-11-25 12:08:45 +0000

    moon-shot.sh wait a bit and try again if an empty image file is downloaded

--- bin/moon-shot.sh
+++ bin/moon-shot.sh
@@ -4,12 +4,23 @@ OUTPUT=$HOME/d/Dropbox/Photos/Wallpaper/WorkSafe/astronomy-photos/earth-moon
 URL=http://aa.usno.navy.mil/imagery/moon
 #URL="http://api.usno.navy.mil/imagery/moon.png?sequence=15&ID=AA-URL"
 FILE=$OUTPUT/moon-phase-`datestampfn.sh`.png
-TMP=/tmp/moon.html
+TMP=`mktemp`
 
-wget --output-document $TMP "$URL"
-URL=`perl -ne 'if (m{"preload" [^>]+ src="([^"]+)"}xms) { print $1; exit 0; } END { exit 1; }' $TMP`
-echo image: $URL
-wget --output-document $FILE "$URL"
+function get_moon
+{
+	wget --output-document $TMP "$URL"
+	URL=`perl -ne 'if (m{"preload" [^>]+ src="([^"]+)"}xms) { print $1; exit 0; } END { exit 1; }' $TMP`
+	rm $TMP
+	echo image url: $URL
+	wget --output-document $FILE "$URL"
+}
+
+touch $FILE
+while file $FILE | grep empty;
+	do get_moon;
+	sleep 10
+done
+
 echo $FILE
 
 exit 0
