f0dd51ada9ec397cc43f4a15519f009100d1ed9b
Brent S.A. Cowgill <zardoz@infoserve.net>
2018-01-19 21:35:45 +0000

    promiseTracer.js trace, label and error functions for tracing through promise then/catch functions.

---
+++ bin/template/javascript/promiseTracer.js
@@ -0,0 +1,34 @@
+// A tiny little tool for tracing through promises
+
+export default function promiseTracer(group = 'Promise', traceOn = false) {
+  const log = console;
+  const identity = v => v;
+  const make = (fn) => traceOn ? fn : identity;
+
+  const label = make((l) => {
+    return (v) => { log.debug(`${group}@${l}`, v); return v; };
+  });
+
+  const error = make((l) => {
+    return (v) => { log.error(`${group}@${l}`, v); return v; };
+  });
+
+  const trace = make((fn, l) => {
+    return (v) => {
+      let out;
+      label(`${l} in`)(v);
+      try {
+        out = fn(v);
+        label(`${l} out`)(out);
+      }
+      catch (exception)
+      {
+        error(`${l} throws`)(exception);
+        throw exception;
+      }
+      return out;
+    };
+  });
+
+  return { trace, label, error };
+}
