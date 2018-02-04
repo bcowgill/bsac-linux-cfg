8ec57fd33f08c569602d100aa50a890a0252e838
Brent S.A. Cowgill <zardoz@infoserve.net>
2018-02-04 09:02:56 +0000

    git hooks prepare-commit-msg branch/WORKON var with [POLICY]

--- bin/template/git-hook/install.sh
+++ bin/template/git-hook/install.sh
@@ -1,2 +1,17 @@
 # install the named hook file as a git hook
-cp $1 ../../../.git/hooks/
+HOOK=$1
+RENAME=${2:-$HOOK}
+
+if [ -z "$HOOK" ]; then
+	cat <<EOF
+usage: $0 hook-file [rename]
+
+This script will install the named hook-file from this directory
+into the .git/hooks directory for this repository.  If the file
+needs to be renamed because it has a .suffix, supply it as the
+second parameter.
+EOF
+	exit 1
+fi
+
+cp $HOOK ../../../.git/hooks/$RENAME
