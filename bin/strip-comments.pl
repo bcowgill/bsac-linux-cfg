65d2b9bf1c25a149527d172fed0b2d5df7d1aaa8
Brent S.A. Cowgill <zardoz@infoserve.net>
2018-02-11 09:48:21 +0000

    resolved In strip-comments.pl unit tests for inplace editing and implemented it.

--- bin/strip-comments.pl
+++ bin/strip-comments.pl
@@ -4,14 +4,19 @@
 use strict;
 use warnings;
 use English -no_match_vars;
-use File::Slurp;
+use File::Copy qw(cp);    # copy and preserve source files permissions
+use File::Slurp qw(:std :edit);
+use autodie qw(open cp);
 use open IN => ':raw';
+
+local $INPUT_RECORD_SEPARATOR = undef;
 
 my $bl = qr{(\A|\n)}xms;
 my $el = qr{(\z|\n)}xms;
 
-my $STRIP = 0;
-my $SHOW = 'kept';
+my $INPLACE;
+my $STRIP = 1;
+my $SHOW = 'keep';
 my $JAVADOC = ''; # or strict or undefined
 my %Keep = (
 	eslint => 1,
@@ -21,6 +26,14 @@ my %Keep = (
 	istanbul => 0,
 	prettier => 1,
 );
+my $KEEP_RE;
+
+my @comments = ();
+my @keep_comments = ();
+
+process_args();
+check_options();
+main();
 
 sub usage
 {
@@ -31,6 +44,7 @@ usage: $0 [--help] filename...
 
 Strips out C/C++ style comments from source code in files specified.
 
+--inplace=.bak will edit the file in place backing up to file extension provided.
 --show will show the comments which would be stripped, but leaves them in place.
 --keep will show the comments which are kept, without stripping anything from the file.
 --eslint or --noeslint allows or strips eslint directive comments
@@ -48,166 +62,252 @@ USAGE
 	exit ($message ? 1 : 0);
 }
 
-if (scalar(@ARGV) && $ARGV[0] eq '--help')
+sub process_args
 {
-	usage();
-}
+	if (scalar(@ARGV) && $ARGV[0] eq '--help')
+	{
+		usage();
+	}
 
-while (scalar(@ARGV) && $ARGV[0] =~ m{\A --}xms)
-{
-	my $option = shift(@ARGV);
-	if ($option eq '--show')
+	while (scalar(@ARGV) && $ARGV[0] =~ m{\A --}xms)
 	{
-		$STRIP = 0;
-		$SHOW = 'stripped';
-	}
-	elsif ($option eq '--keep')
-	{
-		$STRIP = 0;
-		$SHOW = 'kept';
-	}
-	elsif ($option =~ m{\A --(no)?((esl|jsl|jsh)int|jscs|istanbul|prettier)}xms)
-	{
-		if (exists($Keep{$2}))
+		my $option = shift(@ARGV);
+		if ($option eq '--show')
 		{
-			$Keep{$2} = ($1 ? 0 : 1);
+			$STRIP = 0;
+			$SHOW = 'stripped';
+		}
+		elsif ($option eq '--keep')
+		{
+			$STRIP = 0;
+			$SHOW = 'keep';
+		}
+		elsif ($option =~ m{\A --(no)?((esl|jsl|jsh)int|jscs|istanbul|prettier)}xms)
+		{
+			if (exists($Keep{$2}))
+			{
+				$Keep{$2} = ($1 ? 0 : 1);
+			}
+			else
+			{
+				usage("Invalid option $option");
+			}
+		}
+		elsif ($option =~ m{\A --(no)?javadoc(=(strip|strict|lite))?}xms)
+		{
+			if ( $1 || ($3 && $3 eq 'strip'))
+			{
+				$JAVADOC = '';
+			}
+			elsif ($3)
+			{
+				$JAVADOC = $3;
+			}
+			else
+			{
+				$JAVADOC = 'strict';
+			}
+		}
+		elsif ($option =~ m{\A --inplace(=(.+))?}xms)
+		{
+			$SHOW = '';
+			if (defined $2)
+			{
+				$INPLACE = $2;
+			}
+			else
+			{
+				$INPLACE = '';
+			}
 		}
 		else
 		{
 			usage("Invalid option $option");
 		}
 	}
-	elsif ($option =~ m{\A --(no)?javadoc(=(strip|strict|lite))?}xms)
+}
+
+sub check_options
+{
+	if (defined $INPLACE)
 	{
-		if ($1 || $3 eq 'strip')
+		usage("You cannot specify both --inplace and --$SHOW") if $SHOW;
+		usage("You must specify a filename when using --inplace") unless scalar(@ARGV);
+	}
+}
+
+sub main
+{
+	$KEEP_RE = join('|', grep { $Keep{$ARG} } keys(%Keep));
+	$KEEP_RE = "|$KEEP_RE" if length($KEEP_RE);
+	if (scalar(@ARGV))
+	{
+		foreach my $file (@ARGV)
 		{
-			$JAVADOC = '';
-		}
-		elsif ($3)
-		{
-			$JAVADOC = $3;
-		}
-		else
-		{
-			$JAVADOC = 'strict';
+			process_file($file);
 		}
 	}
 	else
 	{
-		usage("Invalid option $option");
+		process_stdio();
 	}
 }
 
-local $INPUT_RECORD_SEPARATOR = undef;
+sub process_file
+{
+	my ($filename) = @ARG;
+	print "$filename:\n";
+	if (defined $INPLACE)
+	{
+		edit_file_inplace($filename, $INPLACE);
+	}
+	else
+	{
+		process_entire_file($filename);
+	}
+}
 
-my $file = <>;
-my @comments = ();
-my @keep_comments = ();
+sub process_stdio
+{
+	my $file = <>;
+	my $output = process_file_content($file);
+	print $output;
+}
 
-my $KEEP_RE = join('|', grep { $Keep{$ARG} } keys(%Keep));
-$KEEP_RE = "|$KEEP_RE" if length($KEEP_RE);
+sub edit_file_inplace
+{
+	my ($filename, $suffix) = @ARG;
+	my $backup = "$filename$suffix";
+	my $content = read_file($filename);
+	my $output = process_file_content($content);
+	if ($content ne $output)
+	{
+		unless ($filename eq $backup)
+		{
+			cp($filename, $backup);
+		}
+		write_file($filename, $output);
+	}
+}
+
+sub process_entire_file
+{
+	my ($filename) = @ARG;
+	my $content = read_file($filename);
+	my $output = process_file_content($content);
+	print $output;
+}
+
+sub process_file_content
+{
+	my ($file) = @ARG;
+
+	@comments = ();
+	@keep_comments = ();
+	my $top_comment;
+	($top_comment, $file) = grab_it($file, qr{ \A ( (\s*) (// [^\n]* $el)+ ) }xms);
+	#print "tc1: $top_comment\n[$file]\n";
+	($top_comment, $file) = grab_it($file, qr{ \A ( (\s*) (/\* .*? \*/) ) }xms) unless $top_comment;
+	#print "tc2: $top_comment\n[$file]\n";
+	if ($top_comment)
+	{
+		push(@keep_comments, $top_comment);
+	}
+
+	# /***** something ******/
+	# /* sorry, this is ok... */
+	$file =~ s{
+		(\s*)
+		(/\*
+			(\s*)
+			(.*?)
+		\*/)
+		}{
+		my ($prespace, $comment, $space, $message) = ($1, $2, $3, $4);
+		replace_comment($prespace, $comment, $space, $message);
+	}xmsgei;
+
+	$file =~ s{
+		(\s*)
+		(// ([^\n]* \n))
+		}{
+		my ($prespace, $comment, $message) = ($1, $2, $3);
+		my $space;
+		($space, $message) = grab_it($message, qr{\A (\s+)}xms);
+		replace_comment($prespace, $comment, $space, $message, "\n");
+	}xmsgei;
+
+	my $output;
+	if ($STRIP)
+	{
+		$output = "$top_comment$file\n";
+	}
+	elsif ($SHOW eq 'keep')
+	{
+		$output = join("\n", @keep_comments);
+	}
+	else
+	{
+		$output = join("\n", @comments);
+	}
+	return $output;
+}
 
 sub grab_it
 {
-  my ($string, $re) = @ARG;
-  my $got = '';
-  if ($string =~ m{$re}xms)
-  {
-    $string =~ s{$re}{}xms;
-    $got = defined($1) ? $1 : '';
-  }
-  return ($got, $string);
+	my ($string, $re) = @ARG;
+	my $got = '';
+	if ($string =~ m{$re}xms)
+	{
+		$string =~ s{$re}{}xms;
+		$got = defined($1) ? $1 : '';
+	}
+	return ($got, $string);
 }
 
 sub keep_comment
 {
-  my ($prespace, $comment, $space, $message) = @ARG;
-  my $punct = '';
-  my $comma = qr{[,:\.]}xms;
-  ($punct, $message) = grab_it($message, qr{\A([^a-z0-9\s]+ \s*)}xms);
+	my ($prespace, $comment, $space, $message) = @ARG;
+	my $punct = '';
+	my $comma = qr{[,:\.]}xms;
+	($punct, $message) = grab_it($message, qr{\A([^a-z0-9\s]+ \s*)}xms);
 
-  if ($message =~ m{ \A (sorry$comma|apologies$comma$KEEP_RE) }xms)
-  {
-    return 1;
-  }
-  if ($message =~ m{ \A (sorry|apologies)[,:\.] }xms)
-  {
-    return 1;
-  }
-  if ($punct =~ m{\A \* \s*}xms)
-  {
-    if ($JAVADOC eq "strict")
-    {
-      return ($message =~ m{\@\w+}xms);
-    }
-    elsif ($JAVADOC)
-    {
-      return 1;
-    }
-  }
+	if ($message =~ m{ \A (sorry$comma|apologies$comma$KEEP_RE) }xms)
+	{
+		return 1;
+	}
+	if ($message =~ m{ \A (sorry|apologies)[,:\.] }xms)
+	{
+		return 1;
+	}
+	if ($punct =~ m{\A \* \s*}xms)
+	{
+		if ($JAVADOC eq "strict")
+		{
+			return ($message =~ m{\@\w+}xms);
+		}
+		elsif ($JAVADOC)
+		{
+			return 1;
+		}
+	}
 
-  return 0;
+	return 0;
 }
 
 sub replace_comment
 {
-  my ($prespace, $comment, $space, $message, $with)  = @ARG;
-  my $replace;
-  if (keep_comment($prespace, $comment, $space, $message))
-  {
-    $replace = $prespace . $comment;
-    push(@keep_comments, $comment);
-  }
-  else
-  {
-    $replace = $with || "";
-    push(@comments, $comment);
-  }
-  return $replace;
-}
-
-my $top_comment;
-($top_comment, $file) = grab_it($file, qr{ \A ( (\s*) (// [^\n]* $el)+ ) }xms);
-#print "tc1: $top_comment\n[$file]\n";
-($top_comment, $file) = grab_it($file, qr{ \A ( (\s*) (/\* .*? \*/) ) }xms) unless $top_comment;
-#print "tc2: $top_comment\n[$file]\n";
-if ($top_comment)
-{
-  push(@keep_comments, $top_comment);
-}
-
-# /***** something ******/
-# /* sorry, this is ok... */
-$file =~ s{
-  (\s*)
-  (/\*
-    (\s*)
-    (.*?)
-  \*/)
-  }{
-  my ($prespace, $comment, $space, $message) = ($1, $2, $3, $4);
-  replace_comment($prespace, $comment, $space, $message);
-}xmsgei;
-
-$file =~ s{
-  (\s*)
-  (// ([^\n]* \n))
-  }{
-  my ($prespace, $comment, $message) = ($1, $2, $3);
-  my $space;
-  ($space, $message) = grab_it($message, qr{\A (\s+)}xms);
-  replace_comment($prespace, $comment, $space, $message, "\n");
-}xmsgei;
-
-if ($STRIP)
-{
-  print "$top_comment$file\n";
-}
-elsif ($SHOW eq 'kept')
-{
-  print join("\n", @keep_comments);
-}
-else
-{
-  print join("\n", @comments);
+	my ($prespace, $comment, $space, $message, $with) = @ARG;
+	my $replace;
+	if (keep_comment($prespace, $comment, $space, $message))
+	{
+		$replace = $prespace . $comment;
+		push(@keep_comments, $comment);
+	}
+	else
+	{
+		$replace = $with || "";
+		push(@comments, $comment);
+	}
+	return $replace;
 }
--- bin/tests/strip-comments/tests.sh
+++ bin/tests/strip-comments/tests.sh
@@ -12,7 +12,7 @@ SKIP=0
 
 # Include testing library and make output dir exist
 source ../shell-test.sh
-PLAN 19
+PLAN 31
 
 [ -d out ] || mkdir out
 rm out/* > /dev/null 2>&1 || OK "output dir ready"
@@ -41,14 +41,58 @@ if [ 0 == "$SKIP" ]; then
 	OUT=out/$TEST.out
 	BASE=base/$TEST.base
 	ARGS="$DEBUG --invalid $SAMPLE"
-   $PROGRAM $ARGS > $OUT 2>&1 || ERR=$?
-   assertCommandFails $ERR $EXPECT "$PROGARM $ARGS"
-   assertFilesEqual "$OUT" "$BASE" "$TEST"
+	$PROGRAM $ARGS > $OUT 2>&1 || ERR=$?
+	assertCommandFails $ERR $EXPECT "$PROGARM $ARGS"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
 else
 	echo SKIP $TEST "$SKIP"
 fi
 
-SAMPLE=in/top-comment1.txt
+echo TEST $CMD command inplace and keep are incompatible
+TEST=command-invalid-inplace-keep
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	EXPECT=1
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG --inplace --keep $SAMPLE"
+	$PROGRAM $ARGS > $OUT 2>&1 || ERR=$?
+	assertCommandFails $ERR $EXPECT "$PROGARM $ARGS"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
+echo TEST $CMD command inplace and show are incompatible
+TEST=command-invalid-inplace-show
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	EXPECT=1
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG --inplace --show $SAMPLE"
+	$PROGRAM $ARGS > $OUT 2>&1 || ERR=$?
+	assertCommandFails $ERR $EXPECT "$PROGARM $ARGS"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
+echo TEST $CMD command inplace must have filename arguments
+TEST=command-invalid-inplace-nofiles
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	EXPECT=1
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG --inplace"
+	$PROGRAM $ARGS > $OUT 2>&1 || ERR=$?
+	assertCommandFails $ERR $EXPECT "$PROGARM $ARGS"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
 echo TEST $CMD only comment block in file preserved - line comment
 TEST=only-comment-line
 if [ 0 == "$SKIP" ]; then
@@ -131,6 +175,45 @@ if [ 0 == "$SKIP" ]; then
 else
 	echo SKIP $TEST "$SKIP"
 fi
+
+SAMPLE=in/comments1.txt
+echo TEST $CMD strip comments in place with backup
+TEST=strip-comments-inplace-backup
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	OUT=out/$TEST.out
+	STDOUT=out/$TEST.stdout.out
+	BASE=base/$TEST.base
+	STDOUTBASE=base/$TEST.stdout.base
+	ARGS="$DEBUG --inplace=.bak"
+	cp $SAMPLE $OUT
+	$PROGRAM $ARGS $OUT > $STDOUT || assertCommandSuccess $? "$PROGRAM $ARGS"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+	assertFilesEqual "$STDOUT" "$STDOUTBASE" "STDOUT $TEST"
+	assertFilesEqual "$SAMPLE" "$OUT.bak" "BACKUP $TEST"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
+SAMPLE=in/comments1.txt
+echo TEST $CMD strip comments in place without backup
+TEST=strip-comments-inplace
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	OUT=out/$TEST.out
+	STDOUT=out/$TEST.stdout.out
+	BASE=base/$TEST.base
+	STDOUTBASE=base/$TEST.stdout.base
+	ARGS="$DEBUG --inplace"
+	cp $SAMPLE $OUT
+	$PROGRAM $ARGS $OUT > $STDOUT || assertCommandSuccess $? "$PROGRAM $ARGS"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+	assertFilesEqual "$STDOUT" "$STDOUTBASE" "STDOUT $TEST"
+	assertFileMissing "$OUT.bak" "BACKUP $TEST"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
 
 SAMPLE=in/comments1.txt
 echo TEST $CMD show comments which would be stripped
