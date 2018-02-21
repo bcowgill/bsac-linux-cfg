b63b13efda188af49a8d05893acdd3a20b5238eb
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2018-02-21 09:04:54 +0000

    resolved In find-ez.sh simplified the MACOS parsing of find output.

--- bin/find-ez.sh
+++ bin/find-ez.sh
@@ -7,7 +7,7 @@ find-ez () {
 	if which sw_vers > /dev/null; then
 		# MACOS
 		# 1387357        8 -rwxr-xr-x    1 bcowgill         staff                 116  7 Oct 15:05 ./xbuild-screen-upper.sh
-		pushd "$sourceDir" > /dev/null && find . -type f -ls | perl -pne '$q = chr(34); @x = @{split(/\s+/g)}[6, -1]; $size = shift(@x); $path = pop(@x); $time = join(" ", @x); $_ = "${q}$path$q\t$size\t$time\n"' && popd > /dev/null
+		pushd "$sourceDir" > /dev/null && find . -type f -ls | perl -pne '$q = chr(34); s{\A \d+ \s+ \d+ \s+ [drwx\-]+ \s+ \d+ \s+ \w+ \s+ \w+ \s+}{}xms; @x = split(/\s+/g); $size = shift(@x); $path = pop(@x); $time = join(" ", @x); $_ = "${q}$path$q\t$size\t$time\n"' && popd > /dev/null
 	else
 		# linux output:
 		# "./diffemacs.sh"	136	2016-08-22+00:59:01.5543460410
