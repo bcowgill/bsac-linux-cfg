0d126988c27042b44d760d779cb067771862e79b
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2018-02-25 11:12:33 +0000

    resolved Added prepend.sh based on inject-middle.sh injects a file at top of another

---
+++ bin/prepend.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+CONTENT=
+export CONTENT_FILE=$1
+shift
+if [ "--content" == "$CONTENT_FILE" ]; then
+	CONTENT="$1"
+	shift
+	export CONTENT_FILE=`mktemp`
+	echo "$CONTENT" >> $CONTENT_FILE
+fi
+
+if [ -z "$1" ]; then
+	cat <<EOF
+usage: $0 [--content "content to prepend"] [content-filename] file ...
+
+This script will insert some content at the top of the files listed.
+
+--content  specify the actual content to insert into the files.
+content-filename  specifies the file to insert into the files.
+
+example:
+
+$0 --content "/* eslint max-len: 100 */" filename
+EOF
+	exit 1
+fi
+
+#echo "CONTENT=$CONTENT"
+#echo "CONTENT_FILE=$CONTENT_FILE"
+#cat "$CONTENT_FILE"
+#echo args $*
+
+perl -e '
+	BEGIN
+	{
+		local $/ = undef;
+		my $fh;
+		open($fh, "<", $ENV{CONTENT_FILE}) || die "content file <$ENV{CONTENT_FILE}>: $!";
+		$content = <$fh>;
+		close($fh);
+	}
+
+	foreach my $file (@ARGV)
+	{
+		local $/ = undef;
+		print "inserting content into $file\n";
+		my $fh;
+		open($fh, "<", $file) || die "target file <$file>: $!";
+		my $target = <$fh>;
+		close($fh);
+
+		open($fh, ">", $file) || die "target file <$file>: $!";
+		print $fh $content . $target;
+		close($fh);
+	}
+' $*
+
+if [ ! -z "$CONTENT" ]; then
+	rm "$CONTENT_FILE"
+fi
+
