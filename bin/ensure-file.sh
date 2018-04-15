6945fcbbd67efa30d7e96f5b5c49853fe1f79f83
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2018-04-15 19:29:08 +0100

    resolved Added ensure-file.sh like touch but will create directories needed

---
+++ bin/ensure-file.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Ensure that a file exists by touching it and creating the directory path if needed.
+
+if [ -z "$1" ]; then
+	echo "
+usage: $0 path-to-filename
+
+This is like a simple touch, but wnsures that the file will exist if the directory doesn't.
+"
+	exit 1
+fi
+
+DIR=`dirname $1`
+
+#echo DIR=$DIR
+
+[ -d "$DIR" ] || mkdir -p "$DIR"
+touch "$1"
+
