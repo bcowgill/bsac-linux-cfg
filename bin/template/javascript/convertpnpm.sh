bc491f28777a93ecebe313971e1710d51c7ad848
Brent S.A. Cowgill <zardoz@infoserve.net>
2018-04-21 12:31:51 +0100

    resolved Added template/javascript/convertpnpm.sh to switch javascipt package managers

---
+++ bin/template/javascript/convertpnpm.sh
@@ -0,0 +1,30 @@
+#npm install -g pnpm
+#pnpm install -g pnpm
+date
+echo Before conversion disk space used
+du -sk
+
+PROJ=`find . -name package.json \
+	| grep -vE '/.(atom|n[pv]m|cache/yarn)/' \
+	| grep -v /check-system/ \
+	| grep -v node_modules \
+	| perl -pne 's{/package\.json}{}xmsg'
+`
+
+for d in $PROJ
+do
+	if [ -d $d/node_modules ]; then
+		echo $d has node_modules
+		SAVE=/data/me/saved_node_modules/$d
+		mkdir -p $SAVE
+		pushd $d
+			mv node_modules $SAVE
+			pnpm install
+			git add shrinkwrap.yaml
+		popd
+	fi
+done
+
+echo Finished conversion disk space used
+du -sk
+date
