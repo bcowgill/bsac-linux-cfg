04e8b019422176f1e89957a00d09440a9078b4e1
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2018-04-24 20:06:57 +0100

    resolved Added git-use-kdiff3.sh for git mergetool setup on windows machine\

---
+++ bin/git-use-kdiff3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# configure git difftool and mergetool to use kdiff3
+# https://stackoverflow.com/questions/33308482/git-how-configure-kdiff3-as-merge-tool-and-diff-tool
+
+# change trustExitCode false  if it turns out kdiff3 does not exit with error codes properly.
+
+git config --global --add merge.tool kdiff3
+git config --global --add mergetool.kdiff3.path "C:/Program Files/KDiff3/kdiff3.exe"
+git config --global --add mergetool.kdiff3.trustExitCode true
+
+git config --global --add diff.guitool kdiff3
+git config --global --add difftool.kdiff3.path "C:/Program Files/KDiff3/kdiff3.exe"
+git config --global --add difftool.kdiff3.trustExitCode true
