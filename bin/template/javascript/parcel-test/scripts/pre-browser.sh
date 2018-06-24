ccd7e9175b263c377a8a50b52af59aa8d36d20c5
Brent S.A. Cowgill <zardoz@infoserve.net>
2018-06-24 20:19:56 +0100

    resolved In parcel-test use different env setting for babel-preset-env instead of .babelrc.node
    removed .babelrc.browser and .node

--- bin/template/javascript/parcel-test/scripts/pre-browser.sh
+++ bin/template/javascript/parcel-test/scripts/pre-browser.sh
@@ -1,3 +1,1 @@
-# TODO may be able to use babel-preset-env environment configs to get rid of this
-echo "using .babelrc.browser"
-cp .babelrc.browser .babelrc
+echo building for browser.
--- bin/template/javascript/parcel-test/scripts/pre-node.sh
+++ bin/template/javascript/parcel-test/scripts/pre-node.sh
@@ -1,3 +1,1 @@
-# TODO may be able to use babel-preset-env environment configs to get rid of this i.e. NODE_ENV=node to specify a target:node in .babelrc
-echo "using .babelrc.node"
-cp .babelrc.node .babelrc
+echo building for node.
