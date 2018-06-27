fffe0e259d5771dcee7d2cd1fa170984fc7e9cd5
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2018-06-27 16:15:39 +0100

    resolved In javascript templates, jest unit tesing configuration

---
+++ bin/template/javascript/parcel-test/src/test/jest-setup-test-framework.js
@@ -0,0 +1,5 @@
+// configured in jest.config.js to run once before each test suite is executed
+// runs after the test framework has been set up.
+// console.log('in jest-setup-test-framework.js');
+// console.log('describe', describe, describe.only, describe.skip);
+// console.log('it', it, it.only, it.skip);
