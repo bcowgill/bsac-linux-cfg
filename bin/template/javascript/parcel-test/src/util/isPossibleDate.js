b27fb810fdd36f0f8dccdefc3efdaaec7a157389
Brent S.A. Cowgill <zardoz@infoserve.net>
2018-07-08 18:14:10 +0100

    wip In parcel-test isPossibleDate tests all passing and fully covered

--- bin/template/javascript/parcel-test/src/util/isPossibleDate.js
+++ bin/template/javascript/parcel-test/src/util/isPossibleDate.js
@@ -1,84 +1,8 @@
 // isPossibleDate.js
 // used to check if a given year, month, date value could be used to create a valid date.
 
-import isString from 'lodash/isString';
 import isValidDate from'./isValidDate';
-
-const DECIMAL = 10;
-const INVALID_DATE = new Date('2000-99-99');
-
-// pad to number of digits with optional negative sign
-function padDigits(value, length) {
-  if (value < 0) {
-    return '-' + padDigits(-value, length);
-  }
-  const valueString = '' + value;
-  if (valueString.length >= length) {
-    return valueString;
-  }
-  return '0' + padDigits(value, length - 1);
-}
-
-function padYear(yearValue) {
-  let yearString;
-  if (isNaN(yearValue)) {
-    yearString = yearValue;
-  }
-  else if (yearValue < 0) {
-    yearString = padDigits(yearValue, 6);
-  }
-  else {
-    yearString = padDigits(yearValue, 4);
-  }
-  return yearString;
-}
-
-// makes a date object, valid or invalid, but does not throw an error
-function dateOf(dateString, dateInfo) {
-  let date;
-  try {
-    date = new Date(dateString);
-    dateInfo.date = date;
-    dateInfo.dateYear = 1900 + date.getYear();
-    dateInfo.dateMonth = 1 + date.getMonth();
-    dateInfo.dateDay = date.getDate();
-    dateInfo.timestamp = date.getTime();
-  }
-  catch (ignored) {
-    // for years too far out of range
-    date = INVALID_DATE;
-  }
-// console.error('dateOf', dateString, dateInfo);
-  return date;
-}
-
-export function prepareYMD(year, month, day) {
-  const yearValue = parseInt(year, DECIMAL);
-  const monthValue = parseInt(month, DECIMAL);
-  const dayValue = parseInt(day, DECIMAL);
-  const yearString = padYear(yearValue);
-  const monthString = (monthValue < 10 ? '0' : '') + monthValue;
-  const dayString = (dayValue < 10 ? '0' : '') + dayValue;
-  const dateString = `${yearString}-${monthString}-${dayString}`;
-
-  const dateInfo = {
-    year: yearValue,
-    month: monthValue,
-    day: dayValue,
-    yyyymmdd: dateString,
-    yyyy: yearString,
-    mm: monthString,
-    dd: dayString,
-    date: INVALID_DATE,
-    dateYear: NaN,
-    dateMonth: NaN,
-    dateDay: NaN,
-    timestamp: NaN,
-  };
-  dateOf(dateString, dateInfo);
-
-  return dateInfo;
-}
+import prepareYMD, { parseDateString } from './prepareYMD';
 
 export function isPossibleDateReturn(dateInfo, result) {
   return isValidDate(dateInfo.date)
@@ -112,9 +36,17 @@ export function isPossibleDateYMDParts(year, month, day) {
   return isPossibleDateReturn(dateInfo, [dateInfo.yyyy, dateInfo.mm, dateInfo.dd]);
 }
 
+// Input may be a dateString or year, month, day values
+// dateString: yyyy-mm-dd or -yyyyyy-mm-dd
+// returns dateInfo object or false
 export default function isPossibleDate(checkDate, ...rest) {
-//  if (isString)
+  const ymd = (rest.length < 2)
+    ? parseDateString(checkDate)
+    : [checkDate, rest[0], rest[1]];
 
-  const date = isString(checkDate) ? new Date(checkDate) : checkDate;
-  return isValidDate(date);
+  if (ymd) {
+    const dateInfo = prepareYMD.apply(null, ymd);
+    return isPossibleDateReturn(dateInfo, dateInfo);
+  }
+  return false;
 }
--- bin/template/javascript/parcel-test/src/util/isPossibleDate.spec.js
+++ bin/template/javascript/parcel-test/src/util/isPossibleDate.spec.js
@@ -1,5 +1,8 @@
 import isDate from 'lodash/isDate';
+import isPossibleDate from './isPossibleDate';
 import * as pd from './isPossibleDate';
+import prepareYMD from './prepareYMD';
+
 
 const suite = 'src/util isPossibleDate';
 
@@ -19,82 +22,6 @@ const invalidDates = [
 ];
 
 describe(suite, function descIsPossibleDate() {
-  it('prepareYMD 2000, 3, 4 should return dateInfo', function testPrepareYMD20000304() {
-    const dateInfo = pd.prepareYMD(2000, 3, 4);
-    expect(dateInfo).toEqual({
-      "date": new Date('2000-03-04T00:00:00.000Z'),
-      "dd": "04",
-      "dateDay": 4,
-      "dateMonth": 3,
-      "dateYear": 2000,
-      "day": 4,
-      "month": 3,
-      "mm": "03",
-      "year": 2000,
-      "yyyy": "2000",
-      "yyyymmdd": "2000-03-04",
-      "timestamp": 952128000000,
-    });
-  });
-
-  it('prepareYMD 32AD, 3, 4 should return dateInfo', function testPrepareYMD32AD0304() {
-    const dateInfo = pd.prepareYMD('  32', '  3  ',  '   4  ');
-    expect(dateInfo).toEqual({
-      "date": new Date('0032-03-04T00:00:00.000Z'),
-      "dd": "04",
-      "dateDay": 4,
-      "dateMonth": 3,
-      "dateYear": 32,
-      "day": 4,
-      "month": 3,
-      "mm": "03",
-      "year": 32,
-      "yyyy": "0032",
-      "yyyymmdd": "0032-03-04",
-      "timestamp": -61151932800000,
-    });
-  });
-
-
-  it('prepareYMD 32BC, 3, 4 should return dateInfo', function testPrepareYMD32BC0304() {
-    const dateInfo = pd.prepareYMD('  -32', '  3  ',  '   4  ');
-    expect(dateInfo).toEqual({
-      "date": new Date('-000032-03-04T00:00:00.000Z'),
-      "dateDay": 4,
-      "dateMonth": 3,
-      "dateYear": -32,
-      "day": 4,
-      "dd": "04",
-      "mm": "03",
-      "month": 3,
-      "timestamp": -63171619200000,
-      "year": -32,
-      "yyyy": "-000032",
-      "yyyymmdd": "-000032-03-04",
-    });
-  });
-
-  it('prepareYMD 1999, 99, 99 should return dateInfo', function testPrepareYMD19999999() {
-    const dateInfo = pd.prepareYMD('  1999 ', '  99  ',  '   99  ');
-    // Must compare invalid date separately of framework generates a Range Error
-    expect(dateInfo.date.toString()).toBe('Invalid Date');
-    delete dateInfo.date;
-
-    expect(dateInfo).toEqual({
-      "dateDay": NaN,
-      "dateMonth": NaN,
-      "dateYear": NaN,
-      "day": 99,
-      "dd": "99",
-      "mm": "99",
-      "month": 99,
-      "timestamp": NaN,
-      "year": 1999,
-      "yyyy": "1999",
-      "yyyymmdd": "1999-99-99",
-    });
-  });
-
   it('isPossibleDateYMD should return date object for valid dates: ', function testIsPossibleDateValidAsDate() {
     const date = pd.isPossibleDateYMD(2000, 3, 4);
     expect(isDate(date)).toBeTruthy();
@@ -109,15 +36,55 @@ describe(suite, function descIsPossibleDate() {
     expect(isDate(date)).toBeTruthy();
   });
 
+  it('isPossibleDateYMDString should return YMD dateString for valid dates: ', function testIsPossibleDateValidAsDateString() {
+    const date = pd.isPossibleDateYMDString(2000, 3, 4);
+    expect(date).toBe('2000-03-04');
+  });
+
+  it('isPossibleDateYMDParts should return YMD padded array for valid dates: ', function testIsPossibleDateValidAsArray() {
+    const date = pd.isPossibleDateYMDParts(2000, 3, 4);
+    expect(date).toEqual(["2000", "03", "04"]);
+  });
+
+  it('isPossibleDate should return dateInfo for valid dates: ', function testIsPossibleDateValidAsDateInfo() {
+    const date = isPossibleDate(2000, 3, 4);
+    expect(date).toEqual({
+      "date": new Date('2000-03-04T00:00:00.000Z'),
+      "dateDay": 4,
+      "dateMonth": 3,
+      "dateYear": 2000,
+      "day": 4,
+      "dd": "04",
+      "mm": "03",
+      "month": 3,
+      "timestamp": 952128000000,
+      "year": 2000,
+      "yyyy": "2000",
+      "yyyymmdd": "2000-03-04",
+    });
+  });
+
   validDates.map((dateArray) => {
     it('should handle valid dates: ' + dateArray.join(', '), function testIsPossibleDateValid() {
       expect(pd.isPossibleDateYMD.apply(null, dateArray)).toBeTruthy();
+      expect(pd.isPossibleDateYMDString.apply(null, dateArray)).toBeTruthy();
+      expect(pd.isPossibleDateYMDParts.apply(null, dateArray)).toBeTruthy();
+      expect(isPossibleDate.apply(null, dateArray)).toBeTruthy();
+
+      const dateString = prepareYMD.apply(null, dateArray).yyyymmdd;
+      expect(isPossibleDate(dateString)).toBeTruthy();
     });
   });
 
   invalidDates.map((dateArray) => {
     it('should handle invalid dates: ' + dateArray.join(', '), function testIsPossibleDateInvalid() {
       expect(pd.isPossibleDateYMD.apply(null, dateArray)).toBeFalsy();
+      expect(pd.isPossibleDateYMDString.apply(null, dateArray)).toBeFalsy();
+      expect(pd.isPossibleDateYMDParts.apply(null, dateArray)).toBeFalsy();
+      expect(isPossibleDate.apply(null, dateArray)).toBeFalsy();
+
+      const dateString = prepareYMD.apply(null, dateArray).yyyymmdd;
+      expect(isPossibleDate(dateString)).toBeFalsy();
     });
   });
 });
