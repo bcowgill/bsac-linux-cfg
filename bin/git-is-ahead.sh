df8894c10d4e0b67f5492d8c7a8d5fcb65c8adee
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2018-09-04 16:46:37 +0100

    resolved Added some output to get-is-ahead.sh and git-is-clean.sh

--- bin/git-is-ahead.sh
+++ bin/git-is-ahead.sh
@@ -1,3 +1,4 @@
-# check if your repo had unpushed commits
+# check if your repo has unpushed commits
 git status -b -s --porcelain | head -1 | grep '\[ahead' > /dev/null && exit 0
+echo There are no unpushed commits.
 exit 1
--- bin/git-is-clean.sh
+++ bin/git-is-clean.sh
@@ -1,4 +1,6 @@
 # check if there are no local or staged changes in your git repo
 # apart from untracked files
 [ 0 == `git diff | wc -l` ] && [ 0 == `git diff --staged | wc -l` ] && exit 0
+echo Aborting, there are modified files:
+git status --short | head
 exit 1
