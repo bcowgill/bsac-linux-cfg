3b12cc9b47def0f0ae85666fba033950be1eda43
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2018-11-16 10:09:10 +0000

    wip In oh-my-git-aliases.sh a few fixes and additions from my .bash_aliases

--- bin/oh-my-git-aliases.sh
+++ bin/oh-my-git-aliases.sh
@@ -21,10 +21,10 @@ alias gcd='gco develop'
 alias gcm='gco master'
 
 alias ggpush='g push origin $(current_branch)'
-alias ggpushf='ggpush --force-with-lease'
+alias ggpushf='ggpush --force-with-lease origin $(current_branch)'
 alias ggpusht='g push origin --tags'
 
-alias gc='g commit'
+alias gc='g commit' # clashes with gc command
 alias gc!='gc --amend'
 alias gca='gc -a'
 alias gca!='gca --amend'
@@ -34,8 +34,10 @@ alias glr='gl --rebase'
 alias glff='gl --ff-only'
 
 alias gm='g merge'
+alias gma='gm --abort' # BSAC
 alias gmff='gm --ff-only'
 alias gmnff='gm --no-ff'
+alias gmt='g mergetool' # BSAC
 
 alias ga='g add'
 alias gap='ga --patch'
@@ -43,8 +45,10 @@ alias gai='ga -i'
 
 alias gb='g branch'
 alias gba='gb -a'
+alias gbl='gb --list' # BSAC
+alias gblr='gb --list --remote' # BSAC
 
-alias ggr='git grep'
+alias ggr='g grep'
 
 alias grb='g rebase'
 alias grbi='grb --interactive'
@@ -52,26 +56,29 @@ alias grbiod='grbi origin/develop'
 alias grbiom='grbi origin/master'
 alias grbc='grb --continue'
 alias grba='grb --abort'
+alias grbs='grb --skip' # BSAC
 
 alias gt='g tag'
 
 alias gre='g reset'
 alias greh='gre --hard'
-alias ggrh='greh origin/$(current_branch)'
+alias ggrh='greh @{u}'
 
 grf() {
         gre @~ "$@" && gc! --no-edit
 }
 
 alias gd='g diff'
+alias gdc='gd --cached'
 alias gdo='gd origin/$(current_branch) $(current_branch)'
+alias gdt='g difftool --no-prompt --ignore-all-space' # BSAC
 
 alias gcp='g cherry-pick'
 alias gcpa='gcp --abort'
 alias gcpc='gcp --continue'
 
-alias gsts='git stash show --text'
 alias gsta='g stash'
+alias gsts='gsta show --text'
 alias gstp='gsta pop'
 alias gstd='gsta drop'
 alias gstaa='gsta apply' # BSAC
@@ -81,10 +88,13 @@ alias gcl='g clone'
 
 alias gr='g remote'
 
+alias glog='g log --oneline --graph --decorate --all' # BSAC
+
+alias ggraph='g graph --simplify-by-decoration' # BSAC
+
 # https://github.com/robbyrussell/oh-my-zsh
 function current_branch() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || \
   ref=$(git rev-parse --short HEAD 2> /dev/null) || return
   echo ${ref#refs/heads/}
 }
-
