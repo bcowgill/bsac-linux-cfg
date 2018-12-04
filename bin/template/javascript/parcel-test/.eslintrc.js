6f3012cab52765da3e8e73c200dd0a57ee53be01
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2018-12-04 16:09:45 +0000

    resolved Prettier used to format .*rc files properly

--- bin/template/javascript/parcel-test/.eslintrc.js
+++ bin/template/javascript/parcel-test/.eslintrc.js
@@ -1,48 +1,34 @@
 module.exports = {
-    // TODO try it "parser": "babel-eslint",
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es6": true,
-        "jest": true,
-        "node": true
-    },
-    "extends": [
-        "plugin:react/recommended",
-        "plugin:prettier/recommended"
-    ],
-    "settings": {
-        "react": {
-            "version": "16.0"
-        }
-    },
-    "parserOptions": {
-        "ecmaVersion": "2019",
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "globals": {},
-    "rules": {
-        "indent": [
-            "off",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
-};
+	// TODO try it "parser": "babel-eslint",
+	env: {
+		browser: true,
+		commonjs: true,
+		es6: true,
+		jest: true,
+		node: true,
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:prettier/recommended'
+	],
+	settings: {
+		react: {
+			version: '16.0',
+		},
+	},
+	parserOptions: {
+		ecmaVersion: '2019',
+		ecmaFeatures: {
+			jsx: true,
+		},
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	globals: {},
+	rules: {
+		indent: ['off', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+	},
+}
