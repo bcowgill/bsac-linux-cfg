a444ec8a65122f32ac78d7e5504d994876986409
Brent S.A. Cowgill <zardoz@infoserve.net>
2019-01-01 11:02:44 +0000

    wip Added md5sum.sh and ls-spacefiles.sh to help out with md5summing of files

---
+++ bin/md5sum.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# md5sum wrapper to handle file names with spaces in them.
+
+# md5sum.sh directory 2>&1 | tee md5sum.lst
+# md5sum --check md5sum.lst
+
+DIR=${1:-.}
+find $DIR -type f | perl -ne 'chomp; system(qq{md5sum "$_"})'
