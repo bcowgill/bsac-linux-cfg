80611abd27dcf59c5507c56afb7cb1c91feb4010
Brent S.A. Cowgill <zardoz@infoserve.net>
2019-02-17 11:03:31 +0000

    resolved Added wma2mp3.sh to convert WMA file to MP3

---
+++ bin/wma2mp3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+FILE="$1"
+
+if [ -z "$FILE" ]; then
+	echo "usage: `basename $0` filename
+
+This will convert a Windows Media Audio file .WMA file into a .mp3 file using the avconv utility.
+The .mp3 file will be written to the same directory as the .WMA file.
+"
+	exit 1
+fi
+
+D=`dirname "$FILE"`
+F=`basename "$FILE" .wma`
+F=`basename "$F" .WMA`
+
+avconv -y -i "$FILE" -acodec libmp3lame -b:a 160k -ac 2 -ar 44100 "$D/$F.mp3"
