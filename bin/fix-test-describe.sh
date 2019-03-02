f16db7d4cb028e7b4d4dcd2ac192b32122509ad1
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2019-03-02 14:01:58 +0000

    resolved Added fix-test-describe.sh to wrap a test plan
    adds a surrounding describe(...) block in a test plan using the file
    name

---
+++ bin/fix-test-describe.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# multiline search/replace to fix javascript test plan by wrapping
+# the outermost describe in the file with the filename
+
+if [ ! -z "$2" ]; then
+	for FILE in $*
+	do
+		$0 $FILE
+	done
+	exit $?
+fi
+
+cp "$1" "$1.bak"
+FILE="$1" perl -e '
+	use strict;
+	use warnings;
+	use Data::Dumper;
+
+	local $/ = undef;
+	my $q = chr(39);
+	my $Q = chr(34);
+	my $DEBUG = 1;
+
+	$_ = <>;
+
+	# wrap first describe with file name
+	s{
+		(describe\()
+	}{
+		"$1$q$ENV{FILE}$q, function descSuiteFileName () \{\n$1"
+	}xmse;
+
+	# handle closing describe, assuming in first col
+	s{
+		(\n\}\)\;)
+	}{
+		"$1$1 // descSuiteFileName"
+	}xmse;
+
+	print $_;
+
+' $1.bak > $1
