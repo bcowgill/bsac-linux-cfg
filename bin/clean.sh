a92b3283d9aacfc1e0cbc868589893e44652397f
Brent S.A. Cowgill <zardoz@infoserve.net>
2019-04-14 08:43:19 +0100

    resolved Created dataAdd.js and clean.sh fix for Linux/MacOS differences

--- bin/clean.sh
+++ bin/clean.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 # find and delete backup files
-#find-bak.sh -exec rm {} \;
-find-bak.sh -delete
+if which sw_vers > /dev/null 2>&1 ; then
+	# MACOS!
+	find-bak.sh -delete
+else
+	find-bak.sh -exec rm {} \;
+fi
---
+++ bin/dateAdd.js
@@ -0,0 +1,52 @@
+#!/usr/bin/env node
+
+Date.prototype.addDays = function(days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
+const FIRST_ARG = 2;
+
+let date = new Date();
+let output = false;
+
+function usage(message) {
+	const command = process.argv[1];
+	if (message) {
+		console.log(message);
+	}
+	console.log(`
+${command} [date] number ...
+
+This will add a certain number of days to today's date or the specific date specified.
+Each additional number provided is added to the original date.
+`);
+	process.exit(message ? 1 : 0);
+}
+
+function showAddedDate(date, days) {
+	console.log(date);
+	console.log(`plus ${days} days`);
+	console.log(date.addDays(days));
+	console.log('');
+	output = true;
+}
+
+if (process.argv.length < FIRST_ARG) {
+	usage('Please provide a date string or the number of days to add.');
+}
+
+for (let idx = FIRST_ARG; idx < process.argv.length; idx++) {
+	const arg = process.argv[idx];
+	if (/^\s*\d+$$/.test(arg)) {
+		showAddedDate(date, new Number(arg));
+	}
+	else {
+		date = new Date(arg);
+	}
+}
+
+if (!output) {
+	usage('Please provide a number of days to add.');
+}
