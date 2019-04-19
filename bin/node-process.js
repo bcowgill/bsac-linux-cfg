891195c72fa35c2ab11a1e98aa4ce71b0685babd
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2019-04-19 12:44:20 +0100

    resolved Added node-process.js to debug the process object
    added a package.json to allow other .js files to work after a npm
    install

---
+++ bin/node-process.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+// Show most useful information about the node process.
+
+const NODE_ENV = Object.keys(process.env).filter(env => /^(NODE|N[VP]M|TMP|USER|HOME|SHELL|PATH|LOG|TERM|COLUMN|LINE|PWD|EDITOR|LANG|TZ)/.test(env)).sort();
+
+console.log(`
+node process object:
+.title ${process.title}
+.version ${process.version}
+.platform ${process.platform}
+.arch ${process.arch}
+.pid ${process.pid}
+.ppid ${process.ppid}
+.debugPort ${process.debugPort}
+.argv.length ${process.argv.length}
+.argv0 ${process.argv0}
+.execPath ${process.execPath}
+.argv[0] ${process.argv[0]} == path to node
+.argv[1] ${process.argv[1]} == path to javascript file
+.mainModule.filename = ${process.mainModule.filename}
+.mainModule.paths = ${process.mainModule.paths.join('\n  ')}
+__filename ${__filename}
+.cwd() ${process.cwd()}
+.env node related:
+  ${NODE_ENV.map(env => `${env}: ${process.env[env]}`).join('\n  ')}
+`.trim());
+
+if (process.argv[2] === 'dump') {
+	console.log(process)
+}
---
+++ bin/npm-pkg-vers.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+try
+{
+	require('glob');
+}
+catch (exception)
+{
+	console.error("exception: ", exception);
+	console.error("process.argv", process.argv);
+	console.error("process.env.NODE", process.env.NODE);
+	console.error("process.env.NVM_PATH", process.env.NVM_PATH);
+	console.error("process.env.NVM_DIR", process.env.NVM_DIR);
+	console.error("process.env.NVM_BIN", process.env.NVM_BIN);
+	usage('This program requires glob: npm install -g glob\nTo diagnose module loading use an exported environment variable NODE_DEBUG=module');
+}
+
+function usage (message = '')
+{
+	const help = `
+${message}
+Usage: ${binname()}
+
+This program shows the version number of all modules installed in ./node_modules directory.
+`.trim();
+
+	console.log(help);
+	process.exit(message ? 1 : 0);
+}
+
+function binname ()
+{
+	const LEAD_DIRSEP = /^[\/\\]/;
+	return __filename.replace(__dirname, '').replace(LEAD_DIRSEP, '');
+}
+
+const glob = require('glob');
+
+const testFolder = './node_modules';
+
+function getVersionFromJSON(fileName) {
+  let version;
+
+  try {
+    const package = require(fileName);
+    version = package.version;
+  } catch (exception) {
+    console.error('exception', exception);
+  }
+  return version;
+}
+
+const options = {};
+glob(`${testFolder}/**/package.json`, options, function(error, files) {
+  if (!error) {
+    files.forEach((fileName) => {
+      const file = fileName
+        .replace(/\/package.json/, '')
+        .replace(`${testFolder}/`, '');
+      const version = getVersionFromJSON(fileName);
+      if (version) {
+        console.log(`${file} is_version ${version}`);
+      } else {
+        console.log(file, 'unknown');
+      }
+    });
+  } else {
+    console.error('error', error);
+  }
+});
