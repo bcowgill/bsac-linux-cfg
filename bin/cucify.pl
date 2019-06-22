165d93d9b0b399704ba877ec27bd47f17658cac6
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2019-06-22 14:00:12 +0100

    resolved In cucify.pl a few fixes to handle asking for input

--- bin/cucify.pl
+++ bin/cucify.pl
@@ -3,25 +3,63 @@
 
 use strict;
 use warnings;
+use English qw(-no_match_vars);
+
+my $DEBUG = 0;
 
 sub usage
 {
-	my ($message) = @_;
+	my ($message) = @ARG;
 	print "$message\n\n" if $message;
 	print <<"USAGE";
+$0 --ask [file-name] > cypress/integration/PAYO4B-NNN-description.dev.feature
+or
 STORY="JIRA-ID" FEATURE="Name of feature" $0 [file-name]
 
 This will take the Given When Then statements of Acceptance Criteria from a Jira story and format it as Cucumber compliant code.
 
+--ask     If specified, will ask for the story Id and Feature description.
+file-name optional. Name of file to read cucumber text from.
 STORY     The story Id within jira or other tracking system.
 FEATURE   The description of the feature being described.
 USAGE
 	exit($message ? 1 : 0);
 }
 
-my $story = $ENV{STORY} || usage('Please provide a STORY Id.');
-my $feature = $ENV{FEATURE} ? "Feature: [$story] - $ENV{FEATURE}" : usage('Please provide a FEATURE description.');
-my $print_feature = 1;
+my $story;
+my $feature;
+
+sub make_feature
+{
+	my ($story, $description) = @ARG;
+	return "Feature: [$story] - $description";
+}
+
+if (($ARGV[0] || '') eq '--ask')
+{
+	shift;
+	print STDERR "STORY ID (i.e. PAYO4B-1234) ? ";
+	$story = <STDIN>;
+	chomp($story);
+	print STDERR "Feature Description (i.e. Make Multiple Payment) ? ";
+	$feature = <STDIN>;
+	chomp($feature);
+	$feature = make_feature($story, $feature);
+}
+else
+{
+	$story = $ENV{STORY} || usage('Please provide a STORY Id.');
+	$feature = $ENV{FEATURE} ? "Feature: [$story] - $ENV{FEATURE}" : usage('Please provide a FEATURE description.');
+}
+print STDERR "Paste or type acceptance criteria for story and then Ctrl-D\n";
+
+if ($DEBUG)
+{
+	print "STORY=$story\nFEATURE=$feature\n";
+	exit 1;
+}
+
+my $print_feature = 0;
 my $i = '  ';
 my $b = '*'; # to make parts bold for Jira
 
@@ -31,14 +69,17 @@ while (my $line = <>) {
 	next if $line =~ m{\A\s*\z}xms;
 	next if $line =~ m{\A\s*feature}xms;
 	$line =~ s{\s+}{ }xmsg;
-	$line =~ s{\A\s*(feature|scenario|given|when|then|and|\@)}{ucfirst(lc($1))}xmsei;
+	$line =~ s{\A\s*(feature:?|scenario:?|given|when|then|and|\@)}{ucfirst(lc($1))}xmsei;
 	$line =~ s{\A\s*AC\s*(\d+)\s*}{Scenario: [$story-AC$1] }xms;
+	$line =~ s{\bAC\s*(\d+)}{AC$1}xms;
 	$line =~ s{\A\@}{$i\@}xmsi;
-	$line =~ s{\A(scenario:)}{\n$i\@devCWA\n$i$1}xmsi;
+	$line =~ s{\A(scenario)\s*\d+\s*:}{$1:}xmsgi;
+	$line =~ s{\A(scenario:)}{\n$i\@devCWA\n$i\@skip\n$i$1}xmsi;
 	$line =~ s{\A(given|when|then)}{$i$i$b$1$b}xmsi;
 	$line =~ s{\A(and)}{$i$i$i$b$1$b}xmsi;
-	$line =~ s{(scenario.+)\s*\z}{$i$i$i$b$1$b}xmsi;
+	$line =~ s{[\ \t]+(scenario.+)\s*\z}{$i$b$1$b}xmsi;
 	$line =~ s{(\[.+?\])}{$b$1$b}xmsg;
+	$line =~ s{[\ \t]+(\n|\z)}{$1}xmsg;
 	if ($print_feature) {
 		print "\n$b$feature$b\n";
 	}
