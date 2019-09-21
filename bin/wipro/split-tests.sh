3215429cfdc77cf9c8421b8105432fbd8e310185
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2019-09-21 17:25:42 +0100

    resolved Added split-tests.sh to split cypress tests among the team

---
+++ bin/wipro/split-tests.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# create a markdown checkbox list of cypress tests for each team member
+# i.e.
+# # part 1
+# * [ ] 355-2-remove-for-mobile-multiple-payment.dev.feature
+PEOPLE="sandy.lst ammy.lst shabby.lst brent.lst"
+if [ ! -z "$1" ]; then
+	PEOPLE="$*"
+fi
+
+pushd /Users/bcowgill/workspace/projects/o4b/o4b-payments-bdd
+pushd cypress/integration
+
+# find cypress features which are not all skipped (usually new stories)
+echo \* `ls PAY*.dev.feature | wc -l` cypress dev feature files
+for file in PAY*.dev.feature
+do
+	SCENARIOS=`grep -E '^\s*Scenario' $file | wc -l`
+	SKIPS=`grep -E '@skip' $file | wc -l`
+	[ "$SCENARIOS" != "$SKIPS" ] && echo "$file" >> tests.txt
+done
+echo \* `cat tests.txt | wc -l` feature files with enabled tests
+
+# clean up names and sort the by number
+perl -pne 's{PAYO4B-}{}xms' tests.txt \
+	| sort -n \
+	> test-list.txt
+
+split-list.pl test-list.txt $PEOPLE \
+
+for file in $PEOPLE
+do
+	echo \* `cat $file | wc -l` $file
+done
+echo " "
+
+# convert lines to checkboxes and show names
+for file in $PEOPLE
+do
+	echo "# $file" | perl -pne 's{\.lst}{}xms'
+	cat $file | perl -pne 's{\A}{* [ ] }xms'
+	echo " "
+done
+
+rm tests.txt test-list.txt $PEOPLE
+
