eb0673d68d808880d19afb05c9879300ba3a1d66
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2019-09-26 21:38:03 +0100

    wip Updated notes and templates for modules and programs

--- bin/template/python/python-lite.py
+++ bin/template/python/python-lite.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
 
+"""Documentaation for the program"""
+
+import python_module
+
+# Lambda Functions
+
+# User defined local Functions
+
+# python statements
+
 print 'Hello, world!'
