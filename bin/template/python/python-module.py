8622a9c5333ef0ae1dab766cce2c5ab90ad624a6
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2019-09-26 20:40:09 +0100

    wip began filling in python-module template and documenting

--- bin/template/python/python-module.py
+++ bin/template/python/python-module.py
@@ -1,3 +1,29 @@
-# python-module.py - a python module
+"""Template an example python module this is the documentation"""
 
+import sys
 
+__all__ = ["go", "Place"]
+
+# Modue functions
+
+def go(place = "home"):
+    """Documentation for the go function
+
+    The 'place' argument gives the name of the place to go.
+    It defaults to "home."
+    """
+    print "I am going to", place
+
+# Module classes
+
+class Place(object):
+
+    """Documentation for the Place class."""
+
+    def __init__(self, name = "home"):
+        self.name = name
+
+    def __str__(self):
+        return ("{There's no place like %s}" % self.name)
+
+# Module initialization
