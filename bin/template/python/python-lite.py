be5c38abf5a26b1790694677db9e09cee28598a8
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2019-09-29 21:36:23 +0100

    resolved In python-lite.py exit code set correctly

--- bin/template/python/python-lite.py
+++ bin/template/python/python-lite.py
@@ -9,7 +9,9 @@ from traceback import print_exc, print_stack
 import python_module
 
 
-DEBUG=True
+DEBUG=False
+
+exit_code=0
 
 # Lambda Functions
 
@@ -31,6 +33,20 @@ def print_exception(error):
     else:
         print "\n\n>>>>> Caught Error: %s\n" % error
 
+def usage(option, message):
+    print "Argument error: %s\n"% (message)
+    print "\
+usage: %s [-x number] [-y number] [-i or --indent number] [-v] [-? -h or --help]\
+\n\
+\n-x   specifies the X axis coordinate. default 0.\
+\n-y   specifies the Y axis coordinate. default 0.\
+\n-i   specifies the indentation amount. default 0.\
+\n-v   verbose flag.\
+\n-h   provide detailed program help.\
+\n\
+\nThis program just demonstrates a number of commonly needed features of a comand line command written in python.\
+\n"% (sys.argv[0])
+
 def parse_cli():
     params = {}
     (opts, args) = getopt.gnu_getopt(sys.argv[1:], 'vx:y:h?i:', ['help', 'indent='])
@@ -51,11 +67,16 @@ try:
     print "args:", args
     print 'Hello, world!', 4/0
     f()
+except getopt.GetoptError, error:
+    usage(error.opt, error.msg)
+    exit_code=1
 except Exception, error:
     print_exception(error)
+    exit_code=2
 else:
     # happens when there were no errors
     print "Success"
 finally:
     # happens always unless os.abort() called
     print "Cleanup"
+    sys.exit(exit_code)
