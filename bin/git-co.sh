493fde2bda6d2ab1673dc6df9323e629774af4fd
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2019-11-01 09:20:44 +0000

    resolved Added git-co.sh and git-fpr.sh to auto checkout/pull a branch

---
+++ bin/git-co.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SEARCH="$1"
+
+if [ -z "$SEARCH" ]; then
+	echo "
+$0 search
+
+git checkout the single branch which matches the search pattern specified.
+
+If more than one branch matches the pattern then they are shown and nothing else happens.
+
+If there are only two matches and you are currently on one of them you will switch to the other one.
+"
+	exit 1
+fi
+
+
+if [ `git branch --list | grep -v '*' | grep -i "$SEARCH" | wc -l` == "1" ]; then
+	BRANCH=`git branch --list | grep -v '*' | grep -i "$SEARCH"`
+	git checkout "$BRANCH"
+else
+	git branch --list | grep -i "$SEARCH" | sort
+fi
---
+++ bin/git-fpr.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SEARCH="$1"
+
+if [ -z "$SEARCH" ]; then
+	echo "
+$0 search
+
+git fetch a pull request branch which matches the search pattern specified.
+
+If more than one remote branch matches the pattern then they are shown and nothing else happens.
+
+This will perform a pull and hard reset to the remote branch.
+"
+	exit 1
+fi
+
+
+if [ `git branch --list --remote | grep -v '*' | grep -i "$SEARCH" | wc -l` == "1" ]; then
+	BRANCH=`git branch --list --remote | grep -v '*' | grep -i "$SEARCH"`
+	git-fetch-pull-request.sh "$BRANCH"
+else
+	git branch --list --remote | grep -i "$SEARCH" | sort
+fi
