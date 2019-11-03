7f8f8fe49bfc4d0506ab04bd2aeffd534809bdce
Brent S.A. Cowgill <zardoz@infoserve.net>
2019-11-03 12:24:50 +0000

    resolved Added convert-unplayable script using audacity program

---
+++ bin/convert-unplayable.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# find unplayable files and convert them to playable formats
+
+FIND=find-unplayable.sh
+HAS=has-playable.pl
+
+if [ -x ./find-unplayable.sh ]; then
+	FIND=./find-unplayable.sh
+fi
+echo Finding unplayable files with: $FIND
+$FIND | $HAS --not
+
+TOTAL=`$FIND | $HAS --not | wc -l`
+
+while [ 0 != $TOTAL ]
+do
+	echo
+	echo $TOTAL unplayable sound files remain.
+	file=`$FIND | $HAS --not | head -1`
+	echo "$file"
+	echo Press ENTER to manually convert this file to .mp3, .wav, .ogg with audacity...
+	read wait
+	audacity "$file" 2> /dev/null
+	TOTAL=`$FIND | $HAS --not | wc -l`
+done
+echo No unplayable sound files found.
