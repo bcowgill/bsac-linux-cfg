8c968733e9222cb5599a19c913e3dbf9677e3ca9
Brent S.A. Cowgill <zardoz@infoserve.net>
2020-01-02 19:34:09 +0000

    resolved In image-crop-rotate.sh added usage function

--- bin/image-crop-rotate.sh
+++ bin/image-crop-rotate.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
-# process images by rotating and cropping them as needed
+USAGE=1
 PREFIX=${1:-processed-}
 ROTATE=${2:-90}
 # crop percentage
 WIDTH=${3:-55.6}
 HEIGHT=${4:-81}
 
+function usage {
+	cmd=$(basename $0)
+	echo "
+usage: $cmd [--help] prefix rotate width height
+
+Process images by rotating and cropping them as needed.
+
+This will process all the image files in the current directory and rename them with the prefix provided.
+
+Warning: it will first delete all image files which match the prefix. default is $PREFIX
+
+A rotate amount of 90 is 90 degrees clockwise and -90 is 90 degrees counter-clockwise. default is $ROTATE
+
+The width and height values are percentages. default is [$WIDTH % x $HEIGHT %]
+"
+	exit 0
+}
+
+[ "$PREFIX" == "--help" ] && usage
+[ "$PREFIX" == "--man" ] && usage
+[ "$PREFIX" == "-?" ] && usage
+
 rm $PREFIX*.jpg
 for photo in `ls *.jpg *.JPG *.png *.PNG`;
 do
+	USAGE=0
 	identify $photo
 	IMAGE_WIDTH=`identify -format "%w" $photo`
 	IMAGE_HEIGHT=`identify -format "%h" $photo`
@@ -18,3 +41,7 @@ do
 	convert $photo -crop ${CROP_WIDTH}x${CROP_HEIGHT}+0+0 -rotate $ROTATE $PREFIX$photo
 	display $PREFIX$photo
 done
+
+if [ $USAGE == 1 ]; then
+	usage
+fi
