3311a267ad881a6b80c5eac55d19ecdfca61c9a8
Brent S.A. Cowgill <zardoz@infoserve.net>
2020-09-10 08:21:10 +0100

    wip Added rename-files.sh for renaming photos from SD cards

---
+++ bin/rename-files.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [ -z "$2" ]; then
+	echo "
+$0 match replace [--exec]
+
+--exec   specify to cause the files to be renamed.
+
+Attempts to rename files in the current directory that match the match string.
+The new file name will be converted to lower case letters.
+By default it only shows how the files would be renamed but doesn\'t do it.
+
+Example:
+$0 imag photos-today-
+
+Renames IMAG0000.AVI to photos-today-0000.avi
+
+"
+	exit 0
+fi
+
+export MATCH="$1"
+export REPLACE="$2"
+export EXEC="$3"
+ls | perl -MFile::Copy -ne '
+	chomp;
+	$from = $_;
+	$to = lc($from);
+	$to =~ s{$ENV{MATCH}}{$ENV{REPLACE}}i;
+	if ($from ne $to) {
+		++$moved;
+		print qq{mv "$from" "$to"\n};
+		File::Copy::mv($from, $to) if ($ENV{EXEC});
+	}
+	END {
+		$tense = $ENV{EXEC} ? "were" : "would be";
+		$suffix = $ENV{EXEC} ? "" : "  Specify --exec to rename them.";
+		if ($moved)
+		{
+			print qq{$moved files $tense renamed.$suffix\n};
+		}
+		else
+		{
+			print qq{No files $tense renamed.\n};
+		}
+	}
+'
