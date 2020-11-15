027831b3c1da15728c80f7d2731fcc11c7967c3d
Brent S.A. Cowgill <zardoz@infoserve.net>
2020-11-15 12:01:05 +0000

    wip In datestamp.sh documented win/mac version works

--- bin/datestamp.sh
+++ bin/datestamp.sh
@@ -1,8 +1,18 @@
-# output an rfc-3339 timestamp.
+# output an rfc-3339 timestamp. or something close
 # 2017-11-07 12:32:52+00:00
 if date --rfc-3339=seconds 2> /dev/null; then
 	exit 0
 else
+	# windows and Mac
 #	date '+%Y-%m-%d %H:%M:%S%:z' on linux it would be
 	date '+%Y-%m-%d %H:%M:%S%z' | perl -pne 's{(\d\d \s*) \z}{:$1}xmsg'
 fi
+exit
+# fall back to perl
+perl -MPOSIX -e '
+	my $z = strftime("%z", localtime);
+	$z =~ s{(\d)(\d\d)\z}{$1:$2}xms;
+	my $date = strftime("%Y-%m-%d %H:%M:%S$z\n",localtime);
+	print $date;
+'
+exit
