694b60140f8d35a2ff007c0d3100c3d3fc120818
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2021-02-24 13:51:52 +0000

    wip Improve renumber-by-time.sh handle arguments, show help

--- bin/renumber-by-time.sh
+++ bin/renumber-by-time.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
-# CUSTOM some custom changes must be made to use this on different files.
+# Renumber/rename screen shot files by their timestamp.
 
 TEST=1
+if [ "$1" == "--go" ]; then
+	TEST=
+	shift
+fi
+PREFIX="$1"
+EXT="${2:-png}"
+if [ "$3" == "--go" ]; then
+	TEST=
+fi
 
 function usage {
 	local code
 	code=$1
 	cmd=$(basename $0)
 	echo "
-$cmd [--help|--man|-?]
+$cmd [--help|--man|-?] [--go] [prefix] [extension]
 
-Renumber files which may already have numbers in them by timestamp.
-Needs custom configuration to work defaults to .png files only.
+Renumber or rename files which may already have numbers in them by timestamp.
 
---man   Shows help for this tool.
---help  Shows help for this tool.
--?      Shows help for this tool.
+prefix    optional New file name prefix for numbered files.
+extension optional File extension for files to rename. defaults to $EXT
+--go      Make it so.  Default is to show what would happen but not do it.
+--man     Shows help for this tool.
+--help    Shows help for this tool.
+-?        Shows help for this tool.
+
+Creates a temporary directory 'done' to move the files to and a go.sh shell script to do the moving.
 
 See also renumber-files.sh, rename-files.sh, auto-rename.pl, choose.pl, cp-random.pl
 
@@ -32,23 +45,26 @@ if [ "$1" == "-?" ]; then
 	usage 0
 fi
 
-[ "$TEST" == 1 ] && echo TEST mode, will not make any changes.
 GO=
-[ "$TEST" == 1 ] && GO=--append
+[ "$TEST" == 1 ] && GO=-a
 
 mkdir -p done
 ls -rt | grep png | \
-	perl -pne '
+	EXT="$EXT" PREFIX="$PREFIX" perl -pne '
 	chomp;
 	my $in = $_;
-	my $out = $_;
+	my $out = $ENV{PREFIX} ? "$ENV{PREFIX}.$ENV{EXT}" : $_;
 	++$num;
 	$num = "0$num" if length($num) < 2;
-	$out =~ s{(-\d+)?(\.png)}{-0$num$2}xms;
-	$_ = qq{mv $in done/$out\n};
+	$out =~ s{(-\d+)?(\.$ENV{EXT})}{-0$num$2}xms;
+	$_ = qq{mv "$in" "done/$out"\n};
 ' | tee $GO go.sh
+
+[ "$TEST" == 1 ] && echo TEST mode, did not make any changes. Run ./go.sh to make changes if all looks well.
+
 chmod +x go.sh
 if [ -z "$TEST" ]; then
 	./go.sh
-	mv done/*.png .
+	mv done/*.$EXT .
+	rmdir done
 fi
