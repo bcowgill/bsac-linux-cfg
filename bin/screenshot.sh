c327f67ad0ab171d22c74175ce0dae0c601f925f
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2021-02-26 08:42:55 +0000

    wip Changed screenshot.sh to default to select an area to grab

--- bin/screenshot.sh
+++ bin/screenshot.sh
@@ -2,11 +2,16 @@
 # BSACSYS Part of Brent S.A. Cowgill's System Toolkit
 # start tool to take a screenshot interactively and keep it running
 
+ARGS=--area
+if [ ! -z "$1" ]; then
+	ARGS="$*"
+fi
+
 echo Delete the file ~/_DELETE_TO_STOP_SCREENSHOTS to terminate screen shot app.
 touch ~/_DELETE_TO_STOP_SCREENSHOTS
 while [ -e ~/_DELETE_TO_STOP_SCREENSHOTS ]
 do
 	if [ `pswide.sh | grep gnome-screenshot | grep -v grep | wc -l` == "0" ]; then
-		gnome-screenshot --interactive
+		gnome-screenshot --interactive $ARGS
 	fi
 done
