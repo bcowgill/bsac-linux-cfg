10223dc8c2296a28afe2b0813a966de97b6a5d44
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2021-05-07 12:14:54 +0100

    wip Added rvmv.sh and rvcp.sh scripts to move files to a destination

---
+++ bin/rvcp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# BSACSYS Part of Brent S.A. Cowgill's System Toolkit
+# reverse copy give a destination first then file/pattern good for sending a number of files to the same place in successive commands
+cp -t "$1" "$2"
---
+++ bin/rvmv.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# BSACSYS Part of Brent S.A. Cowgill's System Toolkit
+# reverse move give a destination first then file/pattern good for sending a number of files to the same place in successive commands.
+mv -t "$1" "$2"
