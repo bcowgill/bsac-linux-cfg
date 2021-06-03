760e9570b1d56c9b447105fb724ab1fcadc6c432
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2021-06-03 07:43:46 +0100

    wip Added xkcd-assorted-colors-48.js most common 48 color definitions

---
+++ bin/template/html/color/xkcd-assorted-colors-48.js
@@ -0,0 +1,57 @@
+// 48 most commonly known colors
+
+export const purple = '#7e1e9c';
+export const green = '#15b01a';
+export const blue = '#0343df';
+export const pink = '#ff81c0';
+export const brown = '#6d3700';
+export const red = '#e50000';
+
+export const lightBlue = '#95d0fc';
+export const teal = '#029386';
+export const orange = '#f97306';
+export const lightGreen = '#96f97b';
+export const magenta = '#c20078';
+export const yellow = '#ffff14';
+
+export const skyBlue= '#75bbfd';
+export const grey = '#929591';
+export const limeGreen = '#89fe05';
+export const lightPurple = '#bf77f6';
+export const violet = '#9a0eea';
+export const darkGreen = '#033500';
+
+export const turquoise = '#06c2ac';
+export const lavender = '#c79fef';
+export const darkBlue = '#00035b';
+export const tan = '#d1b26f';
+export const cyan = '#00ffff';
+export const aqua = '#13eac9';
+
+export const forestGreen = '#06470c';
+export const mauve = '#ae7181';
+export const darkPurple = '#35063e';
+export const brightGreen = '#01ff07';
+export const maroon = '#650021';
+export const olive = '#6e750e';
+
+export const salmon = '#ff796c';
+export const beige = '#e6daa6';
+export const royalBlue= '#0504aa';
+export const navyBlue = '#001146';
+export const lilac = '#cea2fd';
+export const black= '#000';
+
+export const hotPink = '#ff028d';
+export const lightBrown = '#ad8150';
+export const paleGreen = '#c7fdb5';
+export const peach = '#ffb07c';
+export const oliveGreen = '#677a04';
+export const darkPink = '#cb416b';
+
+export const periwinkle = '#8e82fe';
+export const seaGreen = '#53fca1';
+export const lime = '#aaff32';
+export const indigo = '#380282';
+export const mustard = '#ceb301';
+export const lightPink = '#ffd1df';
