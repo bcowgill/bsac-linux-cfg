02214cf18a046847a9805f686a3a60b0fcd9fd5c
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2021-06-03 12:05:24 +0100

    wip In find-sum.sh implemented linux version.

--- bin/find-sum.sh
+++ bin/find-sum.sh
@@ -1,35 +1,59 @@
 #!/bin/bash
 # BSACKIT Part of Brent S.A. Cowgill's Developer Toolkit
 # find-sum easy find shows just checksum, size, name and symlink destination
+# caveat -- only works well with file names that have no spaces in them.
 
-# 3730389851 48K ./IntroConversationBgrMobile.jpg
-# 324824302 25B ./Logotype.js -> ../mc-demo-eu/Logotype.js
+# using cksum:
+# 677252015 9 "./list.txt"
+# 677252015 9 "./xxx.lse" -> "list.txt"
 
-SUM=cksum
+# using md5sum:
+# 8052fdd2c8af1a27262a9210acd1fe58 "./list.txt"
+# 8052fdd2c8af1a27262a9210acd1fe58 "./xxx.lse" -> "list.txt"
+
+if [ -z "$SUM" ]; then
+	SUM=cksum
+	if which md5sum > /dev/null; then
+		SUM=md5sum
+	fi
+fi
+#echo SUM=$SUM
 
 find-sum () {
 	local sourceDir
 	sourceDir="$1"
 	if which sw_vers > /dev/null 2>&1 ; then
 		# MACOS
-    # -rw-r--r--@ 1 bcowgill  staff    48K 30 Apr 06:25 ./IntroConversationBgrMobile.jpg
-    # lrwxr-xr-x  1 bcowgill  staff    25B  3 Jun 10:27 ./Logotype.js -> ../mc-demo-eu/Logotype.js
-    # first convert to simple output size, name, target name
-    # 25B ./Logotype.js -> ../mc-demo-eu/Logotype.js
-    # 303B ./config.js
-    # then add in checksum as prefix to line
-    # 714223608 303 config.js
+		# -rw-r--r--@ 1 bcowgill  staff    48K 30 Apr 06:25 ./IntroConversationBgrMobile.jpg
+		# lrwxr-xr-x  1 bcowgill  staff    25B  3 Jun 10:27 ./Logotype.js -> ../mc-demo-eu/Logotype.js
+		# first convert to simple output name, target name
+		# ./Logotype.js -> ../mc-demo-eu/Logotype.js
+		# ./config.js
+		# then add in checksum as prefix to line
+		# 714223608 303 config.js
 		pushd "$sourceDir" > /dev/null \
-      && pwd | perl -pne 's{\A.+/(src/)}{$1}xms' \
-      && find -s . \( -type f -o -type l \) -exec ls -lh {} \; \
-      |  grep -v DS_Store \
-      | perl -pne 's{\A.+?([0-9.]+[A-Z])\s+.+?\.}{$1 .}xms' \
-      | SUM=$SUM perl -pne 'm{\s(\S+)}xms; my @checksum = split(/\s+/, `$ENV{SUM} $1`); $_ = qq{$checksum[0] $_}' \
-      && popd > /dev/null
+			&& pwd | perl -pne 's{\A.+/(src/)}{$1}xms' \
+			&& find -s . \( -type f -o -type l \) -exec ls -lh {} \; \
+			| grep -v DS_Store \
+			| perl -pne 's{\A.+?\s+(\./)}{$1}xms' \
+			| SUM=$SUM perl -pne 'm{\A(\S+)}xms; my @checksum = split(/\s+/, `$ENV{SUM} $1`); $_ = qq{$checksum[0] $_}' \
+		&& popd > /dev/null
 	else
 		# linux output:
-		# "./diffemacs.sh"	136	2016-08-22+00:59:01.5543460410
-		pushd "$sourceDir" > /dev/null && find . \( -type f -o -type l \) -printf '"%h/%f"\t%s\t%T+\n' && popd > /dev/null
+		# lrwxrwxrwx 1 me me 8 Jun  3 11:39 ./xxx.lse -> list.txt
+		# -rw-rw-r-- 1 me me 9 Jun  3 11:37 ./list.txt
+		pushd "$sourceDir" > /dev/null \
+			&& find . \( -type f -o -type l \) -printf '"%h/%f" -> "%l"\n' \
+			| perl -pne 's{\s+->\s+""}{}xms' \
+			| SUM=$SUM perl -pne '
+				my @split = split(/\s+->\s+/, $_);
+				my $file = $split[0];
+				my @checksum = split(/\s+/, `$ENV{SUM} $file`);
+				pop(@checksum);
+				my $checksum = join(" ", @checksum);
+				$_ = qq{$checksum $_}
+			' | sort \
+		&& popd > /dev/null
 	fi
 }
 
