9f0944fd6ac336fcbe453ce207a95476fb562da9
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2021-06-07 16:07:49 +0100

    wip In find-sum.sh begin addition of windows special case

--- bin/find-sum.sh
+++ bin/find-sum.sh
@@ -100,25 +100,33 @@ find-sum () {
 		&& popd > /dev/null
 	else
 		if [ "$OSTYPE" == "msys" ]; then
-			# windows
-			echo No support for windows as yet.
+			# windows output: (no symlinks, emulated by .wym files instead)
+			# lrwxrwxrwx 1 me me 8 Jun  3 11:39 ./xxx.lse
+			# lrwxrwxrwx 1 me me 8 Jun  3 11:39 ./xxx.lse.wym
+			# -rw-rw-r-- 1 me me 9 Jun  3 11:37 ./list.txt
 			# TODO as there are no symlinks allowed, all we can do is examine existing inheritance.lst files
 			# and update the entries that are not SYM links. We rely on wymlink -c to check that file contents
 			# of symlinks haven't changed.  If they do, the Windows developer will have to manually change the
 			# inheritance.lst file and update the checksum manually.
-			exit 1
+			pushd "$sourceDir" > /dev/null \
+				&& pwd | perl -pne 's{\A.+/(src/)}{$1}xms' \
+				&& find . \( -type f \) -exec ls -lh {} \; \
+				| filter-out \
+				| grep -vE '\.wym\s*$' \
+				| add-checksums \
+			&& popd > /dev/null
+		else
+			# linux output:
+			# lrwxrwxrwx 1 me me 8 Jun  3 11:39 ./xxx.lse -> list.txt
+			# -rw-rw-r-- 1 me me 9 Jun  3 11:37 ./list.txt
+			pushd "$sourceDir" > /dev/null \
+				&& pwd | perl -pne 's{\A.+/(src/)}{$1}xms' \
+				&& find . \( -type f -o -type l \) -printf '"%h/%f" -> "%l"\n' \
+				| filter-out \
+				| perl -pne 's{\s+->\s+""}{}xms' \
+				| add-checksums \
+			&& popd > /dev/null
 		fi
-
-		# linux output:
-		# lrwxrwxrwx 1 me me 8 Jun  3 11:39 ./xxx.lse -> list.txt
-		# -rw-rw-r-- 1 me me 9 Jun  3 11:37 ./list.txt
-		pushd "$sourceDir" > /dev/null \
-			&& pwd | perl -pne 's{\A.+/(src/)}{$1}xms' \
-			&& find . \( -type f -o -type l \) -printf '"%h/%f" -> "%l"\n' \
-			| filter-out \
-			| perl -pne 's{\s+->\s+""}{}xms' \
-			| add-checksums \
-		&& popd > /dev/null
 	fi
 }
 
