e1d2647293b459352508a04a63168dd6a0bd02c7
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2021-12-22 17:53:47 +0000

    wip In birdsong.sh prevent the same bird from singing at same time

--- bin/birdsong.sh
+++ bin/birdsong.sh
@@ -1,17 +1,59 @@
 #!/bin/bash
 
-DIR=${1:-$HOME/d/Music/_Ringtones/birdsong}
-LAST=1
-SONG=1
+LAST=silence
+SONG=$LAST
 NORM=-3
+PARAM=--no-show-progress
+
+DIR=${1:-$HOME/d/Music/_Ringtones/birdsong}
+DICE=${2:-50}
+EXCLUDE=${3:-wood-pigeon}
+EXT=mp3
+
+function usage {
+	local code
+	code=$1
+	cmd=$(basename $0)
+	echo "
+$cmd [--help|--man|-?] [directory] [delay_dice] [exclude]
+
+This will play random sound files [.$EXT] from a given directory with a random delay between them.
+
+directory   The directory to choose sound files from. default is "$DIR"
+delay_dice  The number of standard dice to roll to determine the delay in seconds. default is $DICE
+exclude     The regex pattern to use for excluding chosen sound files. default is "$EXCLUDE"
+--man   Shows help for this tool.
+--help  Shows help for this tool.
+-?      Shows help for this tool.
+
+When choosing a sound file to play, it will force the next file to be different from the previous file.  It will also choose a file that is not currently being played by another instance of $cmd.
+
+It will also filter out any file that matches "$EXCLUDE"
+
+See also choose.pl, pswide.sh
+
+"
+	exit $code
+}
+
+if [ "$1" == "--help" ]; then
+	usage 0
+fi
+if [ "$1" == "--man" ]; then
+	usage 0
+fi
+if [ "$1" == "-?" ]; then
+	usage 0
+fi
 
 while [ ! -z "$SONG" ];
 do
 	#echo 1:$LAST 2:$SONG
 	if [ "$LAST" == "$SONG" ]; then
-		SONG=`ls -1 $DIR/*.mp3 | grep -v pigeon | choose.pl`
+		SONG=`ls -1 $DIR/*.$EXT | grep -vE "$EXCLUDE" | choose.pl`
 		# pick another bird if the chosen one is currently singing.
-		if pswide.sh | grep -v grep | grep "$SONG" 2> /dev/null; then
+		if pswide.sh | grep -v grep | grep $PARAM | grep "$SONG" 2> /dev/null > /dev/null; then
+			echo $SONG is already being sung...
 			SONG="$LAST"
 		fi
 	fi
@@ -20,11 +62,11 @@ do
 		if which exiftool > /dev/null; then
 			exiftool "$SONG" |grep -E 'Title|Duration'
 		fi
-		play --no-show-progress "$SONG"
-		#play --norm=$NORM "$SONG"
+		play $PARAM "$SONG"
+		#play $PARAM --norm=$NORM "$SONG"
 		LAST="$SONG"
 
-		SLEEP=`perl -e 'sub r{return int(1+rand(6))}; for (1..50) {$d += r()}; print $d'`
+		SLEEP=`DICE=$DICE perl -e 'sub r{return int(1+rand(6))}; for (1..$ENV{DICE}) {$d += r()}; print $d'`
 		echo sleep $SLEEP
 		sleep $SLEEP
 	fi
