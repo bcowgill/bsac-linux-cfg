992ea84c0605d5f6a46554abcf89e0f2361b46ed
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2022-01-09 16:47:44 +0000

    wip Added cuckoo.sh and cuckoo sound files to make a cuckoo clock

---
+++ bin/cuckoo.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Sound off a number of cuckoo chimes based on the current hour.
+# BSACKIT Part of Brent S.A. Cowgill's Developer Toolkit
+
+CUCKOO=${1:-$HOME/bin/sounds/single-cuckoo-sound.mp3}
+DELAY=0
+
+hour=`date '+%I'`
+hour=$(($hour + 0))
+while [ $hour != "0" ];
+do
+	if [ -f "$CUCKOO" ]; then
+		sound-play.sh $CUCKOO
+	else
+		echo CUCKOO $hour!
+	fi
+	sleep $DELAY
+	hour=$(($hour - 1))
+done
+
+exit
+# crontab -e line to add to sound an hourly cuckoo clock
+0 * * * * $HOME/bin/cuckoo.sh > /tmp/$LOGNAME/crontab-cuckoo.log  2>&1
