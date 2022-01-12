28e25022e2c5eabe81ea512807cdde5e43fae089
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2022-01-12 16:42:25 +0000

    wip In cuckoo.sh MACOS plays a tiger sound for cuckoo

--- bin/cuckoo.sh
+++ bin/cuckoo.sh
@@ -2,7 +2,19 @@
 # Sound off a number of cuckoo chimes based on the current hour.
 # BSACKIT Part of Brent S.A. Cowgill's Developer Toolkit
 
-CUCKOO=${1:-$HOME/bin/sounds/single-cuckoo-sound.mp3}
+# TODO select sound by name cuckoo.sh frog
+# TODO change sound used on Mac/Linux by day of week!
+# date +%u
+# Nature sound effects: https://soundbible.com/search.php?q=cricket
+
+CLUCK=$HOME/bin/sounds/single-cuckoo-sound.mp3
+TIGER="$HOME/bin/sounds/Tiger Growling-SoundBible.com-258880045.mp3"
+if which sw_vers > /dev/null 2>&1 ; then
+	# MACOS
+	CLUCK="$TIGER"
+fi
+
+CUCKOO="${1:-$CLUCK}"
 DELAY=0
 
 hour=`date '+%I' | perl -pne 's{\A\s*0([0-9])}{$1}xms'`
@@ -10,7 +22,7 @@ hour=`date '+%I' | perl -pne 's{\A\s*0([0-9])}{$1}xms'`
 while [ $hour != "0" ];
 do
 	if [ -f "$CUCKOO" ]; then
-		sound-play.sh $CUCKOO
+		sound-play.sh "$CUCKOO"
 	else
 		echo CUCKOO $hour!
 	fi
