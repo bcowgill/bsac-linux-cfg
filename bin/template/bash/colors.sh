a1bf389b060dbf1dfb9344a91984632d59426414
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2022-01-14 14:07:19 +0000

    wip In colors.sh hi() function improved usability

--- bin/template/bash/colors.sh
+++ bin/template/bash/colors.sh
@@ -5,32 +5,58 @@ PURPLE='\033[0;35m'
 NC='\033[0;0m' # No Color
 
 function out {
-  echo -e "$*${NC}"
+	echo -e "$*${NC}"
 }
 
 function ok {
-  out "🆗 ${GREEN}$*"
-}
-
-function hi {
-  local RESTORE
-  RESTORE=${2:-$NC}
-  echo -n "${PURPLE}$1${RESTORE}"
+	out "🆗 ${GREEN}$*"
 }
 
 function warning {
-  out "${YELLOW}⚠️  $*"
+	out "${YELLOW}⚠️  $*"
 }
 
 function error {
-    ERROR=${1:-0}
-    out "${RED}❌ ${@:2}"
+	ERROR=${1:-0}
+	out "${RED}❌ ${@:2}"
 }
 
+function hi {
+	local HIGH RESTORE
+	RESTORE=${2:-$NC}
+	HIGH=${3:-$PURPLE}
+	if [ $RESTORE == "ok" ]; then
+		RESTORE=$GREEN
+	elif [ $RESTORE == "out" ]; then
+		RESTORE=$NC
+	elif [ $RESTORE == "warn" ]; then
+		RESTORE=$YELLOW
+	elif [ $RESTORE == "warning" ]; then
+		RESTORE=$YELLOW
+	elif [ $RESTORE == "error" ]; then
+		RESTORE=$RED
+	elif [ $RESTORE == "err" ]; then
+		RESTORE=$RED
+	fi
+	if [ $HIGH == "ok" ]; then
+		HIGH=$GREEN
+	elif [ $HIGH == "out" ]; then
+		HIGH=$NC
+	elif [ $HIGH == "warn" ]; then
+		HIGH=$YELLOW
+	elif [ $HIGH == "warning" ]; then
+		HIGH=$YELLOW
+	elif [ $HIGH == "error" ]; then
+		HIGH=$RED
+	elif [ $HIGH == "err" ]; then
+		HIGH=$RED
+	fi
+	echo -n "${HIGH}$1${RESTORE}"
+}
 
 echo "This is a normal echo message."
-out "This is an out  message with a [`hi highlight`]."
-ok "This is an ok message with [`hi "a highlight" $GREEN`] in color."
-warning "This is a warning in [`hi color $YELLOW`]!"
-error 12 "This is an error in [`hi color $RED`]!"
+out "This is an out message with a [`hi highlight`]."
+ok "This is an ok message with [`hi "a highlight" ok`] in color."
+warning "This is a warning in [`hi color warning out`]!"
+error 12 "This is an error in [`hi color error`]!"
 echo "This is a normal echo message."
