5b063e7f51d10ffb25c64ed0da5e7b524f777a97
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2022-05-23 17:18:46 +0100

    wip In shu.sh debugging turned on to see why not working

--- bin/shu.sh
+++ bin/shu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # silence the cuckoo clock if running.
 ps | grep bin/cuckoo.sh | grep -v grep
+set -x
 PID=`ps | grep bin/cuckoo.sh | grep -v grep | perl -pne '@x=split(/\s+/); $_ = $x[0]'`
 if [ ! -z "$PID" ]; then
 	ps -p $PID
