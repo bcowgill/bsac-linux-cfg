ca98d0969e54c29d84f1646c80ffea5fdd7bbcfa
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2022-05-23 12:43:13 +0100

    wip In shu.sh add debugging of process ids

--- bin/shu.sh
+++ bin/shu.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # silence the cuckoo clock if running.
+ps | grep bin/cuckoo.sh | grep -v grep
 PID=`ps | grep bin/cuckoo.sh | grep -v grep | perl -pne '@x=split(/\s+/); $_ = $x[0]'`
 if [ ! -z "$PID" ]; then
 	ps -p $PID
