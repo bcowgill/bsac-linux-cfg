5e9cf7ed503cb3b54520558a2d41481b4e29fd91
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2022-10-09 11:08:40 +0100

    wip Added ls-lower.sh and ls-upper.sh to show variations of letters

---
+++ bin/ls-lower.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# BSACKIT Part of Brent S.A. Cowgill's Developer Toolkit
+# WINDEV tool useful on windows development machine
+
+function usage {
+	local code
+	code=$1
+	cmd=$(basename $0)
+	echo "
+$cmd [--help|--man|-?] [letter]
+
+This will list all the lower case latin or cyrillic variations of the letter specified.
+
+letter  A letter from a to z to display variations for. If omitted it shows all variations for all letters a-z.
+--man   Shows help for this tool.
+--help  Shows help for this tool.
+-?      Shows help for this tool.
+
+See also ls-upper.sh, grep-utf8.sh
+
+Example:
+
+$cmd d
+"
+	exit $code
+}
+if [ "$1" == "--help" ]; then
+	usage 0
+fi
+if [ "$1" == "--man" ]; then
+	usage 0
+fi
+if [ "$1" == "-?" ]; then
+	usage 0
+fi
+
+LETTER=$1
+
+if [ -z "$LETTER" ]; then
+	for letter in a b c d e f g h i j k l m n o p q r s t u v w x y z; do
+		$0 $letter
+	done
+else
+	grep-utf8.sh "letter $LETTER\b" | grep -vE 'CAPITAL|\[(NonspacingMark|Format)\]' | grep -E 'LATIN|CYRILLIC' | perl -pne 's{\A(\S+)\s+.+\z}{ $1 }xms; END { print qq{\n\n}}'
+fi
