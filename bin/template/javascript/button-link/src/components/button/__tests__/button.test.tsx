3260ec757d66a5f38218fb4e172e441cd1a6c110
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2023-02-03 15:12:24 +0000

    wip In button-link documented how well ChatGPT did in writing tests for me

--- bin/template/javascript/button-link/src/components/button/__tests__/button.test.tsx
+++ bin/template/javascript/button-link/src/components/button/__tests__/button.test.tsx
@@ -2,6 +2,26 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { Button } from '../button';
 
+/*
+  Test of ChatGPT writing React component unit tests.
+
+  Tests:       2 failed, 5 passed, 7 total
+    button.tsx    |     100 |    91.66 |     100 |     100 | 50-53
+
+  Coverage very high but failed to write tests for default prop values, leaving three lines uncovered.
+  Also 2 tests failed initially - it wrote getByTestId with the wrong expected testId values
+
+  Then I manually fixed the tests, and wrote the additional tests to cover the lines, ending with:
+
+  Tests:       11 passed, 11 total
+    button.tsx    |     100 |      100 |     100 |     100 |  
+
+  So had to write 4 more tests for full coverage.
+  ChatGPT did 64% of the job for me.
+  And there was actually a bug in the code which was discovered by the tests it had written.
+
+ */
+
 const displayName = 'Button';
 
 describe(`${displayName} component`, () => {
