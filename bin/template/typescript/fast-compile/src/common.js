31a45cac840a77a77bf361fc73000e5a7d314723
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2023-02-06 19:27:24 +0000

    wip In fast-compile added sample typescript error

--- bin/template/typescript/fast-compile/src/common.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.B = exports.A = void 0;
-exports.A = 'foo';
-exports.B = 'bar';
--- bin/template/typescript/fast-compile/src/index.ts
+++ bin/template/typescript/fast-compile/src/index.ts
@@ -2,3 +2,13 @@
 import { B } from "./common.ts"; // for npm run fast / compile with typescript@beta == 5
 
 console.log(B)
+
+/* sample TS type error
+const user = {
+  firstName: "Angela",
+  lastName: "Davis",
+  role: "Professor",
+}
+
+console.log(user.name)
+*/
