f471e4624a91780359b2e8625e11cba732287f2b
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2023-02-17 16:08:23 +0000

    wip In fast-compile test JSON.stringify replacer function for PII

--- bin/template/typescript/fast-compile/src/__test__/pii.test.ts
+++ bin/template/typescript/fast-compile/src/__test__/pii.test.ts
@@ -3,6 +3,7 @@ import * as TestMe from '../pii.ts'
 import { suite, describe, log } from './lib.ts'
 
 suite('pii module tests', function descPIISuite() {
+	const DEBUG = false
 	const longer = 4
 	const password = 'pa6sword i$ obscuRed fu11y'
 
@@ -101,4 +102,118 @@ suite('pii module tests', function descPIISuite() {
 			'uuid v4 obscured properly',
 		)
 	}) // obscureInfo
+
+	describe('JSON.stringify() with replacer', function descObscureInfo() {
+		const data = {
+			id: 234134,
+			system: {
+				password:
+					'the password is a real secret and should not be shared',
+				isEnrolled: true,
+			},
+			name: 'Frankie Hollywood',
+			age: 45,
+			height: 6.5,
+			weight: 76,
+			job: 'Janitor',
+			address: '123 Shinging Crescent, Anyville, Wyoming, 23123',
+			financials: {
+				sortCode: '99-23-12',
+				accountNo: '739432912',
+				cardNo: '127409231234',
+			},
+			removeThis: 'should not appear in stringified output at all',
+			boolNullable: false,
+			productNullable: 'Gizmos',
+			ssnPII: '734942145',
+		}
+
+		function fnReplacer(key: string, value: any) {
+			if (DEBUG) {
+				console.warn(
+					`fnReplacer key[${key}] this:`,
+					this,
+					'value<',
+					value,
+					'>',
+				)
+			}
+			if (key === '') {
+				return value
+			}
+
+			// Keys which should NOT be included in stringified object
+			// example only, specific key name
+			if (/^removethis$/i.test(key)) {
+				return void 0
+			}
+
+			// Keys which should become null if they have no truthy value
+			// example only, anything ending with nullable (ignoring case)
+			if (/nullable$/i.test(key)) {
+				return value || null
+			}
+
+			// Keys which contain passwords to be fully obscured
+			// anything with password anywhere within it (ignoring case)
+			if (/password/i.test(key)) {
+				return TestMe.obscurePassword(value)
+			}
+
+			// Keys which contain personally identifiable information and should be partially obscured
+			// anything ending with PII (ignoring case)
+			// and some fully qualified specific field names
+			if (
+				/pii$/i.test(key) ||
+				/^(name|address|sortcode|accountno|cardno)$/i.test(key)
+			) {
+				return TestMe.obscureInfo(value)
+			}
+
+			// All other keys are passed through as they are
+			return value
+		}
+
+		const publicKeys = 'id name address'.split(/ /g)
+
+		assert.deepEqual(
+			JSON.parse(JSON.stringify(data, publicKeys)),
+			{
+				id: 234134,
+				name: 'Frankie Hollywood',
+				address: '123 Shinging Crescent, Anyville, Wyoming, 23123',
+			},
+			'stringify with PII obscured',
+		)
+
+		const result = JSON.parse(JSON.stringify(data, fnReplacer))
+		assert.match(
+			result.system.password,
+			/^\*+$/,
+			'password has been fully obscured',
+		)
+		result.system.password = '*****'
+		assert.deepEqual(
+			result,
+			{
+				id: 234134,
+				system: { password: '*****', isEnrolled: true },
+				name: 'F*****e H*******d',
+				age: 45,
+				height: 6.5,
+				weight: 76,
+				job: 'Janitor',
+				address: '103 S******g C******t, A******e, W*****g, 20003',
+				financials: {
+					sortCode: '11-11-11',
+					accountNo: '700000002',
+					cardNo: '100000000004',
+				},
+				boolNullable: null,
+				productNullable: 'Gizmos',
+				ssnPII: '700000005',
+			},
+			'stringify with PII obscured',
+		)
+	}) // JSON.stringify(..., replacer)
 }) // pii module tests
