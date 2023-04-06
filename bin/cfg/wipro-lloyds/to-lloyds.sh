6a7e115a1b0232d314efb26f4e38b0e85adc68d2
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2023-04-06 07:40:52 +0100

    wip In wipro lloyds config added md5sum.lst and md5sum.sh to list of tools

--- bin/cfg/wipro-lloyds/to-lloyds.sh
+++ bin/cfg/wipro-lloyds/to-lloyds.sh
@@ -43,6 +43,7 @@ cp ~/bin/find-bak.sh \
 	~/bin/ls-tabs.pl \
 	~/bin/fix-tabs.sh \
 	~/bin/fix-spaces.sh \
+	~/bin/md5sum.sh \
 	~/bin/mad.sh \
 	~/bin/sad.sh \
 	~/bin/glad.sh \
@@ -57,14 +58,3 @@ cp ~/bin/find-bak.sh \
 	$B
 
 md5sum.sh tx | tee md5sum.lst
-exit $?
-Sample Linux output with md5sum command...
-119145aa148d9cb14c900370ce7eddcd  tx/c/d/bin/pee.pl
-7e795e8d7b41fa6adf7a32f2dac061d6  tx/c/d/bin/cover-one.sh
-65966141e5aa8dc7efaab4d360ebff04  tx/c/d/bin/calc.sh
-
-Sample Mac output with md5 command...
-MD5 (tx/c/Users/FILEID/my-git-tools.pl.txt) = e89e97204bc24676c32797b2eb01ff24
-MD5 (tx/c/Users/FILEID/.bash_functions) = ee6bb991de53b138af8fb4d35c924dfe
-MD5 (tx/c/Users/FILEID/kdiff3.config) = 04f17f4b4bbcb6253a4f88a3b5172f3e
-MD5 (tx/c/Users/FILEID/bin/rvdiff.sh) = a353b39332938c43bd9d423db25d698c
