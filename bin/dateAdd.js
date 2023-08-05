618f4ef0fd9500445cb650d76c09853e58634d7e
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2023-08-05 11:20:07 +0100

    wip In dateAdd.js show Local time and yyyy-mm-dd format

--- bin/dateAdd.js
+++ bin/dateAdd.js
@@ -1,10 +1,23 @@
 #!/usr/bin/env node
 // BSACKIT Part of Brent S.A. Cowgill's Developer Toolkit
 
+const zeros = "00000000000000000000";
+function pad(string = "", length  = 2) {
+	if (length > zeros.length) {
+		// double the zero length and try again, or throw...
+		throw new RangeError("parameter ‘length’ [" + length + "] exceeds maximum supported padding of " + zeros.length)
+	}
+	return zeros.substr(0, length - string.toString().length) + string;
+}
+
 Date.prototype.addDays = function(days) {
     var date = new Date(this.valueOf());
     date.setDate(date.getDate() + days);
     return date;
+}
+
+Date.prototype.stringify = function() {
+	return this.getFullYear() + '-' + pad(this.getMonth()+1) + '-' + pad(this.getDate()) + ' ' + this.toLocaleTimeString();
 }
 
 const FIRST_ARG = 2;
@@ -27,9 +40,9 @@ Each additional number provided is added to the original date.
 }
 
 function showAddedDate(date, days) {
-	console.log(date);
+	console.log(date.stringify());
 	console.log(`plus ${days} days`);
-	console.log(date.addDays(days));
+	console.log(date.addDays(days).stringify());
 	console.log('');
 	output = true;
 }
