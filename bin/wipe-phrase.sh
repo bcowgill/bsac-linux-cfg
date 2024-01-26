8745f7e7cdf1b0585793e2f50763bf355fac189f
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2024-01-26 07:16:58 +0000

    wip In wipe.sh added help usage and see also wipe-phrase.sh

--- bin/wipe-phrase.sh
+++ bin/wipe-phrase.sh
@@ -1,9 +1,40 @@
 #!/bin/bash
 # BSACSYS Part of Brent S.A. Cowgill's System Toolkit
-# Wipe free space on device by writing fixed data to it
 
 FILE=FILL-UP-DISK-DELETE.DAT
 CWD=`pwd`
+
+function usage {
+	local code
+	code=$1
+	cmd=$(basename $0)
+	echo "
+$cmd [--help|--man|-?] path phrase...
+
+Wipe free space on a specific path or current directory by writing a provided phrase until it fills up.
+
+path    Specify the device path to fill up. Defaults to current directory.
+phrase  The message to use to write to the free space until it is full.
+--man   Shows help for this tool.
+--help  Shows help for this tool.
+-?      Shows help for this tool.
+
+Creates a file FILL-UP-DISK-DELETE.DAT and sucks up all further disk space by writing to it.
+
+See also wipe.sh lock.sh lockusb.sh forever.pl
+"
+	exit $code
+}
+if [ "$1" == "--help" ]; then
+	usage 0
+fi
+if [ "$1" == "--man" ]; then
+	usage 0
+fi
+if [ "$1" == "-?" ]; then
+	usage 0
+fi
+
 WHERE=${1:-$CWD}
 shift
 
@@ -14,4 +45,3 @@ do
 	forever.pl $* > "$WHERE/$FILE" || (sync && rm "$WHERE/$FILE")
 done
 echo wiped `date` with phrase
-
--- bin/wipe.sh
+++ bin/wipe.sh
@@ -1,10 +1,39 @@
 #!/bin/bash
 # BSACSYS Part of Brent S.A. Cowgill's System Toolkit
-# Wipe free space on device by writing random data to it 5 times
 
 FILE=FILL-UP-DISK-DELETE.DAT
 CWD=`pwd`
 WHERE=${1:-$CWD}
+
+function usage {
+	local code
+	code=$1
+	cmd=$(basename $0)
+	echo "
+$cmd [--help|--man|-?] path
+
+Wipe free space on a specific path or current directory by writing random data to it 5 times until it fills up.
+
+path    Specify the device path to fill up. Defaults to current directory.
+--man   Shows help for this tool.
+--help  Shows help for this tool.
+-?      Shows help for this tool.
+
+Creates a file FILL-UP-DISK-DELETE.DAT and sucks up all further disk space by writing to it.
+
+See also wipe-phrase.sh lock.sh lockusb.sh
+"
+	exit $code
+}
+if [ "$1" == "--help" ]; then
+	usage 0
+fi
+if [ "$1" == "--man" ]; then
+	usage 0
+fi
+if [ "$1" == "-?" ]; then
+	usage 0
+fi
 
 for PASS in 1 2 3 4 5;
 do
