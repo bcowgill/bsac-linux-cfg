3729a2bc74d5d42833a4afa445fc0a5861d67139
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2024-02-02 19:03:57 +0000

    wip In genai updated course notes, ailibs.py and rotate offset diagram

---
+++ bin/template/genai/ailibs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# python 2.7.6 has these libs
+import os
+import io
+#import PIL # not in python 3.4.3
+import ast
+import time
+import shutil
+import datetime
+import requests
+
+# python 3.4.3 but not 2.7.6
+import asyncio
+
+# python 2.7.6 and 3.4.3 does not have these libs... (need to install?)
+#import openai
+#import secret
+#import Random
+#import aiohttp
+#import tkinter # requires python3-tk
+
+#import tkinter.scrolledtext as tks #creates a scrollable text window
+#from datetime import datetime
+#from tkinter import *
+#from PIL import Image, ImageOps
+#from PIL import ImageEnhance, ImageOps
+#from io import BytesIO
