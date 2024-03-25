3ac48ac5d928553388ea7dd688efa280b40ba782
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2024-03-25 13:48:51 +0000

    wip In filter-min.sh cleanup and added unit tests for minifier

--- bin/filter-min.sh
+++ bin/filter-min.sh
@@ -10,15 +10,14 @@ function usage {
 	echo "
 $cmd [--help|--man|-?]
 
-This will filter a list of file names to suppress minified, uglified or packed  web development file extensions
-MUSTDO excluding minimised and .git, node_modules, and bower_components.
+This will filter a list of file names to suppress minified, uglified or packed  web development file extensions.
 
 --regex Shows the regex used for matching web file extensions.
 --man   Shows help for this tool.
 --help  Shows help for this tool.
 -?      Shows help for this tool.
 
-Also filters out debugging .map files.
+Also filters out debugging .map files and .git, node_modules, deno-packages and bower_components.
 
 See also filter-web.sh filter-text.sh, filter-docs.sh, filter-videos.sh, filter-fonts.sh, filter-scripts.sh, filter-zips.sh, filter-css.sh, filter-images.sh, filter-sounds.sh, classify.sh
 
@@ -46,3 +45,7 @@ if [ "$1" == "--regex" ]; then
 fi
 
 $GREP '(\.map|(\.|-)(min|pack)\.((c|le|sa|sc)ss|html?|ts|jsx?|json[5c]?))(:|"|\s*$)' $* # .min.css -min.css .pack.css -pack.css .css .less .sass .scss .htm .html .ts .js .jsx .json .json5 .map
+ERR=$?
+if [ $ERR != 0 ]; then
+	usage $ERR
+fi
