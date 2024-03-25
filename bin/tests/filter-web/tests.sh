ddc97f3590af9f51d33d71092d0de1790179b0a0
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2024-03-25 19:04:15 +0000

    wip Added tests for filter-css filter-web and filter-configs

---
+++ bin/tests/filter-web/tests.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+# Shell script based test plan
+# set -e gotchas http://mywiki.wooledge.org/BashFAQ/105
+set -e
+
+source ../filter-sounds/lib.sh
+
+# What we're testing and sample input data
+PROGRAM=../../filter-web.sh
+CMD=`basename $PROGRAM`
+SAMPLE=../filter-sounds/in/SAMPLE.txt
+DEBUG=
+SKIP=0
+
+# Include testing library and make output dir exist
+source ../shell-test.sh
+PLAN 11
+
+[ -d out ] || mkdir out
+rm out/* > /dev/null 2>&1 || OK "output dir ready"
+
+# Do not terminate test plan if out/base comparison fails.
+ERROR_STOP=0
+
+# Filter an output file to remove changing text like dates, etc...
+function filter {
+	local file
+	file="$1"
+	filter_egrep "$file"
+}
+
+echo TEST $CMD command help
+TEST=command-help
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG --help $SAMPLE"
+	$PROGRAM $ARGS > $OUT || assertCommandSuccess $? "$PROGRAM $ARGS"
+	filter "$OUT"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
+echo TEST $CMD command invalid option
+TEST=command-invalid
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	EXPECT=2
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG --invalid $SAMPLE"
+	$PROGRAM $ARGS > $OUT 2>&1 || ERR=$?
+	assertCommandFails $ERR $EXPECT "$PROGARM $ARGS"
+	filter "$OUT"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
+echo TEST $CMD command incompatible options
+TEST=command-incompatible-options
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	EXPECT=1
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG --inplace --keep $SAMPLE"
+	$PROGRAM $ARGS 2>&1 | head -1 > $OUT
+	filter "$OUT"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
+echo TEST $CMD command inplace and show are incompatible
+TEST=command-invalid-inplace-show
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	EXPECT=1
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG --inplace --show $SAMPLE"
+	$PROGRAM $ARGS 2>&1 | head -1 > $OUT
+	filter "$OUT"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
+echo TEST $CMD successful operation
+TEST=success
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG"
+	$PROGRAM $ARGS < $SAMPLE > $OUT || assertCommandSuccess $? "$PROGRAM $ARGS"
+	filter "$OUT"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
+echo TEST $CMD successful operation regex
+TEST=success-regex
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG --regex"
+	$PROGRAM $ARGS < $SAMPLE > $OUT || assertCommandSuccess $? "$PROGRAM $ARGS"
+	filter "$OUT"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
+echo TEST $CMD successful operation suppress
+TEST=success-suppress
+if [ 0 == "$SKIP" ]; then
+	ERR=0
+	OUT=out/$TEST.out
+	BASE=base/$TEST.base
+	ARGS="$DEBUG -v"
+	$PROGRAM $ARGS < $SAMPLE > $OUT || assertCommandSuccess $? "$PROGRAM $ARGS"
+	filter "$OUT"
+	assertFilesEqual "$OUT" "$BASE" "$TEST"
+else
+	echo SKIP $TEST "$SKIP"
+fi
+
+cleanUpAfterTests
