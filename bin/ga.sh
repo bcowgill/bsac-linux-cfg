e9efb91e05435efabc730dd1356195585c252eb6
Brent S.A. Cowgill <968686+bcowgill@users.noreply.github.com>
2024-04-16 07:40:39 +0100

    wip In ga.sh additional .orig files to checkout after clean.sh

--- bin/ga.sh
+++ bin/ga.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # BSACSYS Part of Brent S.A. Cowgill's System Toolkit
 
+ORIG="template/playwright/__vendor__/playwright-core/client/harRouter.js.orig \
+	template/playwright/__vendor__/playwright-core/client/page.js.orig \
+	template/playwright/__vendor__/playwright-core/server/dispatchers/dispatcher.js.orig \
+	template/playwright/__vendor__/playwright-core/server/dispatchers/localUtilsDispatcher.js.orig \
+	cfg/raspberrypi/etc/rc.local.orig"
+
 git add cfg/.gconf/apps/gnome-terminal/profiles/Default/%gconf.xml
 clean.sh
-git checkout -- cfg/raspberrypi/etc/rc.local.orig
-WORKON="wip Updated gconf.xml settings" git commit
+git checkout -- $ORIG
 
+WORKON="wip Updated gconf.xml settings" git commit
