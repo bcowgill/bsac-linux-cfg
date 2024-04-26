ef4834d798e493d397e0e017ecdd729c48df8340
Brent S.A. Cowgill <brent.cowgill@wipro.com>
2024-04-26 21:13:11 +0100

    wip In javascript/prettier finished config, add to package.json
    in ts-react experiment documented why eslint setup failed

--- bin/template/javascript/prettier/prettier.config.mjs
+++ bin/template/javascript/prettier/prettier.config.mjs
@@ -1,30 +1,51 @@
 // prettier.config.js, .prettierrc.js, prettier.config.mjs, or .prettierrc.mjs
 
+// The package.json prettier option has priority over this file.
+
 /** @type {import("prettier").Config} */
 const config = {
-	experimentalTernaries: true,
-	endOfLine: 'lf',
-	useTabs: true,
-	tabWidth: 4,
-	printWidth: 80,
-	proseWrap: 'preserve', // markdown
-	trailingComma: 'all',
-	semi: false,
-	singleQuote: true,
-	jsxSingleQuote: false,
-	singleAttributePerLine: false,
-	arrowParens: 'always',
-	bracketSameLine: false,
-	bracketSpacing: true,
-	quoteProps: 'as-needed',
-	htmlWhitespaceSensitivity: 'css',
-	overrides: [
+	'f-q': true,
+	'experimentalTernaries': true,
+	'endOfLine': 'lf',
+	'useTabs': true,
+	'tabWidth': 4,
+	'printWidth': 80,
+	'proseWrap': 'preserve', // markdown
+	'trailingComma': 'all',
+	'semi': false,
+	'singleQuote': true,
+	'jsxSingleQuote': false,
+	'singleAttributePerLine': false,
+	'arrowParens': 'always',
+	'bracketSameLine': false,
+	'bracketSpacing': true,
+	'quoteProps': 'as-needed',
+	'htmlWhitespaceSensitivity': 'css',
+	'overrides': [
 		{
-			files: ['package*.json'],
-			options: {
-				parser: 'json',
-				useTabs: false,
-				tabWidth: 2,
+			'f-q': true,
+			'files': ['package*.json'],
+			'options': {
+				'f-q': true,
+				'parser': 'json',
+				'useTabs': false,
+				'tabWidth': 2,
+			},
+		},
+		{
+			// Using prettier with this config file will convert unquoted keys object
+			// into a quoted-keys JSON for pasting into package.json
+			// ./node_modules/.bin/prettier --config prettier.config.mjs --write prettier.config.mjs
+			'force-quoted':
+				'a single quoted key, with quoteProps "consistent" will make them all quoted',
+			'files': ['prettier.config.mjs'],
+			'options': {
+				'f-q': true,
+				'useTabs': false,
+				'tabWidth': 2,
+				'singleQuote': false,
+				'quoteProps': 'consistent',
+				'trailingComma': 'none',
 			},
 		},
 	],
